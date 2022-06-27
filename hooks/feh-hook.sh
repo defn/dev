4600324322fc705815a48d93ea8b52430ee7a886
Amanibhavam <iam@defn.sh>
2022-06-27 07:41:06 +0000

    move tailscale cert to operator

--- hooks/feh-hook.sh
+++ hooks/feh-hook.sh
@@ -8,7 +8,17 @@ kubernetes:
   kind: Namespace
   executeHookOnEvent: ["Added"]
 EOF
-else
-  nsName=$(jq -r .[0].object.metadata.name $BINDING_CONTEXT_PATH)
-  echo "Namespace '${nsName}' added"
+  exit 0
 fi
+
+nsName="$(jq -r .[0].object.metadata.name $BINDING_CONTEXT_PATH)"
+echo "Namespace '${nsName}' added"
+
+cd
+source .bashrc
+make kubeconfig
+d="$(docker exec tailscale_docker-extension-desktop-extension-service /app/tailscale cert 2>&1 | grep For.domain | cut -d'"' -f2)"
+docker exec tailscale_docker-extension-desktop-extension-service /app/tailscale cert $d
+docker exec tailscale_docker-extension-desktop-extension-service tar cvfz - $d.crt $d.key > /tmp/$d.tar.gz
+kubectl --context pod -n traefik delete secret default-certificate
+kubectl --context pod create -n traefik secret generic default-certificate --from-file tls.crt=<(tar xfz /tmp/$d.tar.gz -O $d.crt) --from-file tls.key=<(tar xfz /tmp/$d.tar.gz -O $d.key)
