3eeeaa587a2323079191357071d0e5556d125b39
Amanibhavam <iam@defn.sh>
2022-07-24 23:44:18 -0700

    lock password store on droplet

--- tf/main.tf
+++ tf/main.tf
@@ -147,7 +147,7 @@ resource "digitalocean_volume_attachment" "dev" {
     }
 
     inline = [
-      "set -x; chown -R ubuntu:ubuntu /mnt"
+      "set -x; chown -R ubuntu:ubuntu /mnt; cd /mnt/work/password-store && git crypt lock"
     ]
   }
 
