250e9cb63aade91297f417b85ca66771aded4eab
Amanibhavam <iam@defn.sh>
2022-07-24 23:55:36 -0700

    fix chown

--- tf/main.tf
+++ tf/main.tf
@@ -160,7 +160,7 @@ resource "digitalocean_volume_attachment" "dev" {
     }
 
     inline = [
-      "set -x; chown -R ubuntu:ubuntu /mnt; cd /mnt/work/password-store && chown -R root:root . && git crypt lock && chown -R ubuntu:ubuntu"
+      "set -x; chown -R ubuntu:ubuntu /mnt; cd /mnt/work/password-store && chown -R root:root . && git crypt lock && chown -R ubuntu:ubuntu ."
     ]
   }
 
