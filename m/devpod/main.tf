c1a1b9d2ff1290997abeaf0fa60966e32998de5f
Amanibhavam <iam@defn.sh>
2022-08-09 18:12:05 -0700

    run code-server again

--- m/devpod/main.tf
+++ m/devpod/main.tf
@@ -126,7 +126,7 @@ resource "kubernetes_stateful_set" "dev" {
           image_pull_policy = "Always"
 
           command = ["/usr/bin/tini", "--"]
-          args    = ["bash", "-c", "echo exec ~/bin/e code-server --bind-addr 0.0.0.0:8888 --disable-telemetry; exec tail -f /dev/null"]
+          args    = ["bash", "-c", "exec ~/bin/e code-server --bind-addr 0.0.0.0:8888 --disable-telemetry"]
 
           tty = true
 
