7a96e89f7122d9fc1360d7cd98f85bf29232a69b
Amanibhavam <iam@defn.sh>
2022-08-13 22:40:27 +0000

    pass archive to docker build

--- tf/do/user_data.sh
+++ tf/do/user_data.sh
@@ -4,14 +4,12 @@ function main {
     set -exfu
 
     cd /
-    echo "${archive}" | base64 -d | tar xvfz -
-
     install -o ubuntu -g ubuntu ~/.ssh/authorized_keys ~ubuntu/.ssh/ 
 
     cd ~ubuntu
     sudo -u ubuntu -H git fetch
     sudo -u ubuntu -H git reset --hard origin/main
-    sudo -u ubuntu -H bin/e env DEFN_DEV_HOST=${host} DEFN_DEV_IP=${ip} make provision-digital-ocean
+    sudo -u ubuntu -H bin/e env DEFN_DEV_HOST=${host} DEFN_DEV_IP=${ip} DEFN_DEV_ARCHIVE=${archive} make provision-digital-ocean
 }
 
 main "$@"
