1c7978f4fd51d76fc009f7eabc8b6707e5ea14d0
Amanibhavam <iam@defn.sh>
2022-08-13 21:44:47 -0700

    bind imma to 127.0.0.1 for faster k8s

--- tf/do/user_data.sh
+++ tf/do/user_data.sh
@@ -4,12 +4,12 @@ function main {
     set -exfu
 
     cd /
-    install -o ubuntu -g ubuntu ~/.ssh/authorized_keys ~ubuntu/.ssh/ 
+    install -o ubuntu -g ubuntu ~/.ssh/authorized_keys ~ubuntu/.ssh/
 
     cd ~ubuntu
     sudo -u ubuntu -H git fetch
     sudo -u ubuntu -H git reset --hard origin/main
-    sudo -u ubuntu -H bin/e env DEFN_DEV_HOST=${host} DEFN_DEV_IP=${ip} DEFN_DEV_ARCHIVE=${archive} make provision-digital-ocean
+    sudo -u ubuntu -H bin/e env DEFN_DEV_HOST=${host} DEFN_DEV_HOST_API=${host} DEFN_DEV_IP=${ip} DEFN_DEV_ARCHIVE=${archive} make provision-digital-ocean
 }
 
 main "$@"
