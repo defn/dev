43a4f65c0fad7228ba72b78ed85e2c9cd91fa371
Amanibhavam <iam@defn.sh>
2022-09-30 05:39:29 +0000

    use tskey if present

--- d/k3d/k3s-wrapper.sh
+++ d/k3d/k3s-wrapper.sh
@@ -6,6 +6,10 @@ set -exfu
 
 container_ip=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1`
 
+if [[ -n "${DEFN_DEV_TSKEY:-}" ]]; then
+  tailscale up --authkey="${DEFN_DEV_TSKEY}"
+fi
+
 while true; do
   ts_ip=`/tailscale ip -4 || true`
   if test -n "${ts_ip}"; then break; fi
