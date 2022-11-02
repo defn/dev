da902efe050e3e622f3b529ede548a6ad96705dc
Amanibhavam <iam@defn.sh>
2022-11-02 05:36:19 +0000

    stdenv derivation

---
+++ nix/complex.nix
@@ -0,0 +1,18 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  pname = "complex";
+  version = "0.0.1";
+  buildInputs = [];
+  setSourceRoot = "sourceRoot=`pwd`";
+  src = fetchurl {
+    url = "https://github.com/tilt-dev/tilt/releases/download/v0.30.10/tilt.0.30.10.linux.x86_64.tar.gz";
+    sha256 = "67133d806f900eef0a36665b39b8c9ef7d70eacb0f4876ede3ce627049aaa6cf";
+  };
+  buildPhase = ''
+    find . -ls
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp tilt $out/bin
+  '';
+}
