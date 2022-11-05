971cbfae7b052c57c151f6dbb342a68c6a61885f
Amanibhavam <iam@defn.sh>
2022-11-05 08:37:26 +0000

    dont hardcode the system

--- nix/supfoo/flake.nix
+++ nix/supfoo/flake.nix
@@ -9,7 +9,7 @@
   };
 
   outputs = { self, nixpkgs, fooPkg, supPkg, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
           foo = fooPkg.defaultPackage.${system};
           sup = supPkg.defaultPackage.${system};
