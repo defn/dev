e455eb0782a395e0698c3cba507d2c54077d28ae
Amanibhavam <iam@defn.sh>
2022-11-05 18:37:43 +0000

    reindent

--- nix/sup/flake.nix
+++ nix/sup/flake.nix
@@ -19,15 +19,15 @@
             version = "9.0.0";
 
             src = pkgs.fetchurl {
-                url = "https://github.com/tilt-dev/tilt/releases/download/v0.30.10/tilt.0.30.10.linux.x86_64.tar.gz";
-                sha256 = "67133d806f900eef0a36665b39b8c9ef7d70eacb0f4876ede3ce627049aaa6cf";
+              url = "https://github.com/tilt-dev/tilt/releases/download/v0.30.10/tilt.0.30.10.linux.x86_64.tar.gz";
+              sha256 = "67133d806f900eef0a36665b39b8c9ef7d70eacb0f4876ede3ce627049aaa6cf";
             };
 
             sourceRoot = ".";
 
             installPhase = ''
-                install -m 0755 -D tilt $out/bin/sup
-                '';
+              install -m 0755 -D tilt $out/bin/sup
+              '';
 
             meta = with lib; {
               homepage = "https://defn.sh/sup";
--- nix/supfoo/flake.nix
+++ nix/supfoo/flake.nix
@@ -11,18 +11,19 @@
   outputs = { self, nixpkgs, fooPkg, supPkg, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let 
-          pkgs = nixpkgs.legacyPackages.${system};
-          foo = fooPkg.defaultPackage.${system};
-          sup = supPkg.defaultPackage.${system};
-          lib = nixpkgs.lib;
+        pkgs = nixpkgs.legacyPackages.${system};
+        foo = fooPkg.defaultPackage.${system};
+        sup = supPkg.defaultPackage.${system};
+        lib = nixpkgs.lib;
       in {
-        devShell = pkgs.mkShell rec {
-          buildInputs = [
-            foo
-            sup
-            self.defaultPackage.${system}
-          ];
-        };
+        devShell = 
+          pkgs.mkShell rec {
+            buildInputs = [
+              foo
+              sup
+              self.defaultPackage.${system}
+            ];
+          };
 
         defaultPackage =
           with import nixpkgs { inherit system; };
@@ -32,15 +33,15 @@
             version = "9.0.1";
 
             src = pkgs.fetchurl {
-                url = "https://github.com/tilt-dev/tilt/releases/download/v0.30.10/tilt.0.30.10.linux.x86_64.tar.gz";
-                sha256 = "67133d806f900eef0a36665b39b8c9ef7d70eacb0f4876ede3ce627049aaa6cf";
+              url = "https://github.com/tilt-dev/tilt/releases/download/v0.30.10/tilt.0.30.10.linux.x86_64.tar.gz";
+              sha256 = "67133d806f900eef0a36665b39b8c9ef7d70eacb0f4876ede3ce627049aaa6cf";
             };
 
             sourceRoot = ".";
 
             installPhase = ''
-                install -m 0755 -D tilt $out/bin/supfoo
-                '';
+              install -m 0755 -D tilt $out/bin/supfoo
+              '';
 
             meta = with lib; {
               homepage = "https://defn.sh/supfoo";
