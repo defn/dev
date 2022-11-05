6a5f2d13b4cf9b1bc0fdaf376a89c3ce9978cdf4
Amanibhavam <iam@defn.sh>
2022-11-05 08:23:43 +0000

    using sup, foo

---
+++ nix/supfoo/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "using sup, foo"
+
+  inputs = {
+     nixpkgs.url = "github:NixOS/nixpkgs";
+     flake-utils.url = "github:numtide/flake-utils";
+     fooPkg.url = "path:../foo";
+     supPkg.url = "path:../sup";
+  };
+
+  outputs = { self, nixpkgs, fooPkg, supPkg, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+          foo = fooPkg.defaultPackage.${system};
+          sup = supPkg.defaultPackage.${system};
+          lib = nixpkgs.lib;
+
+      in {
+        devShell = pkgs.mkShell rec {
+          buildInputs = [
+            foo
+            sup
+          ];
+        };
+      });
+}
