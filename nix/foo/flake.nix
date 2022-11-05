1503a72ab49cd3c9dee49891670bc4aa1b9e7bb5
Amanibhavam <iam@defn.sh>
2022-11-05 08:05:59 +0000

    build foo

---
+++ nix/foo/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "sup foo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/22.05";
+  };
+
+  outputs = {self, nixpkgs}: {
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86_64-linux"; };
+
+      stdenv.mkDerivation rec {
+        name = "foo-${version}";
+
+        version = "9.0.0";
+
+        src = pkgs.fetchurl {
+            url = "https://github.com/tilt-dev/tilt/releases/download/v0.30.10/tilt.0.30.10.linux.x86_64.tar.gz";
+            sha256 = "67133d806f900eef0a36665b39b8c9ef7d70eacb0f4876ede3ce627049aaa6cf";
+        };
+
+        sourceRoot = ".";
+
+        installPhase = ''
+            install -m 0755 -D tilt $out/bin/foo
+            '';
+
+        meta = with lib; {
+          homepage = "https://defn.sh/foo";
+          description = "packaging binaries with flake";
+          platforms = platforms.linux;
+        };
+      };
+  };
+}
