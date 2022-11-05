89495f1d5da4910d52f3e89c4a091734d83d0f2e
Amanibhavam <iam@defn.sh>
2022-11-05 20:13:47 +0000

    dont need complex, simple example

--- nix/simple/default.nix
+++
@@ -1,9 +0,0 @@
-with (import <nixpkgs> { });
-derivation {
-  inherit coreutils findutils gnutar gzip;
-  name = "simple";
-  builder = "${bash}/bin/bash";
-  args = [ ./simple.sh ];
-  download = /nix/store/5qx34wwpqakxadshwavhbsrv39syayxs-tilt.0.30.10.linux.x86_64.tar.gz;
-  system = builtins.currentSystem;
-}
