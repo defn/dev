55f8e93444435de51b675f490f1fe6d5459c9baf
Amanibhavam <iam@defn.sh>
2022-11-06 20:00:44 +0000

    end to end is commpile, build, image

--- nix/image.sh
+++ nix/image.sh
@@ -2,12 +2,11 @@
 
 set -exu
 
-n build
-sudo rm -rf nix
+name="$1"; shift
+
 mkdir -p nix/store
 time for a in $(nix-store -qR ./result); do rsync -ia $a nix/store/; done
 (echo '# syntax=docker/dockerfile:1'; echo FROM alpine; for a in nix/store/*/; do echo COPY --link $a /$a/; done; echo ENTRYPOINT [ '"/bin/sh"' ]) > Dockerfile
 echo "ENV PATH $(for a in nix/store/*/; do echo -n "/$a/bin:"; done)/bin" >> Dockerfile
-time env DOCKER_BUILDKIT=1 docker build -t meh .
-docker image list | grep ^meh
-docker run -ti --rm meh
+time env DOCKER_BUILDKIT=1 docker build -t "${name}" .
+docker image list | grep "^${name}"
