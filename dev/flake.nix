ab909a89f12c857b0d70db566365ba257c1d34aa
Amanibhavam <iam@defn.sh>
2022-11-09 04:05:34 +0000

    separate config for sharing paackages

---
+++ dev/flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    cue-pkg.url = github:defn/pkg?dir=cue&ref=v0.0.2;
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , cue-pkg
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      cue = cue-pkg.defaultPackage.${system};
+    in
+    {
+      defaultPackage =
+        with import nixpkgs { inherit system; };
+        stdenv.mkDerivation rec {
+          name = "${slug}-${version}";
+
+          slug = "home";
+          version = "0.0.1";
+
+          dontUnpack = true;
+
+          installPhase = "mkdir -p $out";
+
+          propagatedBuildInputs = [
+            cue
+          ];
+
+          meta = with lib;
+            {
+              homepage = "https://defn.sh/${slug}";
+              description = "packaging binaries with flake";
+              platforms = platforms.linux;
+            };
+        };
+    });
+}
