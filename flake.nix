8768b3c07cc412296cb70650ab4cfd980f3a5e23
Amanibhavam <iam@defn.sh>
2022-11-09 04:31:13 +0000

    dogfood homedev

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    home.url = "path:/home/ubuntu/dev";
     tilt-pkg.url = github:defn/pkg?dir=tilt&ref=v0.0.2;
     caddy-pkg.url = github:defn/pkg?dir=caddy&ref=v0.0.1;
     temporalite-pkg.url = github:defn/pkg?dir=temporalite&ref=v0.0.1;
     kubectl-pkg.url = github:defn/pkg?dir=kubectl&ref=v0.0.1;
-    cue-pkg.url = github:defn/pkg?dir=cue&ref=v0.0.2;
     argocd-pkg.url = github:defn/pkg?dir=argocd&ref=v0.0.2;
   };
 
@@ -14,11 +14,11 @@
     { self
     , nixpkgs
     , flake-utils
+    , home
     , tilt-pkg
     , caddy-pkg
     , temporalite-pkg
     , kubectl-pkg
-    , cue-pkg
     , argocd-pkg
     }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -28,13 +28,12 @@
       caddy = caddy-pkg.defaultPackage.${system};
       temporalite = temporalite-pkg.defaultPackage.${system};
       kubectl = kubectl-pkg.defaultPackage.${system};
-      cue = cue-pkg.defaultPackage.${system};
       argocd = argocd-pkg.defaultPackage.${system};
     in
     {
       devShell = pkgs.mkShell {
         buildInputs = [
-          pkgs.jq
+          home.defaultPackage.${system}
           pkgs.go
           pkgs.gotools
           pkgs.go-tools
@@ -58,7 +57,6 @@
           caddy
           temporalite
           kubectl
-          cue
           argocd
         ];
       };
