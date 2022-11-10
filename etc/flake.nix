01504ec8c89ec7455cbf196d075249064250e9c5
Amanibhavam <iam@defn.sh>
2022-11-10 01:12:12 +0000

    rename flake template, use url

--- etc/flake.nix
+++ etc/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    home.url = "path:/home/ubuntu/dev";
+    home.url = "github:defn/dev?dir=dev&ref=v0.0.2";
   };
 
   outputs =
