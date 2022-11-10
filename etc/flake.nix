a51fd89b570384454029b4245c9223b5f68bca8f
Amanibhavam <iam@defn.sh>
2022-11-10 04:07:50 +0000

    upgrade to dev 0.0.4

--- etc/flake.nix
+++ etc/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/22.05"; # nixpkgs-unstable
     flake-utils.url = "github:numtide/flake-utils";
-    home.url = "github:defn/dev?dir=dev&ref=v0.0.2";
+    home.url = "github:defn/dev?dir=dev&ref=v0.0.4";
   };
 
   outputs =
