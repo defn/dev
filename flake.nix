14206e5f159be9c12df063d268cd6ce605b5ffd1
Amanibhavam <iam@defn.sh>
2022-11-11 07:24:00 +0000

    using forEach

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
     dev.url = github:defn/pkg?dir=dev&ref=v0.0.14;
+
     caddy-pkg.url = github:defn/pkg?dir=caddy&ref=v0.0.1;
     kubectl-pkg.url = github:defn/pkg?dir=kubectl&ref=v0.0.1;
     argocd-pkg.url = github:defn/pkg?dir=argocd&ref=v0.0.2;
@@ -20,21 +21,15 @@
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      caddy = caddy-pkg.defaultPackage.${system};
-      kubectl = kubectl-pkg.defaultPackage.${system};
-      argocd = argocd-pkg.defaultPackage.${system};
       values = import ./values.nix { inherit pkgs; inherit system; };
     in
     rec {
       devShell =
         pkgs.mkShell rec {
-          buildInputs = values.buildInputs ++ [
-            dev.defaultPackage.${system}
-            defaultPackage
-            caddy
-            kubectl
-            argocd
-          ];
+          buildInputs =
+            values.buildInputs
+            ++ [ dev.defaultPackage.${system} defaultPackage ]
+            ++ nixpkgs.lib.lists.forEach [ caddy-pkg kubectl-pkg argocd-pkg ] (f: f.defaultPackage.${system});
         };
 
       defaultPackage =
