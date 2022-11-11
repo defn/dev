892a2bf5e6136af93adda86304f870daa8c25a31
Amanibhavam <iam@defn.sh>
2022-11-11 17:13:20 +0000

    fix version

--- min/flake.nix
+++ min/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/22.04; # nixpkgs-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/22.05; # nixpkgs-unstable;
     flake-utils.url = github:numtide/flake-utils;
 
     dev.url = github:defn/pkg?dir=dev&ref=v0.0.15;
