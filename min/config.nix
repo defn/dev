97883a81901e353fdf15892b441e00419e7b51eb
Amanibhavam <iam@defn.sh>
2022-11-12 20:22:37 +0000

    dont repeat url

--- min/config.nix
+++ min/config.nix
@@ -3,30 +3,31 @@ rec {
   version = "0.8.0";
   homepage = "https://defn.sh/${slug}";
   description = "minimal flake config";
+  url_template = input: "https://github.com/charmbracelet/gum/releases/download/v${input.version}/gum_${input.version}_${input.os}_${input.arch}.tar.gz";
   downloads = {
     "x86_64-linux" = rec {
+      inherit version;
       os = "linux";
       arch = "x86_64";
       sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
-      url = "https://github.com/charmbracelet/gum/releases/download/v${version}/gum_${version}_${os}_${arch}.tar.gz";
     };
     "aarch64-linux" = rec {
+      inherit version;
       os = "linux";
       arch = "arm64";
       sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
-      url = "";
     };
     "x86_64-darwin" = rec {
+      inherit version;
       os = "darwin";
       arch = "x86_64";
       sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
-      url = "";
     };
     "aarch64-darwin" = rec {
+      inherit version;
       os = "darwin";
       arch = "x86_64";
       sha256 = " sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
-      url = "";
     };
   };
 }
