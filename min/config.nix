610440556e1e4801a5a7e87de46782a842396d26
Amanibhavam <iam@defn.sh>
2022-11-12 20:16:37 +0000

    fix min

--- min/config.nix
+++ min/config.nix
@@ -1,33 +1,32 @@
 rec {
   slug = "defn-dev-min";
-  version = "0.0.1";
+  version = "0.8.0";
   homepage = "https://defn.sh/${slug}";
   description = "minimal flake config";
   downloads = {
     "x86_64-linux" = rec {
       os = "linux";
       arch = "x86_64";
-      url = "";
       sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+      url = "https://github.com/charmbracelet/gum/releases/download/v${version}/gum_${version}_${os}_${arch}.tar.gz";
     };
     "aarch64-linux" = rec {
       os = "linux";
       arch = "arm64";
-      url = "";
       sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+      url = "";
     };
     "x86_64-darwin" = rec {
       os = "darwin";
       arch = "x86_64";
-      url = "";
       sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+      url = "";
     };
     "aarch64-darwin" = rec {
       os = "darwin";
       arch = "x86_64";
-      url = "";
       sha256 = " sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+      url = "";
     };
   };
 }
-
