49a2fc4b3899ceb8409e6f3d90b158b214e0aca1
Amanibhavam <iam@defn.sh>
2022-11-12 20:14:09 +0000

    put downloads into config.nix

--- min/config.nix
+++ min/config.nix
@@ -3,4 +3,31 @@ rec {
   version = "0.0.1";
   homepage = "https://defn.sh/${slug}";
   description = "minimal flake config";
+  downloads = {
+    "x86_64-linux" = rec {
+      os = "linux";
+      arch = "x86_64";
+      url = "";
+      sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+    };
+    "aarch64-linux" = rec {
+      os = "linux";
+      arch = "arm64";
+      url = "";
+      sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+    };
+    "x86_64-darwin" = rec {
+      os = "darwin";
+      arch = "x86_64";
+      url = "";
+      sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+    };
+    "aarch64-darwin" = rec {
+      os = "darwin";
+      arch = "x86_64";
+      url = "";
+      sha256 = " sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+    };
+  };
 }
+
