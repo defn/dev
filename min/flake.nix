bffcc40a495b99f3c929c8100d049da8b5444986
Amanibhavam <iam@defn.sh>
2022-11-13 05:20:29 +0000

    merging lines

--- min/flake.nix
+++ min/flake.nix
@@ -3,57 +3,53 @@
     dev.url = github:defn/pkg?dir=dev&ref=v0.0.54;
   };
 
-  outputs = inputs:
-    inputs.dev.main {
-      inherit inputs;
+  outputs = inputs: inputs.dev.main {
+    inherit inputs;
 
-      config = rec {
-        slug = "defn-dev-min";
-        version = "0.8.0";
-        homepage = "https://defn.sh/${slug}";
-        description = "minimal flake config";
+    config = rec {
+      slug = "gum";
+      version = "0.8.0";
+      homepage = "https://github.com/defn/pkg/tree/master/${slug}";
+      description = "${slug}";
 
-        url_template = input: "https://github.com/charmbracelet/gum/releases/download/v${input.version}/gum_${input.version}_${input.os}_${input.arch}.tar.gz";
+      url_template = input: "https://github.com/charmbracelet/gum/releases/download/v${input.version}/gum_${input.version}_${input.os}_${input.arch}.tar.gz";
 
-        downloads = {
-          "x86_64-linux" = rec {
-            inherit version;
-            os = "linux";
-            arch = "x86_64";
-            sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
-          };
-          "aarch64-linux" = rec {
-            inherit version;
-            os = "linux";
-            arch = "arm64";
-            sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
-          };
-          "x86_64-darwin" = rec {
-            inherit version;
-            os = "darwin";
-            arch = "x86_64";
-            sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
-          };
-          "aarch64-darwin" = rec {
-            inherit version;
-            os = "darwin";
-            arch = "x86_64";
-            sha256 = " sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
-          };
+      installPhase = { src }: ''
+        install -m 0755 -d $out $out/bin
+        install -m 0755 ${slug} $out/bin/${slug}
+      '';
+
+      downloads = {
+        "x86_64-linux" = rec {
+          inherit version;
+          os = "linux";
+          arch = "x86_64";
+          sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
         };
-
-        installPhase = { src }: ''
-          install -m 0755 -d $out $out/bin
-          install -m 0755 gum $out/bin/gum
-        '';
+        "aarch64-linux" = rec {
+          inherit version;
+          os = "linux";
+          arch = "arm64";
+          sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+        };
+        "x86_64-darwin" = rec {
+          inherit version;
+          os = "darwin";
+          arch = "x86_64";
+          sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+        };
+        "aarch64-darwin" = rec {
+          inherit version;
+          os = "darwin";
+          arch = "x86_64";
+          sha256 = " sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+        };
       };
-
-      handler = { pkgs, wrap, system }:
-        rec {
-          devShell = wrap.devShell;
-          defaultPackage = wrap.nullBuilder {};
-          # TODO defaultPackage = wrap.bashBuilder {};
-          # TODO defaultPackage = wrap.downloadBuilder;
-        };
     };
+
+    handler = { pkgs, wrap, system }: rec {
+      devShell = wrap.devShell;
+      defaultPackage = wrap.downloadBuilder { };
+    };
+  };
 }
