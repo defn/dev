ec93238f0851004d089883687279e310fa6352f8
Amanibhavam <iam@defn.sh>
2022-11-13 03:00:55 +0000

    inline config

--- min/flake.nix
+++ min/flake.nix
@@ -1,29 +1,71 @@
 {
   inputs = {
-    dev.url = github:defn/pkg?dir=dev&ref=v0.0.50;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.53;
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable; # TODO latest
   };
 
   outputs = inputs:
-    inputs.dev.eachDefaultSystem (system:
-      let
-        site = import ./config.nix;
-        pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
-        wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; inherit site; };
-        latest = import inputs.nixpkgs { inherit system; }; # TODO latest
-      in
-      rec {
-        devShell = wrap.devShell;
-        # TODO defaultPackage = wrap.nullBuilder {};
-        # TODO defaultPackage = wrap.downloadBuilder;
-        defaultPackage = wrap.bashBuilder {
-          src = ./.;
-          installPhase = "mkdir -p $out";
+    inputs.dev.main {
+      inherit inputs;
 
-          propagatedBuildInputs = with latest; [ # TODO latest
-            rsync
-          ];
+      config = rec {
+        slug = "defn-dev-min";
+        version = "0.8.0";
+        homepage = "https://defn.sh/${slug}";
+        description = "minimal flake config";
+
+        url_template = input: "https://github.com/charmbracelet/gum/releases/download/v${input.version}/gum_${input.version}_${input.os}_${input.arch}.tar.gz";
+
+        downloads = {
+          "x86_64-linux" = rec {
+            inherit version;
+            os = "linux";
+            arch = "x86_64";
+            sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+          };
+          "aarch64-linux" = rec {
+            inherit version;
+            os = "linux";
+            arch = "arm64";
+            sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+          };
+          "x86_64-darwin" = rec {
+            inherit version;
+            os = "darwin";
+            arch = "x86_64";
+            sha256 = "sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+          };
+          "aarch64-darwin" = rec {
+            inherit version;
+            os = "darwin";
+            arch = "x86_64";
+            sha256 = " sha256-K1LJVGyxXb9gzJTVobSuyoMNIR+uRVLiWg/oiMkU9qc=";
+          };
         };
-      }
-    );
+
+        installPhase = { src }: ''
+          install -m 0755 -d $out $out/bin
+          install -m 0755 gum $out/bin/gum
+        '';
+      };
+
+      handler = { pkgs, wrap, system }:
+        let
+          latest = import inputs.nixpkgs { inherit system; }; # TODO latest
+        in
+        rec {
+          devShell = wrap.devShell;
+          # TODO defaultPackage = wrap.nullBuilder {};
+          # TODO defaultPackage = wrap.downloadBuilder;
+          defaultPackage = wrap.bashBuilder {
+            src = ./.;
+            installPhase = "mkdir -p $out";
+
+            propagatedBuildInputs = with latest; [
+              # TODO latest
+              rsync
+            ];
+          };
+        };
+    };
 }
