51fb9ad07c45eafab10665327e2901ae15c9b676
Amanibhavam <iam@defn.sh>
2022-11-13 01:26:20 +0000

    use latest

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     caddy.url = github:defn/pkg?dir=caddy&ref=v0.0.1;
     kubectl.url = github:defn/pkg?dir=kubectl&ref=v0.0.1;
     argocd.url = github:defn/pkg?dir=argocd&ref=v0.0.2;
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    latest.url = github:NixOS/nixpkgs/nixpkgs-unstable;
   };
 
   outputs = inputs:
@@ -13,7 +13,7 @@
         site = import ./config.nix;
         pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
         wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; inherit site; };
-        latest = import inputs.nixpkgs { inherit system; };
+        latest = import inputs.latest { inherit system; };
       in
       rec {
         devShell = wrap.devShell;
