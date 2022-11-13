e456d2fe25d057f9eaae8c96b8551b74784f379d
Amanibhavam <iam@defn.sh>
2022-11-13 02:57:18 +0000

    get rid of config.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    dev.url = github:defn/pkg?dir=dev&ref=v0.0.50;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.53;
     caddy.url = github:defn/pkg?dir=caddy&ref=v0.0.1;
     kubectl.url = github:defn/pkg?dir=kubectl&ref=v0.0.1;
     argocd.url = github:defn/pkg?dir=argocd&ref=v0.0.2;
@@ -8,33 +8,41 @@
   };
 
   outputs = inputs:
-    inputs.dev.eachDefaultSystem (system:
-      let
-        site = import ./config.nix;
-        pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
-        wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; inherit site; };
-        latest = import inputs.latest { inherit system; };
-      in
-      rec {
-        devShell = wrap.devShell;
-        defaultPackage = wrap.nullBuilder {
-          propagatedBuildInputs = with latest; [
-            rsync
-            go
-            gotools
-            go-tools
-            golangci-lint
-            gopls
-            go-outline
-            gopkgs
-            delve
-            nodejs-18_x
-            nixpkgs-fmt
-            kubernetes-helm
-            kube3d
-            vault
-          ];
+    inputs.dev.main {
+      inherit inputs;
+
+      config = rec {
+        slug = "defn-dev";
+        version = "0.0.1";
+        homepage = "https://defn.sh/${slug}";
+        description = "dev environment home directory";
+      };
+
+      handler = { pkgs, wrap, system }:
+        let
+          latest = import inputs.latest { inherit system; };
+        in
+        rec {
+          devShell = wrap.devShell;
+
+          defaultPackage = wrap.nullBuilder {
+            propagatedBuildInputs = with latest; [
+              rsync
+              go
+              gotools
+              go-tools
+              golangci-lint
+              gopls
+              go-outline
+              gopkgs
+              delve
+              nodejs-18_x
+              nixpkgs-fmt
+              kubernetes-helm
+              kube3d
+              vault
+            ];
+          };
         };
-      }
-    );
+    };
 }
