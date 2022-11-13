2cd23d3808afc9cfe4499dbe63d56a4eb19999d8
Amanibhavam <iam@defn.sh>
2022-11-13 01:14:49 +0000

    update minimum

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    dev.url = github:defn/pkg?dir=dev&ref=v0.0.33;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.47;
     caddy.url = github:defn/pkg?dir=caddy&ref=v0.0.1;
     kubectl.url = github:defn/pkg?dir=kubectl&ref=v0.0.1;
     argocd.url = github:defn/pkg?dir=argocd&ref=v0.0.2;
@@ -14,41 +14,30 @@
         pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
         wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; inherit site; };
         latest = import inputs.nixpkgs { inherit system; };
-        buildInputs = with latest; [
-          rsync
-          go
-          gotools
-          go-tools
-          golangci-lint
-          gopls
-          go-outline
-          gopkgs
-          delve
-          nodejs-18_x
-          nixpkgs-fmt
-          kubernetes-helm
-          kube3d
-          vault
-        ];
       in
-      with site;
       rec {
         devShell = wrap.devShell;
-        defaultPackage = pkgs.stdenv.mkDerivation
-          rec {
-            name = "${slug}-${version}";
+        defaultPackage = wrap.bashBuilder {
+          src = ./.;
+          installPhase = "mkdir -p $out";
 
-            dontUnpack = true;
-
-            installPhase = "mkdir -p $out";
-
-            propagatedBuildInputs = buildInputs;
-
-            meta = with pkgs.lib; with site; {
-              inherit homepage;
-              inherit description;
-            };
-          };
+          propagatedBuildInputs = with latest; [
+            rsync
+            go
+            gotools
+            go-tools
+            golangci-lint
+            gopls
+            go-outline
+            gopkgs
+            delve
+            nodejs-18_x
+            nixpkgs-fmt
+            kubernetes-helm
+            kube3d
+            vault
+          ];
+        };
       }
     );
 }
--- min/flake.nix
+++ min/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    dev.url = github:defn/pkg?dir=dev&ref=v0.0.33;
+    dev.url = github:defn/pkg?dir=dev&ref=v0.0.47;
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable; # TODO
   };
 
   outputs = inputs:
@@ -9,11 +10,19 @@
         site = import ./config.nix;
         pkgs = import inputs.dev.wrapper.nixpkgs { inherit system; };
         wrap = inputs.dev.wrapper.wrap { other = inputs; inherit system; inherit site; };
+        latest = import inputs.nixpkgs { inherit system; }; # TODO
       in
-      with site;
       rec {
         devShell = wrap.devShell;
-        defaultPackage = wrap.downloadBuilder;
+        #defaultPackage = wrap.downloadBuilder;
+        defaultPackage = wrap.bashBuilder {
+          src = ./.;
+          installPhase = "mkdir -p $out";
+
+          propagatedBuildInputs = with latest; [
+            rsync
+          ];
+        };
       }
     );
 }
