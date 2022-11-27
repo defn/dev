6d48b26a36c5d584d2c566648b7127a43d34c100
Amanibhavam <iam@defn.sh>
2022-11-26 17:31:04 -0800

    kustomize support default systems

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     argocd.url = github:defn/pkg?dir=argocd&ref=argocd-2.5.2;
     earthly.url = github:defn/pkg?dir=earthly&ref=earthly-0.6.30-1;
     helm.url = github:defn/pkg?dir=helm&ref=helm-3.10.2;
+    kustomize.url = github:defn/pkg?dir=kustomize&ref=kustomize-4.5.7;
 
-    kustomize.url = github:defn/pkg?dir=kustomize&ref=v0.0.63;
     kubectl.url = github:defn/pkg?dir=kubectl&ref=v0.0.63;
     stern.url = github:defn/pkg?dir=stern&ref=v0.0.56;
     k3d.url = github:defn/pkg?dir=k3d&ref=v0.0.63;
