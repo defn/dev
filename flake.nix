5eec4e12956cf48214a25cec0cffcf7ed642d1eb
Amanibhavam <iam@defn.sh>
2022-12-06 18:27:32 -0800

    Revert "codeserver 4.9.0"
    This reverts commit 7a28ee8e393ea34854b0e1a9cdd29d4b7d85f0e3.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     dev.url = github:defn/pkg/dev-0.0.8?dir=dev;
 
-    codeserver.url = github:defn/pkg/codeserver-4.9.0?dir=codeserver;
+    codeserver.url = github:defn/pkg/codeserver-4.8.3?dir=codeserver;
     caddy.url = github:defn/pkg/caddy-2.6.2?dir=caddy;
     argocd.url = github:defn/pkg/argocd-2.5.2?dir=argocd;
     earthly.url = github:defn/pkg/earthly-0.6.30-1?dir=earthly;
