92b87fbe441b539cd5308b9ebdbdfbf747655fec
Amanibhavam <iam@defn.sh>
2022-12-08 21:58:13 -0800

    update all defn/dev deps

--- flake.nix
+++ flake.nix
@@ -1,30 +1,31 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.8?dir=dev;
+    dev.url = github:defn/pkg/dev-0.0.10?dir=dev;
 
     codeserver.url = github:defn/pkg/codeserver-4.8.3?dir=codeserver;
-    caddy.url = github:defn/pkg/caddy-2.6.2?dir=caddy;
-    argocd.url = github:defn/pkg/argocd-2.5.4?dir=argocd;
+
+    caddy.url = github:defn/pkg/caddy-2.6.2-2?dir=caddy;
+    argocd.url = github:defn/pkg/argocd-2.5.4-2?dir=argocd;
     earthly.url = github:defn/pkg/earthly-0.6.30-1?dir=earthly;
-    helm.url = github:defn/pkg/helm-3.10.2?dir=helm;
-    kustomize.url = github:defn/pkg/kustomize-4.5.7?dir=kustomize;
-    kubectl.url = github:defn/pkg/kubectl-1.24.8-1?dir=kubectl;
-    stern.url = github:defn/pkg/stern-1.22.0?dir=stern;
-    tilt.url = github:defn/pkg/tilt-0.30.12?dir=tilt;
-    k3d.url = github:defn/pkg/k3d-5.4.6?dir=k3d;
-    flyctl.url = github:defn/pkg/flyctl-0.0.437?dir=flyctl;
-    yaegi.url = github:defn/pkg/yaegi-0.14.3?dir=yaegi;
-    step.url = github:defn/pkg/step-0.23.0?dir=step;
+    helm.url = github:defn/pkg/helm-3.10.2-1?dir=helm;
+    kustomize.url = github:defn/pkg/kustomize-4.5.7-1?dir=kustomize;
+    kubectl.url = github:defn/pkg/kubectl-1.24.9-1?dir=kubectl;
+    stern.url = github:defn/pkg/stern-1.22.0-1?dir=stern;
+    tilt.url = github:defn/pkg/tilt-0.30.13-1?dir=tilt;
+    k3d.url = github:defn/pkg/k3d-5.4.6-1?dir=k3d;
+    flyctl.url = github:defn/pkg/flyctl-0.0.437-1?dir=flyctl;
+    yaegi.url = github:defn/pkg/yaegi-0.14.3-1?dir=yaegi;
+    step.url = github:defn/pkg/step-0.23.0-1?dir=step;
 
-    c.url = github:defn/pkg/c-0.0.1?dir=c;
-    tf.url = github:defn/pkg/tf-0.0.1?dir=tf;
-    f.url = github:defn/pkg/f-0.0.1?dir=f;
+    c.url = github:defn/pkg/c-0.0.1-2?dir=c;
+    tf.url = github:defn/pkg/tf-0.0.1-1?dir=tf;
+    f.url = github:defn/pkg/f-0.0.1-1?dir=f;
 
-    terraform.url = github:defn/pkg/terraform-1.3.6-1?dir=terraform;
-    vault.url = github:defn/pkg/vault-1.12.2-1?dir=vault;
-    nomad.url = github:defn/pkg/nomad-1.4.3-1?dir=nomad;
+    terraform.url = github:defn/pkg/terraform-1.3.6-2?dir=terraform;
+    vault.url = github:defn/pkg/vault-1.12.2-2?dir=vault;
+    nomad.url = github:defn/pkg/nomad-1.4.3-2?dir=nomad;
 
-    tailscale.url = github:defn/pkg/tailscale-1.32.3-3?dir=tailscale;
+    tailscale.url = github:defn/pkg/tailscale-1.34.0-1?dir=tailscale;
   };
 
   outputs = inputs:
