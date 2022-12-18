caa8454fe44d18fc281031251017ac7f16458a66
Amanibhavam <iam@defn.sh>
2022-12-17 18:27:47 -0800

    remove sleep

--- docker-code-server/main.tf
+++ docker-code-server/main.tf
@@ -121,7 +121,7 @@ resource "docker_container" "workspace" {
 
   entrypoint = [
     "bash", "-c",
-    "set -xfu; cd; git pull; bash -x bin/persist-cache; sleep 600; export CODER_AGENT_AUTH=token; export CODER_AGENT_URL=http://host.docker.internal:3000/; exec /home/ubuntu/.nix-profile/bin/nix run github:defn/pkg/coder-0.13.4-1?dir=coder -- agent"
+    "set -xfu; cd; git pull; bash -x bin/persist-cache; export CODER_AGENT_AUTH=token; export CODER_AGENT_URL=http://host.docker.internal:3000/; exec /home/ubuntu/.nix-profile/bin/nix run github:defn/pkg/coder-0.13.4-1?dir=coder -- agent"
   ]
 
   host {
