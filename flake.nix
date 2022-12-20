bd878e395f0c1017dbc78ac21935a74ac527aa15
amanibhavam <iam@defn.sh>
2022-12-20 05:57:22 +0000

    use az to collect all the wrappers

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     dev.url = github:defn/pkg/dev-0.0.18?dir=dev;
-    c.url = github:defn/pkg/c-0.0.1-6?dir=c;
+    az.url = github:defn/pkg/az-0.0.1?dir=az;
     earthly.url = github:defn/pkg/earthly-0.6.30-3?dir=earthly;
     tilt.url = github:defn/pkg/tilt-0.30.13-3?dir=tilt;
     gh.url = github:defn/pkg/gh-2.20.2-3?dir=gh;
