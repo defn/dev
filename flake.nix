1fb0c5e1e437cd8a8cdab5267816ee0cc86a3bec
amanibhavam <iam@defn.sh>
2022-12-24 19:57:35 +0000

    install flyctl

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     kubectl.url = github:defn/pkg/kubectl-1.25.5-0?dir=kubectl;
     kustomize.url = github:defn/pkg/kustomize-4.5.7-3?dir=kustomize;
     helm.url = github:defn/pkg/helm-3.10.2-3?dir=helm;
+    flyctl.url = github:defn/pkg/flyctl-0.0.441-2?dir=flyctl;
     localdev.url = github:defn/pkg/localdev-0.0.2?dir=localdev;
   };
 
