10aa6b32116d2222fcf28d0c2b82f0dacd92b4cd
amanibhavam <iam@defn.sh>
2022-12-31 04:11:11 +0000

    flyctl 0.0.442 update

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     tilt.url = github:defn/pkg/tilt-0.30.13-3?dir=tilt;
     gh.url = github:defn/pkg/gh-2.20.2-3?dir=gh;
     webhook.url = github:defn/pkg/webhook-2.8.0?dir=webhook;
-    flyctl.url = github:defn/pkg/flyctl-0.0.441-2?dir=flyctl;
+    flyctl.url = github:defn/pkg/flyctl-0.0.442-0?dir=flyctl;
     vault.url = github:defn/pkg/vault-1.12.2-4?dir=vault;
     localdev.url = github:defn/pkg/localdev-0.0.3?dir=localdev;
   };
