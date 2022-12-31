f1bbea02d1ca8a7fa7e449322ca291574ee4b78c
amanibhavam <iam@defn.sh>
2022-12-31 09:12:24 +0000

    install caddy

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     dev.url = github:defn/pkg/dev-0.0.21?dir=dev;
     localdev.url = github:defn/pkg/localdev-0.0.3?dir=localdev;
+    caddy.url = github:defn/pkg/caddy-2.6.2-5?dir=caddy;
     az.url = github:defn/pkg/az-0.0.8?dir=az;
     earthly.url = github:defn/pkg/earthly-0.6.30-3?dir=earthly;
     tilt.url = github:defn/pkg/tilt-0.30.13-3?dir=tilt;
