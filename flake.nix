8d06ffc1a051de483428a6f1e83f2fdb44b3f961
amanibhavam <iam@defn.sh>
2023-01-08 00:17:17 +0000

    used the wrong function

--- flake.nix
+++ flake.nix
@@ -37,14 +37,13 @@
       };
 
       devShell = wrap.devShell {
-        devInputs = wrap.flakeInputs ++ (pkgs.lib.attrsets.mapAttrsToList (name: value: value) commands);
+        devInputs = (wrap.flakeInputs ++
+          pkgs.lib.attrsets.mapAttrsToList (name: value: value) commands);
       };
 
       defaultPackage = wrap.nullBuilder {
         propagatedBuildInputs = with pkgs; wrap.flakeInputs ++ [
           builders.yaegi
-
-          commands.pass
 
           bashInteractive
           gnupg
@@ -84,16 +83,20 @@
         ];
       };
 
-      commands = {
-        pass = pkgs.writeShellScriptBin "pass" ''
+      commands = pkgs.lib.attrsets.mapAttrs
+        (name: value: (pkgs.writeShellScriptBin "this-${name}" value))
+        scripts;
+
+      scripts = {
+        pass = ''
           { ${pkgs.pass}/bin/pass "$@" 2>&1 1>&3 3>&- | grep -v 'problem with fast path key listing'; } 3>&1 1>&2 | cat
         '';
 
-        coder-delete-database = pkgs.writeShellScriptBin "this-coder-delete-database" ''
+        coder-delete-database = ''
           rm -rf ~/.config/coderv2/postgres
         '';
 
-        coder-server-for-orgs-wildcard-tls = pkgs.writeShellScriptBin "this-coder-server-for-orgs-wildcard-tls" ''
+        coder-server-for-orgs-wildcard-tls = ''
           coder server --no-feature-warning --cache-dir ~/.cache/coder --global-config ~/.config/coderv2 \
             --access-url=$(pass coder_access_url) --wildcard-access-url="$(pass coder_wildcard_access_url)" \
             --http-address=localhost:5555 \
@@ -106,7 +109,7 @@
             --oauth2-github-allowed-orgs=$(pass coder_github_allowed_orgs)
         '';
 
-        coder-server-for-orgs-wildcard = pkgs.writeShellScriptBin "this-coder-server-for-orgs-wildcard" ''
+        coder-server-for-orgs-wildcard = ''
           coder server --no-feature-warning --cache-dir ~/.cache/coder --global-config ~/.config/coderv2 \
             --access-url=$(pass coder_access_url) --wildcard-access-url="$(pass coder_wildcard_access_url)" \
             --http-address=localhost:5555 \
@@ -114,7 +117,7 @@
             --oauth2-github-allowed-orgs=$(pass coder_github_allowed_orgs)
         '';
 
-        coder-server-for-orgs = pkgs.writeShellScriptBin "this-coder-server-for-orgs" ''
+        coder-server-for-orgs = ''
           coder server --no-feature-warning --cache-dir ~/.cache/coder --global-config ~/.config/coderv2 \
             --access-url=http://localhost \
             --http-address=localhost:5555 \
@@ -122,7 +125,7 @@
             --oauth2-github-allowed-orgs=$(pass coder_github_allowed_orgs)
         '';
 
-        coder-server-for-everyone = pkgs.writeShellScriptBin "this-coder-server-for-everyone" ''
+        coder-server-for-everyone = ''
           coder server --no-feature-warning --cache-dir ~/.cache/coder --global-config ~/.config/coderv2 \
           --access-url=http://localhost \
           --http-address=localhost:5555 \
@@ -130,12 +133,12 @@
           --oauth2-github-allow-everyone
         '';
 
-        coder-initial-user = pkgs.writeShellScriptBin "this-coder-initial-user" ''
+        coder-initial-user = ''
           coder login --first-user-email=$(pass coder_admin_email) --first-user-password=$(pass coder_admin_password) --first-user-username=$(pass coder_admin_username) \
             --first-user-trial=false http://localhost
         '';
 
-        coder-template-docker = pkgs.writeShellScriptBin "this-coder-template-docker" ''
+        coder-template-docker = ''
           set -exfu
           cd ~/coder/docker-code-server
           coder template create --yes || true
@@ -144,28 +147,28 @@
           coder template edit docker-code-server --icon "/icon/code.svg"
         '';
 
-        coder-server-wait-for-alive = pkgs.writeShellScriptBin "this-coder-server-wait-for-alive" ''
+        coder-server-wait-for-alive = ''
           while [[ "000" == "$(curl -sS -o /dev/null -w "%{http_code}" --connect-timeout 1 -m 1 http://localhost)" ]]; do sleep 1; done
         '';
 
-        coder-server-wait-for-dead = pkgs.writeShellScriptBin "this-coder-server-wait-for-dead" ''
+        coder-server-wait-for-dead = ''
           while [[ "000" != "$(curl -sS -o /dev/null -w "%{http_code}" --connect-timeout 1 -m 1 http://localhost)" ]]; do sleep 1; done
         '';
 
-        coder-server-kill = pkgs.writeShellScriptBin "this-coder-server-kill" ''
+        coder-server-kill = ''
           pkill -9 -f /coder
           pkill -f postgres
           this-coder-server-wait-for-dead
           pkill -f /this-coder
         '';
 
-        coder-server-stop = pkgs.writeShellScriptBin "this-coder-server-stop" ''
+        coder-server-stop = ''
           pkill -f /coder
           this-coder-server-wait-for-dead
           pkill -f /this-coder
         '';
 
-        coder-init = pkgs.writeShellScriptBin "this-coder-init" ''
+        coder-init = ''
            (
              this-coder-server-wait-for-alive
              if ! coder users show me | grep Organizations: | grep admin; then this-coder-initial-user | cat; fi
@@ -178,7 +181,7 @@
           "this-coder-server-for-$for"
         '';
 
-        build = pkgs.writeShellScriptBin "this-build" ''
+        build = ''
           repo="$(git remote get-url origin)"
           branch="$(git rev-parse --abbrev-ref HEAD)"
 
@@ -196,20 +199,20 @@
           echo
         '';
 
-        trust-ca = pkgs.writeShellScriptBin "this-trust-ca" ''
+        trust-ca = ''
           sudo security add-trusted-cert -d -r trustRoot -k ~/Library/Keychains/login.keychain-db etc/ca.crt
         '';
 
-        down = pkgs.writeShellScriptBin "this-down" ''
+        down = ''
           touch .local/share/code-server/idle
         '';
 
-        logout = pkgs.writeShellScriptBin "this-logout" ''
+        logout = ''
           echo yes | gh auth logout --hostname github.com
           echo RELOADAGENT | gpg-connect-agent
         '';
 
-        login = pkgs.writeShellScriptBin "this-login" ''
+        login = ''
           mark vault
           if nc -z -v localhost 8200 2>/dev/null; then \
             this-vault-unseal; fi
@@ -219,23 +222,26 @@
           this-github-login
         '';
 
-        github-login = pkgs.writeShellScriptBin "this-github-login" ''
+        github-login = ''
           if ! gh auth status; then echo Y | gh auth login -p https -h github.com -w; fi
           -gh extension install cli/gh-webhook
           if test -n "''${GIT_AUTHOR_NAME:-}"; then pass GHCR_TOKEN | docker login ghcr.io -u $GIT_AUTHOR_NAME --password-stdin; fi
           -vault login -method=github token="$(cat ~/.config/gh/hosts.yml  | yq -r '.["github.com"].oauth_token')"
         '';
 
-        home-repos = pkgs.writeShellScriptBin "this-home-repos" ''
+        home-repos = ''
           for a in ~ ~/.dotfiles ~/.password-store; do (echo; echo "$a"; cd "$a" && git pull) || true; done
         '';
 
-        all-repos = pkgs.writeShellScriptBin "this-all-repos" ''
+        all-repos = ''
           this-home-repos
           git pull
         '';
-      };
 
+        nix-gc = ''
+          nix-store --gc
+        '';
+      };
     };
   };
 }
