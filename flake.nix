045dd6e8a2e730fd1c6783577038135d9c283f00
amanibhavam <iam@defn.sh>
2023-01-30 19:54:47 -0800

    localdev: coder 0.15.3

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     dev.url = github:defn/pkg/dev-0.0.22?dir=dev;
-    localdev.url = github:defn/pkg/localdev-0.0.8?dir=localdev;
+    localdev.url = github:defn/pkg/localdev-0.0.9?dir=localdev;
     caddy.url = github:defn/pkg/caddy-2.6.2-5?dir=caddy;
     az.url = github:defn/pkg/az-0.0.9?dir=az;
     awscli.url = github:defn/pkg/awscli-2.0.30-2?dir=awscli;
