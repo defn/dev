e83bf81c0b4866794dfbdfa7059721116932ce62
amanibhavam <iam@defn.sh>
2023-02-19 06:52:15 +0000

    tailscale using pkg.main

--- w/tailscale/flake.nix
+++ w/tailscale/flake.nix
@@ -1,46 +1,49 @@
 {
   inputs = {
-    dev.url = github:defn/pkg/dev-0.0.23-rc9?dir=dev;
+    pkg.url = github:defn/pkg/0.0.165;
     tailscale.url = github:defn/pkg/tailscale-1.36.0-0?dir=tailscale;
     cloudflared.url = github:defn/pkg/cloudflared-2023.2.1-0?dir=cloudflared;
   };
 
-  outputs = inputs:
-    inputs.dev.main rec {
-      inherit inputs;
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
 
-      src = builtins.path { path = ./.; name = builtins.readFile ./SLUG; };
-
-      handler = { pkgs, wrap, system, builders, commands, config }: rec {
-        defaultPackage = wrap.nullBuilder {
-          propagatedBuildInputs = wrap.flakeInputs;
-        };
-      };
-
-      scripts = { system }: {
-        "tailscale-start" = ''
-          set -exfu
-
-          sudo ${inputs.tailscale.defaultPackage.${system}}/bin/tailscaled "$@"
-        '';
-
-        "tailscale-up" = ''
-          set -exfu
-
-          sudo ${inputs.tailscale.defaultPackage.${system}}/bin/tailscale up "$@"
-        '';
-
-        "tailscale-save" = ''
-          set -exfu
-
-          sudo tar cvf - /var/lib/tailscale/tailscaled.state /var/lib/tailscale/certs | base64 -w 0 | pass insert -e -f $GIT_AUTHOR_NAME-tailscale
-        '';
-
-        "tailscale-restore" = ''
-          set -exfu
-
-          pass $GIT_AUTHOR_NAME-tailscale | base64 -d | (cd / && sudo tar xvf -)
-        '';
-      };
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs =
+        let
+          flakeInputs = [
+            inputs.tailscale.defaultPackage.${ctx.system}
+            inputs.cloudflared.defaultPackage.${ctx.system}
+          ];
+        in
+        flakeInputs
+        ++ ctx.commands;
     };
+
+    scripts = { system }: {
+      tailscale-start = ''
+        set -exfu
+
+        sudo ${inputs.tailscale.defaultPackage.${system}}/bin/tailscaled "$@"
+      '';
+
+      tailscale-up = ''
+        set -exfu
+
+        sudo ${inputs.tailscale.defaultPackage.${system}}/bin/tailscale up "$@"
+      '';
+
+      tailscale-save = ''
+        set -exfu
+
+        sudo tar cvf - /var/lib/tailscale/tailscaled.state /var/lib/tailscale/certs | base64 -w 0 | pass insert -e -f $GIT_AUTHOR_NAME-tailscale
+      '';
+
+      tailscale-restore = ''
+        set -exfu
+
+        pass $GIT_AUTHOR_NAME-tailscale | base64 -d | (cd / && sudo tar xvf -)
+      '';
+    };
+  };
 }
