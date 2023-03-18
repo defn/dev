a9b2695bac17d419d9c377f59ebd18fd3c96e1f7
amanibhavam <iam@defn.sh>
2023-03-18 18:28:50 +0000

    optional SLUG

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     pkg.url = github:defn/pkg/0.0.166;
     kubernetes.url = github:defn/pkg/kubernetes-0.0.13?dir=kubernetes;
     cloud.url = github:defn/pkg/cloud-0.0.9?dir=cloud;
-    az.url = github:defn/pkg/az-0.0.25?dir=az;
+    az.url = github:defn/pkg/az-0.0.26?dir=az;
     oci.url = github:defn/pkg/oci-0.0.1?dir=oci;
     nix.url = github:defn/pkg/nix-0.0.1?dir=nix;
     secrets.url = github:defn/pkg/secrets-0.0.3?dir=secrets;
