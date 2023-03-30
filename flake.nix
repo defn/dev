4d1b112d1cc29dfa4e43e2fc14b71bee7b3b819b
amanibhavam <iam@defn.sh>
2023-03-29 20:31:53 -0700

    fix terraform

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/pkg/0.0.172;
     kubernetes.url = github:defn/pkg/kubernetes-0.0.20?dir=kubernetes;
-    cloud.url = github:defn/pkg/cloud-0.0.13?dir=cloud;
+    cloud.url = github:defn/pkg/cloud-0.0.14?dir=cloud;
     az.url = github:defn/pkg/az-0.0.42?dir=az;
     oci.url = github:defn/pkg/oci-0.0.3?dir=oci;
     nix.url = github:defn/pkg/nix-0.0.3?dir=nix;
