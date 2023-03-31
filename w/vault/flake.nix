19d2e82982be788012c8ac662149121cbde7edf8
amanibhavam <iam@defn.sh>
2023-03-31 07:00:18 +0000

    upgrade vault

--- w/vault/flake.nix
+++ w/vault/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     pkg.url = github:defn/pkg/0.0.172;
-    vault.url = github:defn/pkg/vault-1.13.0-2?dir=vault;
+    vault.url = github:defn/pkg/vault-1.13.1-0?dir=vault;
     acme.url = github:defn/pkg/acme-3.0.5-8?dir=acme;
   };
 
