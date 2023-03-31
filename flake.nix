f910a88d56ad006aa549ecbcb693f3a280dfaa6e
amanibhavam <iam@defn.sh>
2023-03-31 07:11:31 +0000

    upgrade n latest

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     pkg.url = github:defn/pkg/0.0.172;
     kubernetes.url = github:defn/pkg/kubernetes-0.0.20?dir=kubernetes;
     cloud.url = github:defn/pkg/cloud-0.0.17?dir=cloud;
-    az.url = github:defn/pkg/az-0.0.43?dir=az;
+    az.url = github:defn/pkg/az-0.0.44?dir=az;
     oci.url = github:defn/pkg/oci-0.0.3?dir=oci;
     nix.url = github:defn/pkg/nix-0.0.3?dir=nix;
     secrets.url = github:defn/pkg/secrets-0.0.5?dir=secrets;
