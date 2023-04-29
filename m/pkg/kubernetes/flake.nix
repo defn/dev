c7cc3cbdd80bcfd9bbc740998a54ddc948d425ab
amanibhavam <iam@defn.sh>
2023-04-29 08:00:17 +0000

    upgrade deps

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -4,14 +4,14 @@
     k3d.url = github:defn/dev/pkg-k3d-5.4.9-7?dir=m/pkg/k3d;
     k9s.url = github:defn/dev/pkg-k9s-0.27.3-7?dir=m/pkg/k9s;
     helm.url = github:defn/dev/pkg-helm-3.11.3-5?dir=m/pkg/helm;
-    kustomize.url = github:defn/dev/pkg-kustomize-5.0.1-6?dir=m/pkg/kustomize;
-    stern.url = github:defn/dev/pkg-stern-1.25.0-5?dir=m/pkg/stern;
+    kustomize.url = github:defn/dev/pkg-kustomize-5.0.1-7?dir=m/pkg/kustomize;
+    stern.url = github:defn/dev/pkg-stern-1.25.0-6?dir=m/pkg/stern;
     argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.4.7-5?dir=m/pkg/argoworkflows;
     argocd.url = github:defn/dev/pkg-argocd-2.6.7-8?dir=m/pkg/argocd;
     kn.url = github:defn/dev/pkg-kn-1.10.0-2?dir=m/pkg/kn;
-    vcluster.url = github:defn/dev/pkg-vcluster-0.15.0-5?dir=m/pkg/vcluster;
-    kubevirt.url = github:defn/dev/pkg-kubevirt-0.59.0-6?dir=m/pkg/kubevirt;
-    velero.url = github:defn/dev/pkg-velero-1.11.0-1?dir=m/pkg/velero;
+    vcluster.url = github:defn/dev/pkg-vcluster-0.15.0-6?dir=m/pkg/vcluster;
+    kubevirt.url = github:defn/dev/pkg-kubevirt-0.59.0-7?dir=m/pkg/kubevirt;
+    velero.url = github:defn/dev/pkg-velero-1.11.0-2?dir=m/pkg/velero;
   };
 
   outputs = inputs: inputs.kubectl.inputs.pkg.main rec {
