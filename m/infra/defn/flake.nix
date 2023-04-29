ece6f2b1683e25e53e6bbbe579c2c09eac47e936
amanibhavam <iam@defn.sh>
2023-04-29 08:07:10 +0000

    no flake for infra/defn

--- m/infra/defn/flake.nix
+++
@@ -1,8 +0,0 @@
-{
-  inputs.infra.url = github:devn/dev/cmd-infra-0.0.30?dir=m/cmd/infra;
-  outputs = inputs: inputs.infra.inputs.app.cdktfMain rec {
-    src = ./.;
-    infra = inputs.infra;
-    infra_cli = "infra";
-  };
-}
