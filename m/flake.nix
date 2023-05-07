147fcac434fa051f4a2258bd3a07c98f150822f3
amanibhavam <iam@defn.sh>
2023-05-07 02:47:46 +0000

    upgrade kubectl, flyctl

--- m/flake.nix
+++ m/flake.nix
@@ -4,8 +4,8 @@
     nodedev.url = github:defn/dev/pkg-nodedev-0.0.48?dir=m/pkg/nodedev;
     localdev.url = github:defn/dev/pkg-localdev-0.0.118?dir=m/pkg/localdev;
     development.url = github:defn/dev/pkg-development-0.0.54?dir=m/pkg/development;
-    cloud.url = github:defn/dev/pkg-cloud-0.0.78?dir=m/pkg/cloud;
-    kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.87?dir=m/pkg/kubernetes;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.79?dir=m/pkg/cloud;
+    kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.88?dir=m/pkg/kubernetes;
     tailscale.url = github:defn/dev/pkg-tailscale-1.40.0-2?dir=m/pkg/tailscale;
   };
 
