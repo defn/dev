6551f05bc72c74cb73b6952514db20be225eec75
amanibhavam <iam@defn.sh>
2023-05-08 23:39:24 +0000

    update deps

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     kubectl.url = github:defn/dev/pkg-kubectl-1.25.9-1?dir=m/pkg/kubectl;
     k3d.url = github:defn/dev/pkg-k3d-5.4.9-7?dir=m/pkg/k3d;
-    k9s.url = github:defn/dev/pkg-k9s-0.27.3-7?dir=m/pkg/k9s;
+    k9s.url = github:defn/dev/pkg-k9s-0.27.4-1?dir=m/pkg/k9s;
     helm.url = github:defn/dev/pkg-helm-3.11.3-5?dir=m/pkg/helm;
     kustomize.url = github:defn/dev/pkg-kustomize-5.0.2-1?dir=m/pkg/kustomize;
     stern.url = github:defn/dev/pkg-stern-1.25.0-6?dir=m/pkg/stern;
