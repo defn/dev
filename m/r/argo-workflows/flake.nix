a551491a07ce08ddf1d9600e014371dcaa0fdbe4
amanibhavam <iam@defn.sh>
2023-05-16 06:22:58 +0000

    upgrade home deps

--- m/r/argo-workflows/flake.nix
+++ m/r/argo-workflows/flake.nix
@@ -1,4 +1,4 @@
 {
-  inputs.app.url = github:defn/dev/app-app-0.0.48?dir=m/app/app;
+  inputs.app.url = github:defn/dev/app-app-0.0.49?dir=m/app/app;
   outputs = inputs: inputs.app.kustomizeMain rec { src = ./.; };
 }
