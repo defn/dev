f118510e1084b82db1fd8f786fd06aa7ded00f49
amanibhavam <iam@defn.sh>
2023-05-17 00:43:55 +0000

    flyctl/ 0.1.4 -> 0.1.6

--- m/pkg/flyctl/flake.nix
+++ m/pkg/flyctl/flake.nix
@@ -17,22 +17,22 @@
       "x86_64-linux" = {
         os = "Linux";
         arch = "x86_64";
-        sha256 = "sha256-zmf+KiNW0gmEcZd+ZuVfGJn9J3Nen/T+r+l7xQy+aJs="; # x86_64-linux
+        sha256 = "sha256-z0BZ0oFZQU4QmEgOd+GA7OoXFTIEzBVb8NXS8SqPm7g="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "Linux";
         arch = "arm64";
-        sha256 = "sha256-x3RyTbmIb6a98BqkmzFdlJXywFrleHBqXVWVkrO7Z1Q="; # aarch64-linux
+        sha256 = "sha256-PidhILJP36qOKH7J/sx25OFHVnCiwEkmTuVRMofCTPw="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "macOS";
         arch = "x86_64";
-        sha256 = "sha256-sFnhMm64MSBkI/h8GYmOptxanPp79WOEi+7J2ZhvHyU="; # x86_64-darwin
+        sha256 = "sha256-2+EPktXcCAw1s8CCHoN76qHHo8lB1SvTDQR6QcgFY+E="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "macOS";
         arch = "arm64";
-        sha256 = "sha256-sg8TLuhnhCm7UufLwltbyG+T7dFJq6rkNWb6M7cmfQY="; # aarch64-darwin
+        sha256 = "sha256-4aoVbedEDsB4R2aWGimu2oSKInP9tfAe09ccC7toBcU="; # aarch64-darwin
       };
     };
   };
