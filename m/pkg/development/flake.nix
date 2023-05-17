cd2b979b5d3746d50290700a3047c198ab21c2d9
amanibhavam <iam@defn.sh>
2023-05-17 00:54:32 +0000

    update deps

--- m/pkg/development/flake.nix
+++ m/pkg/development/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     ibazel.url = github:defn/dev/pkg-ibazel-0.22.0-6?dir=m/pkg/ibazel;
     buildifier.url = github:defn/dev/pkg-buildifier-6.1.2-1?dir=m/pkg/buildifier;
-    bazelisk.url = github:defn/dev/pkg-bazelisk-1.16.0-1?dir=m/pkg/bazelisk;
+    bazelisk.url = github:defn/dev/pkg-bazelisk-1.17.0-1?dir=m/pkg/bazelisk;
   };
 
   outputs = inputs: inputs.ibazel.inputs.pkg.main rec {
