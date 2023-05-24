e4d0b2529cb210757bc87a25da69ca98cd278331
amanibhavam <iam@defn.sh>
2023-05-24 01:56:04 +0000

    update pkg deps

--- m/pkg/localdev/flake.nix
+++ m/pkg/localdev/flake.nix
@@ -5,7 +5,7 @@
     boundary.url = github:defn/dev/pkg-boundary-0.12.2-7?dir=m/pkg/boundary;
     vault.url = github:defn/dev/pkg-vault-1.13.2-2?dir=m/pkg/vault;
     gh.url = github:defn/dev/pkg-gh-2.29.0-1?dir=m/pkg/gh;
-    earthly.url = github:defn/dev/pkg-earthly-0.7.5-1?dir=m/pkg/earthly;
+    earthly.url = github:defn/dev/pkg-earthly-0.7.6-1?dir=m/pkg/earthly;
     buildkite.url = github:defn/dev/pkg-buildkite-3.46.1-1?dir=m/pkg/buildkite;
     bk.url = github:defn/dev/pkg-bk-2.0.0-8?dir=m/pkg/bk;
     buildevents.url = github:defn/dev/pkg-buildevents-0.13.0-7?dir=m/pkg/buildevents;
