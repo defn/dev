f3ac3c574030cbc69bf64bae2ddc2655769eb685
amanibhavam <iam@defn.sh>
2023-05-25 02:48:37 +0000

    update pkg deps

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -7,7 +7,7 @@
     kustomize.url = github:defn/dev/pkg-kustomize-5.0.3-1?dir=m/pkg/kustomize;
     stern.url = github:defn/dev/pkg-stern-1.25.0-6?dir=m/pkg/stern;
     argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.4.7-5?dir=m/pkg/argoworkflows;
-    argocd.url = github:defn/dev/pkg-argocd-2.7.2-1?dir=m/pkg/argocd;
+    argocd.url = github:defn/dev/pkg-argocd-2.7.3-1?dir=m/pkg/argocd;
     kn.url = github:defn/dev/pkg-kn-1.10.0-2?dir=m/pkg/kn;
     vcluster.url = github:defn/dev/pkg-vcluster-0.15.0-6?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-0.59.0-7?dir=m/pkg/kubevirt;
