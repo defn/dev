c004443749c3b7a1047bbd084642c4f53a4c8aa2
amanibhavam <iam@defn.sh>
2023-05-26 00:45:17 +0000

    update m deps

--- m/flake.nix
+++ m/flake.nix
@@ -4,8 +4,8 @@
     nodedev.url = github:defn/dev/pkg-nodedev-0.0.48?dir=m/pkg/nodedev;
     localdev.url = github:defn/dev/pkg-localdev-0.0.125?dir=m/pkg/localdev;
     development.url = github:defn/dev/pkg-development-0.0.55?dir=m/pkg/development;
-    cloud.url = github:defn/dev/pkg-cloud-0.0.91?dir=m/pkg/cloud;
-    kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.97?dir=m/pkg/kubernetes;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.92?dir=m/pkg/cloud;
+    kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.98?dir=m/pkg/kubernetes;
     tailscale.url = github:defn/dev/pkg-tailscale-1.42.0-1?dir=m/pkg/tailscale;
   };
 
