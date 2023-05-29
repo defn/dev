df75a0817d887d5da36e1b65c633595b269d42b0
amanibhavam <iam@defn.sh>
2023-05-29 04:36:01 +0000

    upgrade deps

--- m/r/meh/flake.nix
+++ m/r/meh/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    argo-cd.url = github:defn/dev/r-argo-cd-0.0.30?dir=m/r/argo-cd;
-    argo-workflows.url = github:defn/dev/r-argo-workflows-0.0.23?dir=m/r/argo-workflows;
+    argo-cd.url = github:defn/dev/r-argo-cd-0.0.31?dir=m/r/argo-cd;
+    argo-workflows.url = github:defn/dev/r-argo-workflows-0.0.24?dir=m/r/argo-workflows;
   };
 
   outputs = inputs: inputs.argo-cd.inputs.app.pkg.main rec {
