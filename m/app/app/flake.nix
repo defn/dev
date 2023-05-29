cc16dfe40acd6ee28503506c639b21aac4fd6d35
amanibhavam <iam@defn.sh>
2023-05-29 01:53:15 +0000

    update m deps

--- m/app/app/flake.nix
+++ m/app/app/flake.nix
@@ -3,7 +3,7 @@
     gomod2nix.url = github:defn/gomod2nix/1.5.0-9;
     godev.url = github:defn/dev/pkg-godev-0.0.82?dir=m/pkg/godev;
     nodedev.url = github:defn/dev/pkg-nodedev-0.0.48?dir=m/pkg/nodedev;
-    cloud.url = github:defn/dev/pkg-cloud-0.0.93?dir=m/pkg/cloud;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.94?dir=m/pkg/cloud;
     kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.99?dir=m/pkg/kubernetes;
     localdev.url = github:defn/dev/pkg-localdev-0.0.125?dir=m/pkg/localdev;
     latest.url = github:NixOS/nixpkgs?rev=64c27498901f104a11df646278c4e5c9f4d642db;
