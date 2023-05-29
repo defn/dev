726fb797c45b66113c0b89a7aa6a1b85f8515dbb
amanibhavam <iam@defn.sh>
2023-05-29 04:04:47 +0000

    upgrade inputs

--- m/pkg/cloud/flake.nix
+++ m/pkg/cloud/flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    terraform.url = github:defn/dev/pkg-terraform-1.4.6-2?dir=m/pkg/terraform;
-    packer.url = github:defn/dev/pkg-packer-1.8.7-1?dir=m/pkg/packer;
-    step.url = github:defn/dev/pkg-step-0.24.4-1?dir=m/pkg/step;
-    awscli.url = github:defn/dev/pkg-awscli-2.11.23-1?dir=m/pkg/awscli;
-    flyctl.url = github:defn/dev/pkg-flyctl-0.1.20-1?dir=m/pkg/flyctl;
-    chamber.url = github:defn/dev/pkg-chamber-2.13.0-1?dir=m/pkg/chamber;
+    terraform.url = github:defn/dev/pkg-terraform-1.4.6-3?dir=m/pkg/terraform;
+    packer.url = github:defn/dev/pkg-packer-1.8.7-2?dir=m/pkg/packer;
+    step.url = github:defn/dev/pkg-step-0.24.4-2?dir=m/pkg/step;
+    awscli.url = github:defn/dev/pkg-awscli-2.11.23-2?dir=m/pkg/awscli;
+    flyctl.url = github:defn/dev/pkg-flyctl-0.1.20-2?dir=m/pkg/flyctl;
+    chamber.url = github:defn/dev/pkg-chamber-2.13.0-2?dir=m/pkg/chamber;
   };
 
   outputs = inputs: inputs.terraform.inputs.pkg.main rec {
