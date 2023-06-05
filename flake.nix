4333c9cdd2cd734bc04defea44d32bc9e68afb1d
amanibhavam <iam@defn.sh>
2023-06-05 22:18:23 +0000

    update home deps

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    az.url = github:defn/dev/pkg-az-0.0.125?dir=m/pkg/az;
+    az.url = github:defn/dev/pkg-az-0.0.127?dir=m/pkg/az;
     oci.url = github:defn/dev/pkg-oci-0.0.49?dir=m/pkg/oci;
     nix.url = github:defn/dev/pkg-nix-0.0.48?dir=m/pkg/nix;
     secrets.url = github:defn/dev/pkg-secrets-0.0.51?dir=m/pkg/secrets;
@@ -10,7 +10,7 @@
     acme.url = github:defn/dev/pkg-acme-3.0.5-13?dir=m/pkg/acme;
     tailscale.url = github:defn/dev/pkg-tailscale-1.42.0-3?dir=m/pkg/tailscale;
     development.url = github:defn/dev/pkg-development-0.0.58?dir=m/pkg/development;
-    localdev.url = github:defn/dev/pkg-localdev-0.0.130?dir=m/pkg/localdev;
+    localdev.url = github:defn/dev/pkg-localdev-0.0.131?dir=m/pkg/localdev;
     cloud.url = github:defn/dev/pkg-cloud-0.0.100?dir=m/pkg/cloud;
     shell.url = github:defn/dev/pkg-shell-0.0.49?dir=m/pkg/shell;
   };
