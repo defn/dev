b346f3b090b9d93f2db01afee12e78fc10ba143b
amanibhavam <iam@defn.sh>
2023-06-05 05:51:49 +0000

    build aws config

---
+++ m/meh/aws_config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -exfu
+
+cat "$1"| while read -r org url region; do 
+    cat "$3" | perl -pe "s{ORG}{$org}g; s{REGION}{$region}g"
+    cat "$2" | while read -r org2 member account_id; do 
+        if [[ "$org2" == "$org" ]]; then 
+            cat "$4" | perl -pe "s{ORG}{$org}g; s{MEMBER}{$member}g; s{REGION}{$region}g; s{URL}{$url}g; s{ACCOUNT_ID}{$account_id}g"
+        fi
+    done
+done > "$5.tmp"
+
+mv -f "$5.tmp" "$5"
