2f495145f832c3c9267a39b253f720d5b2a7d5f7
amanibhavam <iam@defn.sh>
2023-06-05 07:08:35 +0000

    more wrppers

--- m/meh/aws_wrapper.sh
+++ m/meh/aws_wrapper.sh
@@ -10,8 +10,26 @@ cat "$2" | while read -r org acc id; do
 #!/bin/bash
 
 export AWS_CONFIG_FILE="$1"
-export AWS_PROFILE="$org-$acc"
+export AWS_PROFILE="$org-$acc-sso"
 exec aws "\$@"
+EOF
+    chmod 755 "$exe"
+
+    exe="$3/$org-$acc-console"
+    cat <<EOF > "$exe"
+#!/bin/bash
+
+export AWS_CONFIG_FILE="$1"
+exec aws-vault login "$org-$acc-sso-source"
+EOF
+    chmod 755 "$exe"
+
+    exe="$3/$org-$acc-exec"
+    cat <<EOF > "$exe"
+#!/bin/bash
+
+export AWS_CONFIG_FILE="$1"
+exec aws-vault exec "$org-$acc-sso-source"
 EOF
     chmod 755 "$exe"
 done
