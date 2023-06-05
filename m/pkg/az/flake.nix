06cbd4fd943e032f22b0dbedac2b8270b11c68e5
amanibhavam <iam@defn.sh>
2023-06-05 03:23:26 +0000

    remove hof, add direnv

--- m/pkg/az/flake.nix
+++ m/pkg/az/flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     cue.url = github:defn/dev/pkg-cue-0.5.0-7?dir=m/pkg/cue;
-    hof.url = github:defn/dev/pkg-hof-0.6.8-beta.13-5?dir=m/pkg/hof;
     gum.url = github:defn/dev/pkg-gum-0.10.0-9?dir=m/pkg/gum;
     glow.url = github:defn/dev/pkg-glow-1.5.1-3?dir=m/pkg/glow;
   };
@@ -16,11 +15,11 @@
         inputs.gum.defaultPackage.${ctx.system}
         inputs.glow.defaultPackage.${ctx.system}
         inputs.cue.defaultPackage.${ctx.system}
-        inputs.hof.defaultPackage.${ctx.system}
         jq
         yq
         gron
         fzf
+        direnv
         bashInteractive
       ];
 
