d2e7d53634b583ea4a41ed27d60883270ec8db73
amanibhavam <iam@defn.sh>
2023-06-08 03:13:57 +0000

    update home deps

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   inputs = {
-    az.url = github:defn/dev/pkg-az-0.0.127?dir=m/pkg/az;
+    az.url = github:defn/dev/pkg-az-0.0.128?dir=m/pkg/az;
     oci.url = github:defn/dev/pkg-oci-0.0.49?dir=m/pkg/oci;
     nix.url = github:defn/dev/pkg-nix-0.0.48?dir=m/pkg/nix;
     secrets.url = github:defn/dev/pkg-secrets-0.0.51?dir=m/pkg/secrets;
     utils.url = github:defn/dev/pkg-utils-0.0.49?dir=m/pkg/utils;
     vpn.url = github:defn/dev/pkg-vpn-0.0.50?dir=m/pkg/vpn;
-    vault.url = github:defn/dev/pkg-vault-1.13.2-4?dir=m/pkg/vault;
+    vault.url = github:defn/dev/pkg-vault-1.13.3-1?dir=m/pkg/vault;
     acme.url = github:defn/dev/pkg-acme-3.0.5-13?dir=m/pkg/acme;
     tailscale.url = github:defn/dev/pkg-tailscale-1.42.0-3?dir=m/pkg/tailscale;
     development.url = github:defn/dev/pkg-development-0.0.58?dir=m/pkg/development;
-    localdev.url = github:defn/dev/pkg-localdev-0.0.131?dir=m/pkg/localdev;
-    cloud.url = github:defn/dev/pkg-cloud-0.0.100?dir=m/pkg/cloud;
+    localdev.url = github:defn/dev/pkg-localdev-0.0.132?dir=m/pkg/localdev;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.101?dir=m/pkg/cloud;
     shell.url = github:defn/dev/pkg-shell-0.0.49?dir=m/pkg/shell;
   };
 
