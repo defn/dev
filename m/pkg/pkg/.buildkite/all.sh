b2618155b0a8d7474c0570ffc85db008636b9f3f
amanibhavam <iam@defn.sh>
2023-06-09 19:51:46 +0000

    bleh

--- m/pkg/pkg/.buildkite/all.sh
+++ m/pkg/pkg/.buildkite/all.sh
@@ -10,13 +10,13 @@ function main {
 	echo "steps:"
 
 	for D in "$@"; do
-		case "$D" in
+		case "${D}" in
 		*/all/)
 			true
 			;;
 		*)
-			if test -d "$D/.buildkite"; then
-				echo -e "  - agents:\n      queue: $GIT_AUTHOR_NAME\n    command: \"cat $D/.buildkite/pipeline.yml | sed 's#command: .#agents:\\\\\\\n      queue: $GIT_AUTHOR_NAME\\\\\\\n    command: \\\"cd $D; #' | buildkite-agent pipeline upload\""
+			if test -d "${D}/.buildkite"; then
+				echo -e "  - agents:\n      queue: ${GIT_AUTHOR_NAME}\n    command: \"cat ${D}/.buildkite/pipeline.yml | sed 's#command: .#agents:\\\\\\\n      queue: ${GIT_AUTHOR_NAME}\\\\\\\n    command: \\\"cd ${D}; #' | buildkite-agent pipeline upload\""
 			fi
 			;;
 		esac
