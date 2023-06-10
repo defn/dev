311d015343261f575bd2d99d6cd1fa491fe17db0
amanibhavam <iam@defn.sh>
2023-06-10 02:11:24 +0000

    no ibazel

--- m/flake.nix
+++ m/flake.nix
@@ -3,7 +3,7 @@
     godev.url = github:defn/dev/pkg-godev-0.0.88?dir=m/pkg/godev;
     nodedev.url = github:defn/dev/pkg-nodedev-0.0.51?dir=m/pkg/nodedev;
     localdev.url = github:defn/dev/pkg-localdev-0.0.132?dir=m/pkg/localdev;
-    development.url = github:defn/dev/pkg-development-0.0.58?dir=m/pkg/development;
+    development.url = github:defn/dev/pkg-development-0.0.59?dir=m/pkg/development;
     cloud.url = github:defn/dev/pkg-cloud-0.0.104?dir=m/pkg/cloud;
     kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.107?dir=m/pkg/kubernetes;
     tailscale.url = github:defn/dev/pkg-tailscale-1.42.0-3?dir=m/pkg/tailscale;
