a77bd313dcfd6f446e79dff5024be5f0a3a1511d
amanibhavam <iam@defn.sh>
2023-06-10 20:09:15 +0000

    try go build

--- m/command/infra.go
+++ m/command/infra.go
@@ -76,13 +76,8 @@ type AwsProps struct {
 // infraCmd represents the infra command
 var infraCmd = &cobra.Command{
 	Use:   "infra",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generates Terraform configs from CUE",
+	Long:  `Generates Terraform configs from CUE.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		aws_props := LoadUserAwsProps()
 
