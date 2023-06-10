02ab139a41d88f28a6f14e9f928fb38d8e39f265
amanibhavam <iam@defn.sh>
2023-06-10 02:07:22 +0000

    buh-bye ibazel

--- m/pkg/development/flake.nix
+++ m/pkg/development/flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    ibazel.url = github:defn/dev/pkg-ibazel-0.22.0-8?dir=m/pkg/ibazel;
     buildifier.url = github:defn/dev/pkg-buildifier-6.1.2-3?dir=m/pkg/buildifier;
     bazelisk.url = github:defn/dev/pkg-bazelisk-1.17.0-3?dir=m/pkg/bazelisk;
   };
@@ -15,7 +14,6 @@
           git
           git-lfs
           inputs.bazelisk.defaultPackage.${ctx.system}
-          inputs.ibazel.defaultPackage.${ctx.system}
           inputs.buildifier.defaultPackage.${ctx.system}
         ];
     };
