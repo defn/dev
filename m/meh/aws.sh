8f95f4ffc956dc56eda3e16a0513ecf9f2da976d
amanibhavam <iam@defn.sh>
2023-06-12 04:39:07 +0000

    supply as clil version

--- m/meh/aws.sh
+++ m/meh/aws.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
 function main {
+	local aws_version
 	local aws_config
 	local aws_profile
 
+	aws_version="$1"
+	shift
+
 	aws_config="$1"
 	shift
+
 	aws_profile="$1"
 	shift
 
-	exec env AWS_CONFIG="${aws_config}" AWS_PROFILE="${aws_profile}" nix run --quiet --quiet --quiet 'github:defn/dev/pkg-awscli-2.11.26-4?dir=m/pkg/awscli' -- "$@"
+	exec env AWS_CONFIG="${aws_config}" AWS_PROFILE="${aws_profile}" nix run --quiet --quiet --quiet "github:defn/dev/pkg-awscli-${aws_version}?dir=m/pkg/awscli" -- "$@"
 }
 
 main "$@"
