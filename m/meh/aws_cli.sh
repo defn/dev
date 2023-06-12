b94393ccee28c54e07e3bd486d3a49f221f52b9d
amanibhavam <iam@defn.sh>
2023-06-12 05:47:59 +0000

    pass json config to program

--- m/meh/aws_cli.sh
+++ m/meh/aws_cli.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function main {
-	local aws_version
+	local config
 	local aws_config
 	local aws_profile
 
-	aws_version="$1"
+	config="$1"
 	shift
 
 	aws_config="$1"
@@ -13,6 +13,9 @@ function main {
 
 	aws_profile="$1"
 	shift
+
+	local aws_version
+	aws_version="$(jq -r .version.aws <"${config}")"
 
 	exec env AWS_CONFIG="${aws_config}" AWS_PROFILE="${aws_profile}" nix run --quiet --quiet --quiet "github:defn/dev/pkg-awscli-${aws_version}?dir=m/pkg/awscli" -- "$@"
 }
