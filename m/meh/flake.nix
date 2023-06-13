5959ce8a3d0944d746115ca5e9c6db3bd9644cfe
amanibhavam <iam@defn.sh>
2023-06-13 08:00:59 +0000

    earthly builds

--- m/meh/flake.nix
+++ m/meh/flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     cue.url = github:defn/dev/pkg-cue-0.5.0-7?dir=m/pkg/cue;
     awscli.url = github:defn/dev/pkg-awscli-2.11.27-1?dir=m/pkg/awscli;
+    earthly.url = github:defn/dev/pkg-earthly-0.7.8-1?dir=m/pkg/earthly;
   };
 
   outputs = inputs: inputs.cue.inputs.pkg.main rec {
@@ -19,6 +20,7 @@
       propagatedBuildInputs = [
         inputs.cue.defaultPackage.${ctx.system}
         inputs.awscli.defaultPackage.${ctx.system}
+        inputs.earthly.defaultPackage.${ctx.system}
       ];
     };
   };
