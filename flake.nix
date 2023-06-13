022473142dec8eb6ddbe72f3cef11f0a09e52730
amanibhavam <iam@defn.sh>
2023-06-13 22:09:58 +0000

    update home deps

--- flake.nix
+++ flake.nix
@@ -10,8 +10,8 @@
     acme.url = github:defn/dev/pkg-acme-3.0.6-1?dir=m/pkg/acme;
     tailscale.url = github:defn/dev/pkg-tailscale-1.42.0-3?dir=m/pkg/tailscale;
     development.url = github:defn/dev/pkg-development-0.0.59?dir=m/pkg/development;
-    localdev.url = github:defn/dev/pkg-localdev-0.0.132?dir=m/pkg/localdev;
-    cloud.url = github:defn/dev/pkg-cloud-0.0.105?dir=m/pkg/cloud;
+    localdev.url = github:defn/dev/pkg-localdev-0.0.133?dir=m/pkg/localdev;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.106?dir=m/pkg/cloud;
     shell.url = github:defn/dev/pkg-shell-0.0.49?dir=m/pkg/shell;
   };
 
