da1057ecda8d0d6af497e44bfe2f4f0bbfa7ac67
amanibhavam <iam@defn.sh>
2023-06-13 22:55:22 +0000

    generate wrappers from aws_accounts.txt

--- m/meh/wrappers.sh
+++ m/meh/wrappers.sh
@@ -4,7 +4,7 @@ set -efuo pipefail
 
 function main {
 	# shellcheck disable=SC2016
-	grep profile ~/.aws/config | awk '/sso-source/ {print $2}' | cut -d- -f1-2 | grep -v sso | runmany 4 'bazel run --script_path=bin/$1 //meh:aws_cli $1'
+	awk '/^[a-z]/ {print $1 "-" $2}' aws_accounts.txt | runmany 4 'bazel run --script_path=bin/$1 //meh:aws_cli $1'
 }
 
 main "$@"
