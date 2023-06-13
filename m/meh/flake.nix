24bdf33215f8e2b5dc3ea61a38ab8959944b5f14
amanibhavam <iam@defn.sh>
2023-06-13 07:17:48 +0000

    use nix for aws cli

--- m/meh/flake.nix
+++ m/meh/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     cue.url = github:defn/dev/pkg-cue-0.5.0-7?dir=m/pkg/cue;
+    awscli.url = github:defn/dev/pkg-awscli-2.11.27-1?dir=m/pkg/awscli;
   };
 
   outputs = inputs: inputs.cue.inputs.pkg.main rec {
@@ -17,6 +18,7 @@
     defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs = [
         inputs.cue.defaultPackage.${ctx.system}
+        inputs.awscli.defaultPackage.${ctx.system}
       ];
     };
   };
