adf970d03e74a13fd6fad0c8415bd6a3e3d7dc93
amanibhavam <iam@defn.sh>
2023-06-15 04:17:02 +0000

    separate macros for earthly, cue

--- m/aws/aws_config.sh
+++ m/aws/aws_config.sh
@@ -2,14 +2,14 @@
 
 set -efu
 
-cat "$1" | while read -r org url region; do
-	cat "$3" | perl -pe "s{ORG}{${org}}g; s{REGION}{${region}}g"
-	cat "$2" | while read -r org2 member account_id; do
+while read -r org url region; do
+	perl -pe "s{ORG}{${org}}g; s{REGION}{${region}}g" <"$3"
+	while read -r org2 member account_id; do
 		if [[ ${org2} == "${org}" ]]; then
 			echo "aws config: ${org} ${member} ${account_id}" 1>&2
-			cat "$4" | perl -pe "s{ORG}{${org}}g; s{MEMBER}{${member}}g; s{REGION}{${region}}g; s{URL}{${url}}g; s{ACCOUNT_ID}{${account_id}}g"
+			perl -pe "s{ORG}{${org}}g; s{MEMBER}{${member}}g; s{REGION}{${region}}g; s{URL}{${url}}g; s{ACCOUNT_ID}{${account_id}}g" <"$4"
 		fi
-	done
-done >"$5.tmp"
+	done <"$2"
+done <"$1" >"$5.tmp"
 
 mv -f "$5.tmp" "$5"
