00e0665e0cd59f2d509cf207767cd82fb6d3cc17
amanibhavam <iam@defn.sh>
2023-06-16 07:46:02 +0000

    add oras

--- m/flake.nix
+++ m/flake.nix
@@ -2,9 +2,9 @@
   inputs = {
     godev.url = github:defn/dev/pkg-godev-0.0.88?dir=m/pkg/godev;
     nodedev.url = github:defn/dev/pkg-nodedev-0.0.51?dir=m/pkg/nodedev;
-    localdev.url = github:defn/dev/pkg-localdev-0.0.134?dir=m/pkg/localdev;
+    localdev.url = github:defn/dev/pkg-localdev-0.0.135?dir=m/pkg/localdev;
     development.url = github:defn/dev/pkg-development-0.0.59?dir=m/pkg/development;
-    cloud.url = github:defn/dev/pkg-cloud-0.0.107?dir=m/pkg/cloud;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.108?dir=m/pkg/cloud;
     kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.108?dir=m/pkg/kubernetes;
     tailscale.url = github:defn/dev/pkg-tailscale-1.42.0-3?dir=m/pkg/tailscale;
   };
