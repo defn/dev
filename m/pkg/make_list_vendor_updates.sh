9d99f5855b91fb1adca5091aabc1708ccda3237f
amanibhavam <iam@defn.sh>
2023-06-22 08:32:51 +0000

    show url

--- m/pkg/make_list_vendor_updates.sh
+++ m/pkg/make_list_vendor_updates.sh
@@ -8,12 +8,14 @@ function main {
 		if grep ghrepo "${a}/flake.json" >/dev/null 2>/dev/null; then
 			(
 				cd "${a}"
-				echo "${a} $(jq -r '.vendor' <flake.json || true) $(../az/bin/n-latest || true)"
+				echo "${a} $(jq -r '.vendor' <flake.json || true) $(../az/bin/n-latest || true)" | while read -r slug current latest real_tag; do
+					echo "${slug} ${current} ${latest} https://github.com/$(jq -r .ghrepo <flake.json)/releases/tag/${real_tag}"
+				done
 			)
 		fi
-	done | while read -r slug current latest; do
+	done | while read -r slug current latest real_tag; do
 		if [[ ${current} != "${latest}" ]]; then
-			echo "${slug} ${current} ${latest}"
+			echo "${slug} ${current} ${latest} ${real_tag}"
 		fi
 	done | (grep -E -v 'kustomize/ 5.0.3 5.1.0' || true)
 }
