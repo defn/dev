cd0ebc76824cb9629f920e9471066026fa4a1c32
amanibhavam <iam@defn.sh>
2023-06-22 09:26:59 +0000

    update pkg deps

--- m/pkg/localdev/flake.nix
+++ m/pkg/localdev/flake.nix
@@ -3,17 +3,17 @@
     tilt.url = github:defn/dev/pkg-tilt-0.32.4-3?dir=m/pkg/tilt;
     nomad.url = github:defn/dev/pkg-nomad-1.5.6-3?dir=m/pkg/nomad;
     boundary.url = github:defn/dev/pkg-boundary-0.13.0-1?dir=m/pkg/boundary;
-    vault.url = github:defn/dev/pkg-vault-1.13.3-1?dir=m/pkg/vault;
-    gh.url = github:defn/dev/pkg-gh-2.30.0-1?dir=m/pkg/gh;
+    vault.url = github:defn/dev/pkg-vault-1.14.0-1?dir=m/pkg/vault;
+    gh.url = github:defn/dev/pkg-gh-2.31.0-1?dir=m/pkg/gh;
     earthly.url = github:defn/dev/pkg-earthly-0.7.8-1?dir=m/pkg/earthly;
     oras.url = github:defn/dev/pkg-oras-1.0.0-2?dir=m/pkg/oras;
-    buildkite.url = github:defn/dev/pkg-buildkite-3.48.0-1?dir=m/pkg/buildkite;
+    buildkite.url = github:defn/dev/pkg-buildkite-3.49.0-1?dir=m/pkg/buildkite;
     bk.url = github:defn/dev/pkg-bk-2.0.0-10?dir=m/pkg/bk;
     buildevents.url = github:defn/dev/pkg-buildevents-0.13.0-9?dir=m/pkg/buildevents;
     honeyvent.url = github:defn/dev/pkg-honeyvent-1.1.3-9?dir=m/pkg/honeyvent;
     honeymarker.url = github:defn/dev/pkg-honeymarker-0.2.10-9?dir=m/pkg/honeymarker;
     honeytail.url = github:defn/dev/pkg-honeytail-1.8.3-7?dir=m/pkg/honeytail;
-    kuma.url = github:defn/dev/pkg-kuma-2.2.1-3?dir=m/pkg/kuma;
+    kuma.url = github:defn/dev/pkg-kuma-2.2.2-1?dir=m/pkg/kuma;
   };
 
   outputs = inputs: inputs.tilt.inputs.pkg.main rec {
