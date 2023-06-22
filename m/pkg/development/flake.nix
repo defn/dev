f0b27c34b832daa2256f3620a8e6673870f71cff
amanibhavam <iam@defn.sh>
2023-06-22 08:04:05 +0000

    upgrade ibazel

--- m/pkg/development/flake.nix
+++ m/pkg/development/flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     buildifier.url = github:defn/dev/pkg-buildifier-6.1.2-3?dir=m/pkg/buildifier;
     bazelisk.url = github:defn/dev/pkg-bazelisk-1.17.0-3?dir=m/pkg/bazelisk;
+    ibazel.url = github:defn/dev/pkg-ibazel-0.22.0-8?dir=m/pkg/ibazel;
   };
 
   outputs = inputs: inputs.buildifier.inputs.pkg.main rec {
@@ -15,6 +16,7 @@
           git-lfs
           inputs.bazelisk.defaultPackage.${ctx.system}
           inputs.buildifier.defaultPackage.${ctx.system}
+          inputs.ibazel.defaultPackage.${ctx.system}
         ];
     };
   };
