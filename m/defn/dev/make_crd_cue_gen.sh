898d72bf345bff0169f611599f6654d6d43ed32f
amanibhavam <iam@defn.sh>
2023-06-25 09:51:45 +0000

    clean up

--- m/defn/dev/make_crd_cue_gen.sh
+++ m/defn/dev/make_crd_cue_gen.sh
@@ -5,6 +5,7 @@ set -eufo pipefail
 function main {
 	go get istio.io/tools/cmd/cue-gen
 	go install istio.io/tools/cmd/cue-gen
+	go mod tidy
 	"$(go env GOPATH)/bin/cue-gen" -f=cue.yaml --crd=true
 }
 
