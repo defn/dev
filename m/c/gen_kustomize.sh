13a342236c1f7024de73f7d4e24b7b29173f0c47
amanibhavam <iam@defn.sh>
2023-06-26 04:38:37 +0000

    generate ksutoize

---
+++ m/c/gen_kustomize.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function main {
+	local app
+	local out
+
+	app="$1"
+	shift
+	out="$1"
+	shift
+
+	cat "${app}" | jq -r '.gen | to_entries[] | .key as $dir | .value | to_entries[] | "\($dir)/\(.key) \(.value | @base64)"' | while read -r fname content; do
+		mkdir -p "${out}/${fname%/*}"
+		echo "${content}" | base64 -d >"${out}/${fname}"
+	done
+}
+
+main "$@"
