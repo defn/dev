e8136f2f24db1efb3b09c6f6f08e636bb7d9a629
amanibhavam <iam@defn.sh>
2023-06-27 05:51:08 +0000

    fixtrunk

--- m/aws/aws_config.sh
+++ m/aws/aws_config.sh
@@ -9,7 +9,7 @@ while read -r org url region; do
 			echo "aws config: ${org} ${member} ${account_id}" 1>&2
 			perl -pe "s{ORG}{${org}}g; s{MEMBER}{${member}}g; s{REGION}{${region}}g; s{URL}{${url}}g; s{ACCOUNT_ID}{${account_id}}g" <"$3"
 		fi
-	done < <(jq -r '.org | . as $org | keys[] | . as $o | $org[$o].account | keys[] | . as $member | "\($o) \($org[$o].account[$member].account) \($org[$o].account[$member].id)"' <"$1")
-done < <(jq -r '.org | . as $org | keys[] | "\(.) \($org[.].url) \($org[.].region)"' <"$1") >"$4.tmp"
+	done < <(jq -r '.org | . as $org | keys[] | . as $o | $org[$o].account | keys[] | . as $member | "\($o) \($org[$o].account[$member].account) \($org[$o].account[$member].id)"' <"$1" || true)
+done < <(jq -r '.org | . as $org | keys[] | "\(.) \($org[.].url) \($org[.].region)"' <"$1" || true) >"$4.tmp"
 
 mv -f "$4.tmp" "$4"
