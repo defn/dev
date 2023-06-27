effb9de79efb549f6154849a6f677ffed60a9302
amanibhavam <iam@defn.sh>
2023-06-27 05:27:56 +0000

    generate aws configs from cue

--- m/aws/aws_config.sh
+++ m/aws/aws_config.sh
@@ -3,13 +3,13 @@
 set -efu
 
 while read -r org url region; do
-	perl -pe "s{ORG}{${org}}g; s{REGION}{${region}}g" <"$3"
+	perl -pe "s{ORG}{${org}}g; s{REGION}{${region}}g" <"$2"
 	while read -r org2 member account_id; do
 		if [[ ${org2} == "${org}" ]]; then
 			echo "aws config: ${org} ${member} ${account_id}" 1>&2
-			perl -pe "s{ORG}{${org}}g; s{MEMBER}{${member}}g; s{REGION}{${region}}g; s{URL}{${url}}g; s{ACCOUNT_ID}{${account_id}}g" <"$4"
+			perl -pe "s{ORG}{${org}}g; s{MEMBER}{${member}}g; s{REGION}{${region}}g; s{URL}{${url}}g; s{ACCOUNT_ID}{${account_id}}g" <"$3"
 		fi
-	done <"$2"
-done <"$1" >"$5.tmp"
+	done < <(jq -r '.org | . as $org | keys[] | . as $o | $org[$o].account | keys[] | . as $member | "\($o) \($org[$o].account[$member].account) \($org[$o].account[$member].id)"' <"$1")
+done < <(jq -r '.org | . as $org | keys[] | "\(.) \($org[.].url) \($org[.].region)"' <"$1") >"$4.tmp"
 
-mv -f "$5.tmp" "$5"
+mv -f "$4.tmp" "$4"
