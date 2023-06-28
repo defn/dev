4d9ec17ba2b013e70613155f07fe167cd7070019
defnn <yk-22610028@defn.sh>
2023-06-28 09:56:27 -0700

    fix zellij

--- m/pkg/shell/flake.nix
+++ m/pkg/shell/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.10?dir=m/pkg/pkg;
-    zellij.url = github:defn/dev/pkg-zellij-0.37.2-1?dir=m/pkg/zellij;
+    zellij.url = github:defn/dev/pkg-zellij-0.37.2-2?dir=m/pkg/zellij;
   };
 
   outputs = inputs: inputs.pkg.main rec {
