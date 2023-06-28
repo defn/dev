770ca5fd5495a8e43123979f3b5b5561f9dda330
defnn <yk-22610028@defn.sh>
2023-06-28 03:28:05 +0000

    add zellij

--- m/pkg/shell/flake.nix
+++ m/pkg/shell/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.10?dir=m/pkg/pkg;
+    zellij.url = github:defn/dev/pkg-zellij-0.37.2-1?dir=m/pkg/zellij;
   };
 
   outputs = inputs: inputs.pkg.main rec {
@@ -8,6 +9,7 @@
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs = with ctx.pkgs; [
+        inputs.zellij.defaultPackage.${ctx.system}
         vim
         openssh
         screen
