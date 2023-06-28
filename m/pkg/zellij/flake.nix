ddeb0aa5acea424751670eb64228c92c49b15736
defnn <yk-22610028@defn.sh>
2023-06-28 03:25:23 +0000

    zellij

---
+++ m/pkg/zellij/flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs.pkg.url = github:defn/dev/pkg-pkg-0.0.10?dir=m/pkg/pkg;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+
+    url_template = input:
+      if input.os == "darwin" then
+        "https://github.com/zellij-org/zellij/releases/download/v${input.vendor}/zellij-${input.arch}-apple-${input.os}.tar.gz"
+      else
+        "https://github.com/zellij-org/zellij/releases/download/v${input.vendor}/zellij-${input.arch}-unknown-${input.os}-musl.tar.gz";
+
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 zellij $out/bin/zellij
+    '';
+
+    downloads = {
+
+
+      "x86_64-linux" = {
+        os = "linux";
+        arch = "x86_64";
+        sha256 = "sha256-GZ2hrzAcmAjVF9WMaG0vwy94vkEvShifjnGV6IZTwck="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "aarch64";
+        sha256 = "sha256-wGkV+04feNTtD4BgijdEtfN2VxK2f6+Mez5BMfIlVGI="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "x86_64";
+        sha256 = "sha256-aTGOfTB0+Lx8cFmfj2k5ih9AvYTXCp2VdPAol5U82Pc="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "Darwin";
+        arch = "aarch64";
+        sha256 = "sha256-aaaaag/4VLrQcMNMl6y5TD0IC7Zdg8YV7TOJVLP2qhE="; # aarch64-darwin
+      };
+    };
+  };
+}
