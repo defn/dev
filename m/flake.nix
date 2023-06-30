6b7e5c3702234a9b815dc0a6f2ce16757a2933c8
defnn <yk-22610028@defn.sh>
2023-06-30 21:24:02 +0000

    update m deps

--- m/flake.nix
+++ m/flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    godev.url = github:defn/dev/pkg-godev-0.0.92?dir=m/pkg/godev;
+    godev.url = github:defn/dev/pkg-godev-0.0.93?dir=m/pkg/godev;
     nodedev.url = github:defn/dev/pkg-nodedev-0.0.51?dir=m/pkg/nodedev;
     localdev.url = github:defn/dev/pkg-localdev-0.0.140?dir=m/pkg/localdev;
     development.url = github:defn/dev/pkg-development-0.0.60?dir=m/pkg/development;
-    cloud.url = github:defn/dev/pkg-cloud-0.0.115?dir=m/pkg/cloud;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.116?dir=m/pkg/cloud;
     kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.114?dir=m/pkg/kubernetes;
     coder.url = github:defn/dev/pkg-coder-0.24.1-1?dir=m/pkg/coder;
     tailscale.url = github:defn/dev/pkg-tailscale-1.44.0-1?dir=m/pkg/tailscale;
