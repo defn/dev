50204a48528e276f4b0f3133216a7bde41041f9a
amanibhavam <iam@defn.sh>
2023-07-07 23:01:06 -0700

    pgrep to find coder.agent

--- m/coder/docker/main.tf
+++ m/coder/docker/main.tf
@@ -31,7 +31,7 @@ resource "coder_agent" "main" {
   startup_script         = <<-EOT
     set -e
 
-    sudo cp $(readlink -f /proc/1/cwd)/coder /usr/local/bin/
+    sudo cp $(readlink -f /proc/$(pgrep -f coder.agent)/cwd)/coder /usr/local/bin/
 
     cd
     ssh -o StrictHostKeyChecking=no git@github.com true || true
