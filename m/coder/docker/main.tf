15591d4b1b8ebdd73d0e9277997b14337f34cdfb
amanibhavam <iam@defn.sh>
2023-07-07 19:03:45 -0700

    split out

--- m/coder/docker/main.tf
+++ m/coder/docker/main.tf
@@ -12,17 +12,12 @@ terraform {
 }
 
 locals {
-  username = data.coder_workspace.me.owner
+  username = "ubuntu"
 }
 
-data "coder_provisioner" "me" {
-}
+data "coder_provisioner" "me" {}
 
-provider "docker" {
-}
-
-data "coder_workspace" "me" {
-}
+data "coder_workspace" "me" {}
 
 resource "coder_agent" "main" {
   arch                   = data.coder_provisioner.me.arch
@@ -40,11 +35,13 @@ resource "coder_agent" "main" {
       mv dev/.git .
       rm -rf dev
       git reset --hard
+      make nix
+      make symlinks
+      make perms
+      make home
     fi
 
-    # install and start code-server
-    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.14.1
-    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+    ~/bin/nix/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
   EOT
 
   # These environment variables allow you to make Git commits right away after creating a
@@ -72,52 +69,5 @@ resource "coder_app" "code-server" {
     url       = "http://localhost:13337/healthz"
     interval  = 5
     threshold = 6
-  }
-}
-
-resource "docker_image" "main" {
-  name = "quay.io/defn/dev:latest-nix"
-}
-
-resource "docker_container" "workspace" {
-  count = data.coder_workspace.me.start_count
-  image = docker_image.main.name
-  # Uses lower() to avoid Docker restriction on container names.
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
-  # Hostname makes the shell more user friendly: coder@my-workspace:~$
-  hostname = data.coder_workspace.me.name
-  # Use the docker gateway if the access URL is 127.0.0.1
-  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
-  env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
-  host {
-    host = "host.docker.internal"
-    ip   = "host-gateway"
-  }
-  volumes {
-    container_path = "/home/ubuntu"
-    volume_name    = "defn-dev-home"
-    read_only      = false
-  }
-  volumes {
-    container_path = "/nix"
-    volume_name    = "defn-dev-nix"
-    read_only      = false
-  }
-  # Add labels in Docker to keep track of orphan resources.
-  labels {
-    label = "coder.owner"
-    value = data.coder_workspace.me.owner
-  }
-  labels {
-    label = "coder.owner_id"
-    value = data.coder_workspace.me.owner_id
-  }
-  labels {
-    label = "coder.workspace_id"
-    value = data.coder_workspace.me.id
-  }
-  labels {
-    label = "coder.workspace_name"
-    value = data.coder_workspace.me.name
   }
 }
