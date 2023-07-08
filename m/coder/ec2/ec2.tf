b1f0c0c949904c7345dc9a23473fedcad155e906
amanibhavam <iam@defn.sh>
2023-07-08 15:23:18 -0700

    move trunk to m

--- m/coder/ec2/ec2.tf
+++ m/coder/ec2/ec2.tf
@@ -11,6 +11,7 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
@@ -23,6 +24,19 @@ resource "aws_instance" "dev" {
   instance_type     = data.coder_parameter.instance_type.value
 
   user_data = data.coder_workspace.me.transition == "start" ? local.user_data_start : local.user_data_end
+
+  root_block_device {
+    volume_size = 50
+    encrypted   = true
+  }
+
+  metadata_options {
+    http_endpont                = "enabled"
+    http_tokens                 = "required"
+    instance_metadata_tags      = "enabled"
+    http_put_response_hop_limit = 1
+  }
+
   tags = {
     Name              = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
     Coder_Provisioned = "true"
