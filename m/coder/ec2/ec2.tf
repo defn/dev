bdbd31f0b2312ae5ea102668b5ffcd38c3bca014
amanibhavam <iam@defn.sh>
2023-07-08 18:29:23 -0700

    ujpload

--- m/coder/ec2/ec2.tf
+++ m/coder/ec2/ec2.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = data.coder_parameter.region.value
 }
 
+locals {
+  coder_name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -18,10 +22,39 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_iam_role" "dev" {
+  name = local.coder_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = ""
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dev" {
+  role       = aws_iam_role.dev.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "dev" {
+  name = local.coder_name
+  role = aws_iam_role.dev.name
+}
+
 resource "aws_instance" "dev" {
   ami               = data.aws_ami.ubuntu.id
   availability_zone = "${data.coder_parameter.region.value}a"
   instance_type     = data.coder_parameter.instance_type.value
+
+  iam_instance_profile = aws_iam_instance_profile.dev.name
 
   user_data = data.coder_workspace.me.transition == "start" ? local.user_data_start : local.user_data_end
 
@@ -38,7 +71,7 @@ resource "aws_instance" "dev" {
   }
 
   tags = {
-    Name              = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+    Name              = local.coder_name
     Coder_Provisioned = "true"
   }
 }
