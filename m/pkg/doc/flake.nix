b7d017b3b1353bf78e7fb423394f632e676801ce
amanibhavam <iam@defn.sh>
2023-07-11 09:46:47 -0700

    add hugo

---
+++ m/pkg/doc/flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    pkg.url = github:defn/dev/pkg-pkg-0.0.10?dir=m/pkg/pkg;
+  };
+
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs =
+        let
+          flakeInputs = [
+            ctx.pkgs.hugo
+          ];
+        in
+        flakeInputs;
+    };
+  };
+}
