97c005c83d7cc8392c5a0711da9651f1003340d2
amanibhavam <iam@defn.sh>
2023-07-11 15:53:22 -0700

    cilium/ 0.15.0 -> 0.15.1

--- m/pkg/cilium/flake.nix
+++ m/pkg/cilium/flake.nix
@@ -18,22 +18,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-UEu7lLVdRgUVe3i/d0fMp3iIiRD4xlcp/mnLlMPTf1s="; # x86_64-linux
+        sha256 = "sha256-29rN2NUqjRUTDjjv8fus7RO2HEo3g++Kh/7GfmyOkWY="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-V9r1hwcxR0AkIfXYvwaQGPc8tmotpbQ5O3Qu9Z7hUTk="; # aarch64-linux
+        sha256 = "sha256-RWxbq31z8bQmp0J6xL6fr7EWKmORcsAu54u+UxGPKVg="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-T/wdSHB4qu5wF0bi4SL+5s1rpq3Cmv3MMmSW6Y5pS7A="; # x86_64-darwin
+        sha256 = "sha256-2VNxsELVBDr3ued9JxWvQYYdPKr0QilmlpuAPOVtW7Q="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-VkUF+mPjfPiVh4hBL+olw8kOoBcm/1nWXV6n7yMt6i8="; # aarch64-darwin
+        sha256 = "sha256-RtOTfxUrKb1wIeyz4ookSrHuLTMdhlKbP06Xf237XqM="; # aarch64-darwin
       };
     };
   };
