0ff2041aa2ab43ff9f804f0c38b101d686183014
defnn <yk-22610028@defn.sh>
2023-07-12 12:55:48 -0700

    update pkg deps

--- m/pkg/home/flake.nix
+++ m/pkg/home/flake.nix
@@ -14,7 +14,7 @@
     development.url = github:defn/dev/pkg-development-0.0.62?dir=m/pkg/development;
     cloud.url = github:defn/dev/pkg-cloud-0.0.124?dir=m/pkg/cloud;
     kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.117?dir=m/pkg/kubernetes;
-    coder.url = github:defn/dev/pkg-coder-0.26.0-1?dir=m/pkg/coder;
+    coder.url = github:defn/dev/pkg-coder-0.26.1-1?dir=m/pkg/coder;
     codeserver.url = github:defn/dev/pkg-codeserver-4.14.1-1?dir=m/pkg/codeserver;
     tailscale.url = github:defn/dev/pkg-tailscale-1.44.0-1?dir=m/pkg/tailscale;
     shell.url = github:defn/dev/pkg-shell-0.0.51?dir=m/pkg/shell;
