d3f0335db55ecbf083a4d58634f5a19a6432d8b2
defnn <yk-22610028@defn.sh>
2023-07-12 12:56:41 -0700

    update pkg deps

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -12,7 +12,7 @@
     vcluster.url = github:defn/dev/pkg-vcluster-0.15.2-1?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.0.0-1?dir=m/pkg/kubevirt;
     velero.url = github:defn/dev/pkg-velero-1.11.0-4?dir=m/pkg/velero;
-    cilium.url = github:defn/dev/pkg-cilium-0.15.1-1?dir=m/pkg/cilium;
+    cilium.url = github:defn/dev/pkg-cilium-0.15.2-1?dir=m/pkg/cilium;
     hubble.url = github:defn/dev/pkg-hubble-0.12.0-1?dir=m/pkg/hubble;
   };
 
