fe76dde2e0e71c27d250ffa9d19282469119f5ab
defnn <yk-22610028@defn.sh>
2023-07-13 14:22:34 -0700

    upgrading umbrella pkgs

--- m/pkg/localdev/flake.nix
+++ m/pkg/localdev/flake.nix
@@ -1,19 +1,19 @@
 {
   inputs = {
-    tilt.url = github:defn/dev/pkg-tilt-0.33.1-1?dir=m/pkg/tilt;
-    nomad.url = github:defn/dev/pkg-nomad-1.5.6-3?dir=m/pkg/nomad;
-    boundary.url = github:defn/dev/pkg-boundary-0.13.0-1?dir=m/pkg/boundary;
-    vault.url = github:defn/dev/pkg-vault-1.14.0-1?dir=m/pkg/vault;
-    gh.url = github:defn/dev/pkg-gh-2.32.0-1?dir=m/pkg/gh;
-    earthly.url = github:defn/dev/pkg-earthly-0.7.11-1?dir=m/pkg/earthly;
-    oras.url = github:defn/dev/pkg-oras-1.0.0-2?dir=m/pkg/oras;
-    buildkite.url = github:defn/dev/pkg-buildkite-3.49.0-1?dir=m/pkg/buildkite;
-    bk.url = github:defn/dev/pkg-bk-2.0.0-10?dir=m/pkg/bk;
-    buildevents.url = github:defn/dev/pkg-buildevents-0.14.0-2?dir=m/pkg/buildevents;
-    honeyvent.url = github:defn/dev/pkg-honeyvent-1.1.3-9?dir=m/pkg/honeyvent;
-    honeymarker.url = github:defn/dev/pkg-honeymarker-0.2.10-9?dir=m/pkg/honeymarker;
-    honeytail.url = github:defn/dev/pkg-honeytail-1.8.3-7?dir=m/pkg/honeytail;
-    kuma.url = github:defn/dev/pkg-kuma-2.3.0-1?dir=m/pkg/kuma;
+    tilt.url = github:defn/dev/pkg-tilt-0.33.1-2?dir=m/pkg/tilt;
+    nomad.url = github:defn/dev/pkg-nomad-1.5.6-4?dir=m/pkg/nomad;
+    boundary.url = github:defn/dev/pkg-boundary-0.13.0-2?dir=m/pkg/boundary;
+    vault.url = github:defn/dev/pkg-vault-1.14.0-2?dir=m/pkg/vault;
+    gh.url = github:defn/dev/pkg-gh-2.32.0-2?dir=m/pkg/gh;
+    earthly.url = github:defn/dev/pkg-earthly-0.7.11-2?dir=m/pkg/earthly;
+    oras.url = github:defn/dev/pkg-oras-1.0.0-3?dir=m/pkg/oras;
+    buildkite.url = github:defn/dev/pkg-buildkite-3.49.0-2?dir=m/pkg/buildkite;
+    bk.url = github:defn/dev/pkg-bk-2.0.0-11?dir=m/pkg/bk;
+    buildevents.url = github:defn/dev/pkg-buildevents-0.14.0-3?dir=m/pkg/buildevents;
+    honeyvent.url = github:defn/dev/pkg-honeyvent-1.1.3-10?dir=m/pkg/honeyvent;
+    honeymarker.url = github:defn/dev/pkg-honeymarker-0.2.10-10?dir=m/pkg/honeymarker;
+    honeytail.url = github:defn/dev/pkg-honeytail-1.8.3-8?dir=m/pkg/honeytail;
+    kuma.url = github:defn/dev/pkg-kuma-2.3.0-2?dir=m/pkg/kuma;
     zola.url = github:defn/dev/pkg-zola-0.0.3?dir=m/pkg/zola;
   };
 
