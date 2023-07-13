5ec889afaeb371ab4a46f589d7ba8870531c6ea8
defnn <yk-22610028@defn.sh>
2023-07-13 14:17:44 -0700

    upgrading umbrella pkgs

--- m/pkg/godev/flake.nix
+++ m/pkg/godev/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    buf.url = github:defn/dev/pkg-buf-1.23.1-1?dir=m/pkg/buf;
-    cosign.url = github:defn/dev/pkg-cosign-2.1.1-1?dir=m/pkg/cosign;
-    goreleaser.url = github:defn/dev/pkg-goreleaser-1.19.2-1?dir=m/pkg/goreleaser;
+    buf.url = github:defn/dev/pkg-buf-1.23.1-2?dir=m/pkg/buf;
+    cosign.url = github:defn/dev/pkg-cosign-2.1.1-2?dir=m/pkg/cosign;
+    goreleaser.url = github:defn/dev/pkg-goreleaser-1.19.2-2?dir=m/pkg/goreleaser;
     # https://github.com/NixOS/nixpkgs/commits/release-23.05
     latest.url = github:NixOS/nixpkgs?rev=5c9ddb86679c400d6b7360797b8a22167c2053f8;
   };
