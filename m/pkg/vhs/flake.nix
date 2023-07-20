e6b0dbb0b9a9f529ee57b9c625f1c4a1ec746abb
amanibhavam <iam@defn.sh>
2023-07-20 15:41:08 -0700

    vhs 0.6.0

--- m/pkg/vhs/flake.nix
+++ m/pkg/vhs/flake.nix
@@ -3,7 +3,7 @@
   outputs = inputs: inputs.pkg.downloadMain rec {
     src = ./.;
 
-    url_template = input: "https://github.com/charmbracelet/vhs/releases/download/v${input.vendor}/vhs_${input.os}_${input.arch}.tar.gz";
+    url_template = input: "https://github.com/charmbracelet/vhs/releases/download/v${input.vendor}/vhs_${input.vendor}_${input.os}_${input.arch}.tar.gz";
 
     installPhase = pkg: ''
       install -m 0755 -d $out $out/bin
@@ -16,22 +16,22 @@
       "x86_64-linux" = {
         os = "Linux";
         arch = "x86_64";
-        sha256 = "sha256-OXBlbeokMe83+rxgEWbdMEbnvbbeOALsUolnHQAOU8s="; # x86_64-linux
+        sha256 = "sha256-VCNMc7onyoMFzEVzvhirY2tWW4g9GVIdBUNLARrrCtw="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "Linux";
         arch = "arm64";
-        sha256 = "sha256-AfVu2uKeoAYAiEaqmDKnAG+LYw1C9onH+xrIAVgemBw="; # aarch64-linux
+        sha256 = "sha256-rnWPbALBhmo6MHDR1yHah3SzByztDIiZXvgKqe4nquQ="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "Darwin";
         arch = "x86_64";
-        sha256 = "sha256-zNBrXuGuaN8qmvE2LeqtynoAEpTplniuSiwRebqJlMc="; # x86_64-darwin
+        sha256 = "sha256-GGlJ6Is0BqQowbv0EGVju/NOE8Cv27R1pJuYenZXoRg="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "Darwin";
         arch = "arm64";
-        sha256 = "sha256-nkU3dg/4VLrQcMNMl6y5TD0IC7Zdg8YV7TOJVLP2qhE="; # aarch64-darwin
+        sha256 = "sha256-aILAqGeYyA+Q+1TzQ6pWXNDXwdQPG4IuHesfONUnhJo="; # aarch64-darwin
       };
     };
   };
