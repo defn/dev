315a14d3fd2a5f415e9cacc453e5a66de04c917f
defnn <yk-22610028@defn.sh>
2023-07-21 16:40:58 +0000

    update pkg deps

--- m/pkg/localdev/flake.nix
+++ m/pkg/localdev/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     tilt.url = github:defn/dev/pkg-tilt-0.33.1-2?dir=m/pkg/tilt;
-    nomad.url = github:defn/dev/pkg-nomad-1.6.0-1?dir=m/pkg/nomad;
+    nomad.url = github:defn/dev/pkg-nomad-1.6.1-1?dir=m/pkg/nomad;
     boundary.url = github:defn/dev/pkg-boundary-0.13.1-1?dir=m/pkg/boundary;
     vault.url = github:defn/dev/pkg-vault-1.14.0-2?dir=m/pkg/vault;
     gh.url = github:defn/dev/pkg-gh-2.32.0-2?dir=m/pkg/gh;
