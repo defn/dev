a116b93d09beead9ef98cde66be6758046ef5ee1
amanibhavam <iam@defn.sh>
2023-07-21 04:30:24 -0700

    add nix volume

--- m/coder/do/do.tf
+++ m/coder/do/do.tf
@@ -10,17 +10,33 @@ resource "digitalocean_volume" "home_volume" {
   }
 }
 
+resource "digitalocean_volume" "nix_volume" {
+  region                   = data.coder_parameter.region.value
+  name                     = "coder-${data.coder_workspace.me.id}-nix"
+  size                     = data.coder_parameter.nix_volume_size.value
+  initial_filesystem_type  = "ext4"
+  initial_filesystem_label = "coder-nix"
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
 resource "digitalocean_droplet" "workspace" {
   count = data.coder_workspace.me.start_count
 
-  region     = data.coder_parameter.region.value
-  name       = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
-  image      = data.coder_parameter.droplet_image.value
-  size       = data.coder_parameter.droplet_size.value
-  volume_ids = [digitalocean_volume.home_volume.id]
+  region = data.coder_parameter.region.value
+  name   = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+  image  = data.coder_parameter.droplet_image.value
+  size   = data.coder_parameter.droplet_size.value
+  volume_ids = [
+    digitalocean_volume.home_volume.id,
+    digitalocean_volume.nix_volume.id
+  ]
 
   user_data = templatefile("cloud-config.yaml.tftpl", {
     home_volume_label = digitalocean_volume.home_volume.initial_filesystem_label
+    nix_volume_label  = digitalocean_volume.nix_volume.initial_filesystem_label
     init_script       = base64encode(coder_agent.main.init_script)
     coder_agent_token = coder_agent.main.token
   })
--- m/coder/do/params.tf
+++ m/coder/do/params.tf
@@ -36,7 +36,6 @@ data "coder_parameter" "droplet_size" {
   }
 }
 
-
 data "coder_parameter" "home_volume_size" {
   name         = "home_volume_size"
   display_name = "Home volume size"
@@ -47,6 +46,20 @@ data "coder_parameter" "home_volume_size" {
 
   validation {
     min = 40
+    max = 100
+  }
+}
+
+data "coder_parameter" "nix_volume_size" {
+  name         = "nix_volume_size"
+  display_name = "nix volume size"
+  description  = "How large would you like your nix volume to be (in GB)?"
+  type         = "number"
+  default      = "20"
+  mutable      = false
+
+  validation {
+    min = 20
     max = 100
   }
 }
