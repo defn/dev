c8e5b6f54dd985a4103350df578ceccbbee73ba7
amanibhavam <iam@defn.sh>
2023-07-24 22:32:44 -0700

    no home dir, just use nix

--- m/coder/do/do.tf
+++ m/coder/do/do.tf
@@ -2,18 +2,6 @@ locals {
   coder_name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
 }
 
-resource "digitalocean_volume" "home_volume" {
-  region                   = data.coder_parameter.region.value
-  name                     = "coder-${data.coder_workspace.me.id}-home"
-  size                     = data.coder_parameter.home_volume_size.value
-  initial_filesystem_type  = "ext4"
-  initial_filesystem_label = "coder-home"
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
-
 resource "digitalocean_volume" "nix_volume" {
   region                   = data.coder_parameter.region.value
   name                     = "coder-${data.coder_workspace.me.id}-nix"
@@ -37,12 +25,10 @@ resource "digitalocean_droplet" "workspace" {
   size  = data.coder_parameter.droplet_size.value
 
   volume_ids = [
-    digitalocean_volume.home_volume.id,
     digitalocean_volume.nix_volume.id
   ]
 
   user_data = templatefile("cloud-config.yaml.tftpl", {
-    home_volume_label = digitalocean_volume.home_volume.initial_filesystem_label
     nix_volume_label  = digitalocean_volume.nix_volume.initial_filesystem_label
     init_script       = base64encode(coder_agent.main.init_script)
     coder_agent_token = coder_agent.main.token
