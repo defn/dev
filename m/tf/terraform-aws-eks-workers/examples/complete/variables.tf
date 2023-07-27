f042a05b6cbaea83baccd156abd37a5ca39ba947
amanibhavam <iam@defn.sh>
2023-07-26 23:35:41 -0700

    trim for ease of merging

---
+++ m/tf/terraform-aws-eks-workers/examples/complete/variables.tf
@@ -0,0 +1,76 @@
+variable "region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the EKS cluster"
+}
+
+variable "cluster_endpoint" {
+  type        = string
+  description = "EKS cluster endpoint"
+}
+
+variable "cluster_certificate_authority_data" {
+  type        = string
+  description = "The base64 encoded certificate data required to communicate with the cluster"
+}
+
+variable "cluster_security_group_ingress_enabled" {
+  type        = bool
+  description = "Whether to enable the EKS cluster Security Group as ingress to workers Security Group"
+}
+
+variable "cluster_security_group_id" {
+  type        = string
+  description = "Security Group ID of the EKS cluster"
+  default     = ""
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type to launch"
+}
+
+variable "max_size" {
+  type        = number
+  description = "The maximum size of the autoscale group"
+}
+
+variable "min_size" {
+  type        = number
+  description = "The minimum size of the autoscale group"
+}
+
+variable "health_check_type" {
+  type        = string
+  description = "Controls how health checking is done. Valid values are `EC2` or `ELB`"
+}
+
+variable "wait_for_capacity_timeout" {
+  type        = string
+  description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out. Setting this to '0' causes Terraform to skip all Capacity Waiting behavior"
+}
+
+variable "autoscaling_policies_enabled" {
+  type        = bool
+  description = "Whether to create `aws_autoscaling_policy` and `aws_cloudwatch_metric_alarm` resources to control Auto Scaling"
+}
+
+variable "cpu_utilization_high_threshold_percent" {
+  type        = number
+  description = "The value against which the specified statistic is compared"
+}
+
+variable "cpu_utilization_low_threshold_percent" {
+  type        = number
+  description = "The value against which the specified statistic is compared"
+}
