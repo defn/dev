4e5589b18cae71cc1bfd6b6822532f90afaf1b58
defnn <yk-22610028@defn.sh>
2023-07-29 00:24:57 -0700

    no tilt wrapper

--- m/pkg/localdev/flake.nix
+++ m/pkg/localdev/flake.nix
@@ -42,19 +42,7 @@
             skopeo
           ];
         in
-        flakeInputs ++ ctx.commands;
-    };
-
-    scripts = { system }: {
-      tilt = ''
-        port="$(
-          (
-          ss -tln --tcp --no-header | awk '{print $4}' | cut -d: -f2 | sort
-          seq 10300 10399
-          ) | sort | uniq -c | perl -ne 'print if m{\b103\d\d\b}' | grep ' 1 ' | awk '{print $2}' | head -1
-        )"
-        exec tilt up --port "$port" "$@"
-      '';
+        flakeInputs;
     };
   };
 }
