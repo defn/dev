324a2471d9c763f34205699cc8db180b09164998
amanibhavam <iam@defn.sh>
2023-07-29 03:05:40 +0000

    update pkg deps

--- m/pkg/cloud/flake.nix
+++ m/pkg/cloud/flake.nix
@@ -3,8 +3,8 @@
     terraform.url = github:defn/dev/pkg-terraform-1.5.4-1?dir=m/pkg/terraform;
     packer.url = github:defn/dev/pkg-packer-1.9.2-1?dir=m/pkg/packer;
     step.url = github:defn/dev/pkg-step-0.24.4-4?dir=m/pkg/step;
-    awscli.url = github:defn/dev/pkg-awscli-2.13.4-1?dir=m/pkg/awscli;
-    flyctl.url = github:defn/dev/pkg-flyctl-0.1.65-1?dir=m/pkg/flyctl;
+    awscli.url = github:defn/dev/pkg-awscli-2.13.5-1?dir=m/pkg/awscli;
+    flyctl.url = github:defn/dev/pkg-flyctl-0.1.66-1?dir=m/pkg/flyctl;
     chamber.url = github:defn/dev/pkg-chamber-2.13.2-2?dir=m/pkg/chamber;
   };
 
