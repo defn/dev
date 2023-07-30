5c61a2b68afd461ff926d951aeee23bce69170e1
amanibhavam <iam@defn.sh>
2023-07-30 13:29:42 -0700

    run nomad

--- m/coder/fly/coder.tf
+++ m/coder/fly/coder.tf
@@ -131,6 +131,22 @@ resource "coder_app" "temporal" {
   }
 }
 
+resource "coder_app" "nomad" {
+  agent_id     = coder_agent.main.id
+  slug         = "nomad"
+  display_name = "nomad"
+  url          = "http://localhost:4646"
+  icon         = "/icon/code.svg"
+  subdomain    = true
+  share        = "owner"
+
+  healthcheck {
+    url       = "http://localhost:4646"
+    interval  = 5
+    threshold = 6
+  }
+}
+
 resource "coder_metadata" "workspace" {
   resource_id = fly_app.workspace.id
 
