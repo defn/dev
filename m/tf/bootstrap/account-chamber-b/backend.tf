6ef12d09317493bc5a33233dffdccb1ab83d2537
amanibhavam <iam@defn.sh>
2023-07-30 02:16:37 +0000

    remote backend

---
+++ m/tf/bootstrap/account-chamber-b/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "dfn-defn-terraform-state"
+    key            = "chamber-b/bootstrap/account-chamber-b/terraform.tfstate"
+    dynamodb_table = "dfn-defn-terraform-state-lock"
+    profile        = "defn-org-sso"
+    role_arn       = ""
+    encrypt        = "true"
+  }
+}
