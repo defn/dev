bbeebc9795328ad751065bf8d1730f897ede9df9
amanibhavam <iam@defn.sh>
2023-07-30 05:46:07 +0000

    plan took 2m 35s with 16 cpu, 18gb ram

---
+++ m/tf/global/s3-whoa-org.tf
@@ -0,0 +1,17 @@
+module "s3-whoa-org" {
+  source  = "../terraform-aws-s3-bucket"
+  context = module.this.context
+
+  providers = {
+    aws = aws.whoa-org
+  }
+
+  acl                = "private"
+  user_enabled       = true
+  versioning_enabled = false
+
+  lifecycle_configuration_rules = local.lifecycle_configuration_rules
+
+  privileged_principal_arns    = local.privileged_principal_arns
+  privileged_principal_actions = local.privileged_principal_actions
+}
