e1ebc147d827bb23a42d4273223c2b6186cfa9ae
amanibhavam <iam@defn.sh>
2023-07-30 02:16:37 +0000

    remote backend

---
+++ m/tf/bootstrap/account-circus-audit/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "dfn-defn-terraform-state"
+    key            = "circus-audit/bootstrap/account-circus-audit/terraform.tfstate"
+    dynamodb_table = "dfn-defn-terraform-state-lock"
+    profile        = "defn-org-sso"
+    role_arn       = ""
+    encrypt        = "true"
+  }
+}
