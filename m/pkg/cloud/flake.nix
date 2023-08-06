3ebe669b73fe6cbf137c83697608ef957670d712
amanibhavam <iam@defn.sh>
2023-08-06 04:07:05 +0000

    aws cli

--- m/pkg/cloud/flake.nix
+++ m/pkg/cloud/flake.nix
@@ -4,7 +4,7 @@
     terraformdocs.url = github:defn/dev/pkg-terraformdocs-0.16.0-1?dir=m/pkg/terraformdocs;
     packer.url = github:defn/dev/pkg-packer-1.9.2-1?dir=m/pkg/packer;
     step.url = github:defn/dev/pkg-step-0.24.4-4?dir=m/pkg/step;
-    awscli.url = github:defn/dev/pkg-awscli-2.13.6-1?dir=m/pkg/awscli;
+    awscli.url = github:defn/dev/pkg-awscli-2.13.7-1?dir=m/pkg/awscli;
     flyctl.url = github:defn/dev/pkg-flyctl-0.1.71-1?dir=m/pkg/flyctl;
     chamber.url = github:defn/dev/pkg-chamber-2.13.3-1?dir=m/pkg/chamber;
   };
