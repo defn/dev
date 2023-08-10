1235913152c0c2c9b163ef83aeb1d4e5cdced98a
defnn <yk-22610028@defn.sh>
2023-08-10 10:20:18 -0700

    update pkg deps

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -9,7 +9,7 @@
     argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.4.9-1?dir=m/pkg/argoworkflows;
     argocd.url = github:defn/dev/pkg-argocd-2.8.0-1?dir=m/pkg/argocd;
     kn.url = github:defn/dev/pkg-kn-1.11.0-1?dir=m/pkg/kn;
-    vcluster.url = github:defn/dev/pkg-vcluster-0.15.2-2?dir=m/pkg/vcluster;
+    vcluster.url = github:defn/dev/pkg-vcluster-0.15.5-1?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.0.0-2?dir=m/pkg/kubevirt;
     velero.url = github:defn/dev/pkg-velero-1.11.1-1?dir=m/pkg/velero;
     cilium.url = github:defn/dev/pkg-cilium-0.15.5-1?dir=m/pkg/cilium;
