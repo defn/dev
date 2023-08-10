7e5ae8b4a28d7b1586ab6bdfe5abc018e308e74d
defnn <yk-22610028@defn.sh>
2023-08-09 20:02:13 -0700

    update pkg deps

--- m/pkg/godev/flake.nix
+++ m/pkg/godev/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    buf.url = github:defn/dev/pkg-buf-1.25.1-1?dir=m/pkg/buf;
+    buf.url = github:defn/dev/pkg-buf-1.26.1-1?dir=m/pkg/buf;
     cosign.url = github:defn/dev/pkg-cosign-2.1.1-2?dir=m/pkg/cosign;
-    goreleaser.url = github:defn/dev/pkg-goreleaser-1.19.2-2?dir=m/pkg/goreleaser;
+    goreleaser.url = github:defn/dev/pkg-goreleaser-1.20.0-1?dir=m/pkg/goreleaser;
     # https://github.com/NixOS/nixpkgs/commits/release-23.05
     latest.url = github:NixOS/nixpkgs?rev=5c9ddb86679c400d6b7360797b8a22167c2053f8;
   };
