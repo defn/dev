2c3a32668d9c59f5e64d5de73a0b6381f95d9c86
defnn <yk-22610028@defn.sh>
2023-08-12 19:48:13 +0000

    remotedev for coder/code-server

--- m/pkg/remotedev/flake.nix
+++ m/pkg/remotedev/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     coder.url = github:defn/dev/pkg-coder-2.0.2-1?dir=m/pkg/coder;
-    codeserver.url = github:defn/dev/pkg-code-server-2.32.1-1?dir=m/pkg/codeserver;
+    codeserver.url = github:defn/dev/pkg-codeserver-4.16.1-1?dir=m/pkg/codeserver;
   };
 
   outputs = inputs: inputs.coder.inputs.pkg.main rec {
