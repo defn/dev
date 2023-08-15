d818493f68c8117fec4635695caec2dfaf142cea
defnn <yk-22610028@defn.sh>
2023-08-15 16:18:38 +0000

    upgrade pkg dep

--- m/pkg/nodedev/flake.nix
+++ m/pkg/nodedev/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    pkg.url = github:defn/dev/pkg-pkg-0.0.11?dir=m/pkg/pkg;
+    pkg.url = github:defn/dev/pkg-pkg-0.0.12?dir=m/pkg/pkg;
     # https://github.com/NixOS/nixpkgs/commits/release-23.05
     latest.url = github:NixOS/nixpkgs?rev=3fe694c4156b84dac12627685c7ae592a71e2206;
   };
