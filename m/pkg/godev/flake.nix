2526625eca0cff37e5df3389cdf0081989358b8a
amanibhavam <iam@defn.sh>
2023-08-14 19:20:58 -0700

    update pkg dep

--- m/pkg/godev/flake.nix
+++ m/pkg/godev/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     buf.url = github:defn/dev/pkg-buf-1.26.1-1?dir=m/pkg/buf;
-    cosign.url = github:defn/dev/pkg-cosign-2.1.1-2?dir=m/pkg/cosign;
+    cosign.url = github:defn/dev/pkg-cosign-2.1.1-3?dir=m/pkg/cosign;
     goreleaser.url = github:defn/dev/pkg-goreleaser-1.20.0-1?dir=m/pkg/goreleaser;
     # https://github.com/NixOS/nixpkgs/commits/release-23.05
     latest.url = github:NixOS/nixpkgs?rev=3fe694c4156b84dac12627685c7ae592a71e2206;
