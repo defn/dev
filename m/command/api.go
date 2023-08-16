d5acdda581effb179490a2bb465f220e8c6b7432
amanibhavam <iam@defn.sh>
2023-08-16 05:40:28 +0000

    working quickly

--- m/command/api.go
+++ m/command/api.go
@@ -11,12 +11,7 @@ import (
 var apiCmd = &cobra.Command{
 	Use:   "api",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Something longer`,
 	Run: func(cmd *cobra.Command, args []string) {
 		r := gin.Default()
 		r.SetTrustedProxies(nil)
