cf0bebf6001aa645a3655fe3573a8f98878aa14e
amanibhavam <iam@defn.sh>
2023-08-17 04:04:33 +0000

    move interesting stuff to web

--- m/w/apps/web/src/routes/+layout.svelte
+++ m/w/apps/web/src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
   import { Page } from 'ui';
+  import { NavLi } from 'flowbite-svelte';
 </script>
 
 <Page>
+  <span slot="nav">
+    <NavLi href="/about">About</NavLi>
+  </span>
   <slot />
 </Page>
