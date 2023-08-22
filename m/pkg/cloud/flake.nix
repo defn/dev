eb7a2a4f3045efd30f4dd40aac82c968031b5066
amanibhavam <iam@defn.sh>
2023-08-22 01:19:28 +0000

    update pkg deps

--- m/pkg/cloud/flake.nix
+++ m/pkg/cloud/flake.nix
@@ -5,7 +5,7 @@
     packer.url = github:defn/dev/pkg-packer-1.9.4-1?dir=m/pkg/packer;
     step.url = github:defn/dev/pkg-step-0.24.4-5?dir=m/pkg/step;
     awscli.url = github:defn/dev/pkg-awscli-2.13.11-1?dir=m/pkg/awscli;
-    flyctl.url = github:defn/dev/pkg-flyctl-0.1.79-1?dir=m/pkg/flyctl;
+    flyctl.url = github:defn/dev/pkg-flyctl-0.1.81-1?dir=m/pkg/flyctl;
     chamber.url = github:defn/dev/pkg-chamber-2.13.3-2?dir=m/pkg/chamber;
   };
 
