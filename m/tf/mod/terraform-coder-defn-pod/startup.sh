6936ec2b58de91a7dcd9b55b905e053b29686d80
amanibhavam <iam@defn.sh>
2023-08-23 05:17:51 +0000

    pod shouldnt update

--- m/tf/mod/terraform-coder-defn-pod/startup.sh
+++ m/tf/mod/terraform-coder-defn-pod/startup.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-set -ex
-
-exec 3>&1
-tail -f /tmp/dfd-startup.log 1>&3 &
-exec >>/tmp/dfd-startup.log 2>&1
-
 function main {
-	curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.16.1
-	/tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+	source .bash_profile
+
+	set -ex
 
 	sudo chmod g-s /home/ubuntu
 
@@ -18,9 +13,7 @@ function main {
 	ssh -o StrictHostKeyChecking=no git@github.com true || true
 	git pull
 
-	cd
-	source .bash_profile
-	make install
+	setsid code-server --auth none --port 13337 &
 }
 
 time main "$@"
