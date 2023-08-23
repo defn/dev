40695da1d63f9d99df913219c9581823cff09a57
amanibhavam <iam@defn.sh>
2023-08-23 02:48:31 +0000

    lets push this new k3d image

--- m/coder/k3d/k3s-wrapper.sh
+++ m/coder/k3d/k3s-wrapper.sh
@@ -2,32 +2,16 @@
 
 set -exu
 
-PATH=${PATH}:/home/ubuntu/bin/nix
-
 tailscaled --statedir=/var/lib/tailscale &
 
 container_ip=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1)
 
-tailscale up --authkey="${DEFN_DEV_TSKEY}" --accept-dns=false --ssh
+tailscale up --authkey="${TSKEY}"
 
 while true; do
 	ts_ip=$(tailscale ip -4 || true)
 	if test -n "${ts_ip}"; then break; fi
 	sleep 1
-done
-
-domain=
-while test -z "${domain}"; do
-	domain=$(tailscale cert 2>&1 | grep ' use ' | cut -d'"' -f2)
-	sleep 1
-done
-
-for a in /var/lib/rancher/k3s/server/manifests; do
-	mkdir -p "${a}"
-	(
-		set +f
-		cp "${a}-bootstrap"/* "${a}"/
-	) || true
 done
 
 exec /bin/k3s "$@" --node-ip "${ts_ip}" --node-external-ip "${ts_ip}" --flannel-iface=tailscale0
