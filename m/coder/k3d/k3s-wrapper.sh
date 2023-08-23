1114ce9a390fa6dcaba39567ff589ec01fe6312b
amanibhavam <iam@defn.sh>
2023-08-23 03:32:11 +0000

    fix k3s symlinks

--- m/coder/k3d/k3s-wrapper.sh
+++ m/coder/k3d/k3s-wrapper.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-set -exu
+if [[ ${1-} == server ]]; then
+	set -exu
 
-tailscaled --statedir=/var/lib/tailscale &
+	tailscaled --statedir=/var/lib/tailscale &
 
-container_ip=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1)
+	container_ip=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1)
 
-tailscale up
+	tailscale up
 
-while true; do
-	ts_ip=$(tailscale ip -4 || true)
-	if test -n "${ts_ip}"; then break; fi
-	sleep 1
-done
+	while true; do
+		ts_ip=$(tailscale ip -4 || true)
+		if test -n "${ts_ip}"; then break; fi
+		sleep 1
+	done
 
-exec /bin/k3s "$@" --node-ip "${ts_ip}" --node-external-ip "${ts_ip}" --flannel-iface=tailscale0
+	exec /bin/k3s-real "$@" --node-ip "${ts_ip}" --node-external-ip "${ts_ip}" --flannel-iface=tailscale0
+else
+	exec /bin/k3s-real "$@"
+fi
