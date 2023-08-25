6721c3938348094c7819b4ec7a550b895ea7d1de
amanibhavam <iam@defn.sh>
2023-08-25 18:48:17 +0000

    update pkg deps

--- m/pkg/development/flake.nix
+++ m/pkg/development/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    buildifier.url = github:defn/dev/pkg-buildifier-6.3.2-1?dir=m/pkg/buildifier;
+    buildifier.url = github:defn/dev/pkg-buildifier-6.3.3-1?dir=m/pkg/buildifier;
     bazelisk.url = github:defn/dev/pkg-bazelisk-1.18.0-1?dir=m/pkg/bazelisk;
     ibazel.url = github:defn/dev/pkg-ibazel-0.23.7-2?dir=m/pkg/ibazel;
   };
