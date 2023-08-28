eca3fce153ddbd4d52e2157cb88997cc2fec883b
amanibhavam <iam@defn.sh>
2023-08-28 05:52:36 +0000

    skip alpha hof

--- m/pkg/make_list_vendor_updates.sh
+++ m/pkg/make_list_vendor_updates.sh
@@ -18,7 +18,7 @@ function main {
 		if [[ ${current} != "${latest}" ]]; then
 			echo "${slug} ${current} ${latest} ${real_tag}"
 		fi
-	done | (grep -E -v 'kustomize/ 5.0.3 5.1|coder-compat/|tailscale/ 1.46.1 1.48.0|tfo/ 2.0.0-beta2 1.3.0|coder/ 2.1.0 2.1.1|argocd/ 2.8.1 2.7.12|argocd/ 2.8.1 2.6.14' || true)
+	done | (grep -E -v 'kustomize/ 5.0.3 5.1|coder-compat/|tailscale/ 1.46.1 1.48.0|tfo/ 2.0.0-beta2 1.3.0|coder/ 2.1.0 2.1.1|argocd/ 2.8.1 2.7.12|argocd/ 2.8.1 2.6.14|hof/ 0.6.8 0.6.9-' || true)
 }
 
 main "$@"
