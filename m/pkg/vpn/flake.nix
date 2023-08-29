e6cace922eaf7a168b705579217248b4deec8d00
amanibhavam <iam@defn.sh>
2023-08-29 06:08:01 +0000

    add cloudflared

--- m/pkg/vpn/flake.nix
+++ m/pkg/vpn/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     tailscale.url = github:defn/dev/pkg-tailscale-1.48.1-1?dir=m/pkg/tailscale;
+    cloudflared.url = github:defn/dev/pkg-cloudflared-1.48.1-1?dir=m/pkg/cloudflared;
   };
 
   outputs = inputs: inputs.tailscale.inputs.pkg.main rec {
@@ -9,6 +10,7 @@
     defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs = with ctx.pkgs; [
         inputs.tailscale.defaultPackage.${ctx.system}
+        inputs.cloudflare.defaultPackage.${ctx.system}
         easyrsa
         openvpn
         wireguard-tools
