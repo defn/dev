90d8cf3b84334b8e6b5df59e3d4a04c5a29f452f
amanibhavam <iam@defn.sh>
2023-08-29 06:09:42 +0000

    add cilium

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -11,6 +11,7 @@
     kn.url = github:defn/dev/pkg-kn-1.11.0-2?dir=m/pkg/kn;
     vcluster.url = github:defn/dev/pkg-vcluster-0.15.7-1?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.0.0-3?dir=m/pkg/kubevirt;
+    linkerd.url = github:defn/dev/pkg-linkerd-0.15.6-1?dir=m/pkg/linkerd;
     cilium.url = github:defn/dev/pkg-cilium-0.15.6-1?dir=m/pkg/cilium;
     hubble.url = github:defn/dev/pkg-hubble-0.12.0-3?dir=m/pkg/hubble;
     tfo.url = github:defn/dev/pkg-tfo-1.3.0-3?dir=m/pkg/tfo;
@@ -34,6 +35,7 @@
             inputs.kn.defaultPackage.${ctx.system}
             inputs.vcluster.defaultPackage.${ctx.system}
             inputs.kubevirt.defaultPackage.${ctx.system}
+            inputs.linkerd.defaultPackage.${ctx.system}
             inputs.cilium.defaultPackage.${ctx.system}
             inputs.hubble.defaultPackage.${ctx.system}
             inputs.tfo.defaultPackage.${ctx.system}
