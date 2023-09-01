c8442ace4f0fea2c7a60dc339ba5c2b84e420aa7
amanibhavam <iam@defn.sh>
2023-08-31 23:27:53 -0700

    simplify

--- m/coder/k3d/k3s-wrapper.sh
+++ m/coder/k3d/k3s-wrapper.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-if [[ ${1-} == server ]]; then
-	set -exu
+set -exu
 
-	tailscaled --statedir=/var/lib/tailscale &
-
-	container_ip=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1)
-
-	tailscale up
-
-	while true; do
-		ts_ip=$(tailscale ip -4 || true)
-		if test -n "${ts_ip}"; then break; fi
-		sleep 1
-	done
-
-	exec /bin/k3s-real "$@" --node-ip "${ts_ip}" --node-external-ip "${ts_ip}" --flannel-iface=tailscale0
-else
+if [[ ${1-} != server ]]; then
 	exec /bin/k3s-real "$@"
+  exit $?
 fi
+
+tailscaled --statedir=/var/lib/tailscale &
+
+container_ip=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | cut -d' ' -f1)
+
+tailscale up
+
+while true; do
+  ts_ip=$(tailscale ip -4 || true)
+  if test -n "${ts_ip}"; then break; fi
+  sleep 1
+done
+
+exec /bin/k3s-real "$@" --node-ip "${ts_ip}" --node-external-ip "${ts_ip}" --flannel-iface=tailscale0
+
