69acb57d64372f0c85e0d1e80ddec9f9b76a47d4
amanibhavam <iam@defn.sh>
2023-09-04 06:25:54 +0000

    add procps

--- m/pkg/shell/flake.nix
+++ m/pkg/shell/flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.12?dir=m/pkg/pkg;
-    zellij.url = github:defn/dev/pkg-zellij-0.38.1-1?dir=m/pkg/zellij;
   };
 
   outputs = inputs: inputs.pkg.main rec {
@@ -9,7 +8,7 @@
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs = with ctx.pkgs; [
-        inputs.zellij.defaultPackage.${ctx.system}
+        procps
         vim
         openssh
         screen
