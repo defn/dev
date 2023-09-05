a612e410e3b967879730301ee9842c9e029046d6
amanibhavam <iam@defn.sh>
2023-09-05 23:51:10 +0000

    fmt

--- m/tf/mod/terraform-coder-defn-pod/coder.tf
+++ m/tf/mod/terraform-coder-defn-pod/coder.tf
@@ -12,7 +12,7 @@ resource "coder_agent" "main" {
   startup_script_timeout = 180
   startup_script         = "workdir=${data.coder_parameter.workdir.value}; source_rev=${data.coder_parameter.source_rev.value}; ${file("${path.module}/startup.sh")}"
 
-    display_apps {
+  display_apps {
     vscode          = true
     vscode_insiders = false
     ssh_helper      = false
