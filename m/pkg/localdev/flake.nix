eeb8b9f407c78c992dfeeb4b8fadf7e18f228454
amanibhavam <iam@defn.sh>
2023-09-07 05:22:53 +0000

    update pkg deps

--- m/pkg/localdev/flake.nix
+++ m/pkg/localdev/flake.nix
@@ -4,7 +4,7 @@
     gh.url = github:defn/dev/pkg-gh-2.34.0-1?dir=m/pkg/gh;
     earthly.url = github:defn/dev/pkg-earthly-0.7.17-1?dir=m/pkg/earthly;
     oras.url = github:defn/dev/pkg-oras-1.1.0-1?dir=m/pkg/oras;
-    buildkite.url = github:defn/dev/pkg-buildkite-3.53.0-1?dir=m/pkg/buildkite;
+    buildkite.url = github:defn/dev/pkg-buildkite-3.54.0-1?dir=m/pkg/buildkite;
     bk.url = github:defn/dev/pkg-bk-2.0.0-12?dir=m/pkg/bk;
     buildevents.url = github:defn/dev/pkg-buildevents-0.15.0-2?dir=m/pkg/buildevents;
     honeyvent.url = github:defn/dev/pkg-honeyvent-1.1.3-11?dir=m/pkg/honeyvent;
