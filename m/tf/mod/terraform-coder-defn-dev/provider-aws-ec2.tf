0d5f5151c880ba96d0d4bbe09579901e35b74175
amanibhavam <iam@defn.sh>
2023-09-09 01:18:29 +0000

    use smaller instance

--- m/tf/mod/terraform-coder-defn-dev/provider-aws-ec2.tf
+++ m/tf/mod/terraform-coder-defn-dev/provider-aws-ec2.tf
@@ -2,12 +2,12 @@ locals {
   aws = {
     region            = "us-west-2"
     availability_zone = "us-west-2a"
-    instance_type     = "m6id.2xlarge"
+    instance_type     = "m6id.xlarge"
     root_volume_size  = data.coder_parameter.nix_volume_size.value
   }
 
   owners     = ["self"]
-  ami_filter = ["base-*"]
+  ami_filter = ["coder-*"]
 }
 
 provider "aws" {
