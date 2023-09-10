0a70e35b36d948c7d16869c260dece9bab8d0c7d
amanibhavam <iam@defn.sh>
2023-09-10 07:07:57 +0000

    shift-ctrl-z

---
+++ m/.joyride/scripts/dfd/meh.cljs
@@ -0,0 +1,19 @@
+(ns dfd.meh
+  (:require ["path" :as path]
+            ["vscode" :as vscode]
+            [joyride.core :as joyride]
+            [promesa.core :as p]))
+
+(defn main []
+  (p/let [panel (vscode/window.createWebviewPanel
+                 "Hello!" "dfd"
+                 vscode/ViewColumn.One
+                 #js {:enableScripts true})
+          uri (vscode/Uri.file (path/join vscode/workspace.rootPath
+                                          ".joyride" "scripts" "dfd" "index.html"))
+          data (vscode/workspace.fs.readFile uri)
+          html (.decode (js/TextDecoder. "utf-8") data)]
+    (set! (.. panel -webview -html) (str html))))
+
+(when (= (joyride/invoked-script) joyride/*file*)
+  (main))
