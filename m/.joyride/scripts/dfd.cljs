9698c8582cd4c2c22c055abd0329e4afa982f90c
amanibhavam <iam@defn.sh>
2023-09-10 20:02:55 +0000

    dont output anything at the ednd

--- m/.joyride/scripts/dfd.cljs
+++ m/.joyride/scripts/dfd.cljs
@@ -1,26 +1,29 @@
-(ns meh
+(ns dfd
   (:require ["path" :as path]
             ["vscode" :as vscode]
             [joyride.core :as joyride]
             [promesa.core :as p]))
 
 (defn main []
-  (p/let [panel (vscode/window.createWebviewPanel
-                 "" "Tutorial"
-                 vscode/ViewColumn.One
-                 #js {:enableScripts true})
-          uri (vscode/Uri.file (path/join vscode/workspace.rootPath
-                                          "index.html"))
+  (p/let [terminal (vscode/window.createTerminal #js {:name "tutorial"})
+          panel (vscode/window.createWebviewPanel "tutorial" "Tutorial" vscode/ViewColumn.One #js {:enableScripts true})
+          uri (vscode/Uri.file (path/join vscode/workspace.rootPath "index.html"))
           data (vscode/workspace.fs.readFile uri)
           html (.decode (js/TextDecoder. "utf-8") data)]
+
     (doto (joyride/output-channel)
       (.appendLine "==============================================================")
       (.appendLine (str "Invoked script: " (joyride/invoked-script)))
       (.appendLine "=============================================================="))
+
     (set! (.. panel -webview -html) (str html))
-    (let [terminal (vscode/window.createTerminal #js {:name "dfd"})]
-        (.show terminal true)
-        (.sendText terminal "make"))
+
+    (doto terminal
+      (.show true)
+      (.sendText "make index.html"))
+
+    (p/-> (vscode/workspace.openTextDocument (path/join vscode/workspace.rootPath "index.cue"))
+          (vscode/window.showTextDocument #js {:preview false, :preserveFocus false}))
   ))
 
 (when (= (joyride/invoked-script) joyride/*file*)
