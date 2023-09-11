016807c5db493391083943d28d079c906ce5a297
amanibhavam <iam@defn.sh>
2023-09-11 04:00:23 +0000

    sorter scripts

--- .config/joyride/scripts/user_activate.cljs
+++ .config/joyride/scripts/user_activate.cljs
@@ -1,6 +1,8 @@
-(ns workspace-activate
-  (:require ["vscode" :as vscode]
+(ns user-activate
+  (:require ["path" :as path]
+            ["vscode" :as vscode]
             [joyride.core :as joyride]
+            [promesa.core :as p]
             [dfd]))
 
 (defonce !db (atom {:disposables []}))
@@ -23,6 +25,27 @@
       .-subscriptions
       (.push disposable)))
 
+(defn tutorial []
+  ;; open index.cue
+  (p/let [doc (vscode/workspace.openTextDocument (path/join vscode/workspace.rootPath "index.cue"))
+          meh (vscode/window.showTextDocument doc #js {:preview false, :preserveFocus false, :viewColumn: vscode/ViewColumn.One})]
+
+    ;; split editor TODO does this execute async? if so, then it's not guaranteed the tutorial loads in column two
+    (vscode/commands.executeCommand "workbench.action.moveEditorToPreviousGroup")
+
+    ;; open index.html
+    (p/let [panel (vscode/window.createWebviewPanel "tutorial" "Tutorial" vscode/ViewColumn.Two #js {:enableScripts true})
+            uri (vscode/Uri.file (path/join vscode/workspace.rootPath "index.html"))
+            data (vscode/workspace.fs.readFile uri)
+            html (.decode (js/TextDecoder. "utf-8") data)]
+      (set! (.. panel -webview -html) (str html))))
+
+  ;; run command in terminal
+  (p/let [terminal (vscode/window.createTerminal #js {:name "tutorial"})]
+    (doto terminal
+      (.show true)
+      (.sendText "make index.html"))))
+
 (defn- main []
   (clear-disposables!)
   (push-disposable
@@ -31,7 +54,8 @@
     (vscode/workspace.onDidOpenTextDocument
       (fn [doc]
         (doto (joyride/output-channel)
-          (.appendLine (str (.-languageId doc) (.-fileName doc)))))))
+          (.appendLine (str "on-did-open-text-document " (.-languageId doc) " " (.-fileName doc)))))))
+    (tutorial)
     (dfd/main))
 
 (when (= (joyride/invoked-script) joyride/*file*)
