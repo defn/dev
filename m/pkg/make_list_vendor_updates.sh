771e0d4a806d6794a11d4da8b8dc2eca0e719427
defnn <yk-22610028@defn.sh>
2023-09-11 13:56:37 -0700

    ignmore more

--- m/pkg/make_list_vendor_updates.sh
+++ m/pkg/make_list_vendor_updates.sh
@@ -18,7 +18,7 @@ function main {
 		if [[ ${current} != "${latest}" ]]; then
 			echo "${slug} ${current} ${latest} ${real_tag}"
 		fi
-	done | (grep -E -v 'kustomize/ 5.0.3 5.1|coder-compat/|linkerd/ 2.14.0 2.12.6|argocd/ 2.8.3 2.6.15|hof/ 0.6.8 0.6.9-' || true)
+	done | (grep -E -v 'kustomize/ 5.0.3 5.1|coder-compat/|linkerd/ 2.14.0 2.1[23]|argocd/ 2.8.3 2.[567]|linkerd/ 2.14.0 23' || true)
 }
 
 main "$@"
