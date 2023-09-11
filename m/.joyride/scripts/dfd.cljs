73d3ca0ec7d87f65b43fb174db79f86b05829ad0
amanibhavam <iam@defn.sh>
2023-09-11 06:31:06 +0000

    no need for all these joyride scripts

--- m/.joyride/scripts/dfd.cljs
+++
@@ -1,9 +0,0 @@
-(ns dfd
-  (:require [joyride.core :as joyride]))
-
-(defn main []
-  (doto (joyride/output-channel)
-    (.appendLine (str "Invoked script: " (joyride/invoked-script)))))
-
-(when (= (joyride/invoked-script) joyride/*file*)
-  (main))
