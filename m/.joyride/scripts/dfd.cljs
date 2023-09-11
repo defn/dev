26dfd1367887383d8c35d80cf710495f49cb223b
amanibhavam <iam@defn.sh>
2023-09-11 03:48:57 +0000

    comments

--- m/.joyride/scripts/dfd.cljs
+++ m/.joyride/scripts/dfd.cljs
@@ -5,32 +5,26 @@
             [promesa.core :as p]))
 
 (defn main []
-    (doto (joyride/output-channel)
-      (.appendLine "==============================================================")
-      (.appendLine (str "Invoked script: " (joyride/invoked-script)))
-      (.appendLine "=============================================================="))
+  ; open index.cue
+  (p/let [doc (vscode/workspace.openTextDocument (path/join vscode/workspace.rootPath "index.cue"))
+          meh (vscode/window.showTextDocument doc #js {:preview false, :preserveFocus false, :viewColumn: vscode/ViewColumn.One})]
 
+    ; split editor TODO does this execute async? if so, then it's not guaranteed the tutorial loads in column two
+    (vscode/commands.executeCommand "workbench.action.moveEditorToPreviousGroup")
 
-    (p/let [doc (vscode/workspace.openTextDocument (path/join vscode/workspace.rootPath "index.cue"))
-            meh (vscode/window.showTextDocument doc #js {:preview false, :preserveFocus false, :viewColumn: vscode/ViewColumn.One})]
+    ; open index.html
+    (p/let [panel (vscode/window.createWebviewPanel "tutorial" "Tutorial" vscode/ViewColumn.Two #js {:enableScripts true})
+            uri (vscode/Uri.file (path/join vscode/workspace.rootPath "index.html"))
+            data (vscode/workspace.fs.readFile uri)
+            html (.decode (js/TextDecoder. "utf-8") data)]
+      (set! (.. panel -webview -html) (str html))))
 
-      (comment "TODO does this execute async? if so, then it's not guaranteed the tutorial loads in column two")
-      (vscode/commands.executeCommand "workbench.action.moveEditorToPreviousGroup")
-
-      (p/let [panel (vscode/window.createWebviewPanel "tutorial" "Tutorial" vscode/ViewColumn.Two #js {:enableScripts true})
-              uri (vscode/Uri.file (path/join vscode/workspace.rootPath "index.html"))
-              data (vscode/workspace.fs.readFile uri)
-              html (.decode (js/TextDecoder. "utf-8") data)]
-
-        (set! (.. panel -webview -html) (str html))))
-
-    (p/let [terminal (vscode/window.createTerminal #js {:name "tutorial"})]
-
-      (doto terminal
-        (.show true)
-        (.sendText "make index.html")))
-
-  )
+  ; run command in terminal
+  (p/let [terminal (vscode/window.createTerminal #js {:name "tutorial"})]
+    (doto terminal
+      (.show true)
+      (.sendText "make index.html")))
+)
 
 (when (= (joyride/invoked-script) joyride/*file*)
   (main))
