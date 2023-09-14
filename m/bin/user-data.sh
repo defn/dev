bc8373a1115a64b8a69c54b0e9f72fd6423d0f4c
amanibhavam <iam@defn.sh>
2023-09-13 22:27:31 -0700

    fix userdata

--- m/bin/user-data.sh
+++ m/bin/user-data.sh
@@ -4,10 +4,11 @@ set -eux
 export CODER_AGENT_AUTH="token"
 export CODER_AGENT_URL="$1"; shift
 export CODER_AGENT_TOKEN="$1"; shift
-shift
 
 cd
+set +x
 source .bash_profile
+set -x
 
 cd ~/m/pkg/coder
 exec nix develop --command coder agent
