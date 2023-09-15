6085c99ffe6d5430e85cd57180c7a483f13d8677
defnn <yk-22610028@defn.sh>
2023-09-14 23:37:49 -0700

    terraform/ 1.5.4 -> 1.5.7

--- m/pkg/terraform/flake.nix
+++ m/pkg/terraform/flake.nix
@@ -21,22 +21,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-FtnAUTfs9/QnqM+hTKnnwOc8sznyyI7jaIJKx7TQd+o="; # x86_64-linux
+        sha256 = "sha256-wO17wy7lKuJVr5mCyMiKekxhBIXPHVX+6wN+q3X6CCw="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-wIfBKYkYFhCSlnONK/g9Rv3KEQglowjVGS8zDoDa1xo="; # aarch64-linux
+        sha256 = "sha256-9LStfGtgiJYKZn40SVyuSQ+wcpR6n/Jmv1kp9TM1ZeQ="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-J6ynVRFD2YvoO3gPoAQLNZxDpnBL3UlRTqWC2UJ1Jxg="; # x86_64-darwin
+        sha256 = "sha256-sxDsDmJul5kADPyOMCR82CfPf4AwyOBAAlfH8RHpNTc="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-bWiw4cDqtfUl85XdruNg4uzN3/ScKvN9Ey6MBFtQAcU="; # aarch64-darwin
+        sha256 = "sha256-23wz6xpEa3OkQ+LFW1MoRfe3DNVhAL7EyW8Vz6tfUMs="; # aarch64-darwin
       };
     };
   };
