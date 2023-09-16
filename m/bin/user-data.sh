d3d8b033035d014054371e5feab3ba91b694411b
amanibhavam <iam@defn.sh>
2023-09-16 13:44:58 -0700

    fix

--- m/bin/user-data.sh
+++ m/bin/user-data.sh
@@ -17,7 +17,7 @@ while [[ -z "${CODER_AGENT_TOKEN}" ]]; do
         TOKEN="$(curl -sSL -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")"
         instance_id="$(curl -sSL -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)"
         cd ~/m/pkg/coder
-        exec nix develop --command true
+        nix develop --command true
         cd ~/m/pkg/awscli
         nix develop --command bash -c "aws secretsmanager get-secret-value --secret-id "${DFD_WORKSPACE_NAME}-${instance_id}" | jq -r '.SecretString | fromjson | .coder_agent_token' > /tmp/.coder-token" || true
     )
