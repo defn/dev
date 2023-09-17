8b90b8d0914b865e1fd97856cd84c69cb74a9bd4
amanibhavam <iam@defn.sh>
2023-09-17 05:58:38 +0000

    run in privileged mode

--- m/tf/mod/terraform-coder-defn-pod/pod.tf
+++ m/tf/mod/terraform-coder-defn-pod/pod.tf
@@ -111,6 +111,7 @@ resource "kubernetes_deployment" "main" {
         security_context {
           run_as_user = 1000
           fs_group    = 1000
+          privileged  = true
         }
 
         service_account_name = local.ns
