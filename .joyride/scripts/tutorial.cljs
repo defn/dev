5c010489104c08ae5e7b67a6d7287178c2d04a78
amanibhavam <iam@defn.sh>
2023-09-23 16:18:46 +0000

    joyride cnfigs for home and m

---
+++ .joyride/scripts/tutorial.cljs
@@ -0,0 +1,5 @@
+(ns tutorial)
+
+(def tutorial_filename "README.md")
+
+(def tutorial_webpage "m/l/dev/lesson/tutorial.html")
---
+++ m/.joyride/scripts/tutorial.cljs
@@ -0,0 +1,5 @@
+(ns tutorial)
+
+(def tutorial_filename "Makefile")
+
+(def tutorial_webpage "l/dev/lesson/tutorial.html")
