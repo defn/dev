2b31793a73e814b2c7a000a36465dfad971e3695
amanibhavam <iam@defn.sh>
2023-09-25 02:47:12 +0000

    surfac filte results

--- m/h/example/hooks/hook.sh
+++ m/h/example/hooks/hook.sh
@@ -22,42 +22,55 @@ function ctx_watch_event {
   cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .watchEvent'
 }
 
+function ctx_filter_result {
+  i="$1"; shift
+  cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .filterResult'
+}
+
 function hook {
   for i in $(ctx_index); do
     echo "====[ $i / $(ctx_index) ]======================================================"
-    echo "Binding : $(ctx_binding "$i")"
-    case "$(ctx_binding "$i")" in
-      onStartup)
+    case "$(ctx_type "$i")" in
+      null)
+        echo "Binding : $(ctx_binding "$i")"
         echo "Starting..."
         ;;
-      *)
+      Synchronization)
+        echo "Binding : $(ctx_binding "$i")"
         echo "Type: $(ctx_type "$i")"
-        case "$(ctx_type "$i")" in
-          Synchronization)
-            cat $BINDING_CONTEXT_PATH | jq -cr --arg i "$i" '.[$i | tonumber] | .objects | map(.object.kind)'
+        cat $BINDING_CONTEXT_PATH | jq -cr --arg i "$i" '.[$i | tonumber] | .objects | map(.object.kind)'
+        ;;
+      Event)
+        echo "Binding : $(ctx_binding "$i")"
+        echo "Type: $(ctx_type "$i")"
+        echo "WatchEvent: $(ctx_watch_event "$i")"
+        echo "FilterResult: $(ctx_filter_result "$i")"
+        case "$(ctx_watch_event "$i")" in
+          Added)
+            cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .object | del(.metadata.managedFields) | .metadata'
             ;;
-          Event)
-            echo "WatchEvent: $(ctx_watch_event "$i")"
-            case "$(ctx_watch_event "$i")" in
-              Added)
-                cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .object | del(.metadata.managedFields) | .metadata'
-                ;;
-              Modified)
-                cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .object | del(.metadata.managedFields) | .metadata'
-                echo $KUBERNETES_PATCH_PATH
-                ;;
-              Deleted)
-                cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .object | del(.metadata.managedFields) | .metadata'
-                ;;
-              *)
-                echo "Unknown watch event: $(ctx_watch_event "$i")"
-                ;;
-            esac
+          Modified)
+            cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .object | del(.metadata.managedFields) | .metadata'
+            echo $KUBERNETES_PATCH_PATH
+            ;;
+          Deleted)
+            cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .object | del(.metadata.managedFields) | .metadata'
             ;;
           *)
-            echo "Unknown type: $(ctx_type "$i")"
+            echo "Unknown watch event: $(ctx_watch_event "$i")"
+            cat $BINDING_CONTEXT_PATH | jq -cr --arg i "$i" '.[$i | tonumber]'
             ;;
         esac
+        ;;
+      Group*)
+        cat $BINDING_CONTEXT_PATH | jq -cr --arg i "$i" '.[$i | tonumber]'
+        ;;
+      Schedule)
+        cat $BINDING_CONTEXT_PATH | jq -cr --arg i "$i" '.[$i | tonumber]'
+        ;;
+      *)
+        echo "Unknown type: $(ctx_type "$i")"
+        cat $BINDING_CONTEXT_PATH | jq -cr --arg i "$i" '.[$i | tonumber]'
         ;;
     esac
     echo "================================================================"
