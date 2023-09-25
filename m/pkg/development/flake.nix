d2250b751420228da1349561aa65cad660263936
defnn <yk-22610028@defn.sh>
2023-09-25 21:45:33 +0000

    no ibazel

--- m/pkg/development/flake.nix
+++ m/pkg/development/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     buildifier.url = github:defn/dev/pkg-buildifier-6.3.3-1?dir=m/pkg/buildifier;
     bazelisk.url = github:defn/dev/pkg-bazelisk-1.18.0-1?dir=m/pkg/bazelisk;
-    ibazel.url = github:defn/dev/pkg-ibazel-0.23.7-2?dir=m/pkg/ibazel;
+    #ibazel.url = github:defn/dev/pkg-ibazel-0.23.7-2?dir=m/pkg/ibazel;
   };
 
   outputs = inputs: inputs.buildifier.inputs.pkg.main rec {
@@ -17,7 +17,7 @@
           graphviz
           inputs.bazelisk.defaultPackage.${ctx.system}
           inputs.buildifier.defaultPackage.${ctx.system}
-          inputs.ibazel.defaultPackage.${ctx.system}
+          #inputs.ibazel.defaultPackage.${ctx.system}
         ];
     };
   };
