a186f7c9d95402854fe4270865566615961dc101
amanibhavam <iam@defn.sh>
2023-09-25 00:19:53 +0000

    iterate over objects

--- m/h/example/hooks/hook.sh
+++ m/h/example/hooks/hook.sh
@@ -1,12 +1,53 @@
 #!/usr/bin/env bash
 
+set -efu
+
+function ctx_index  {
+  cat $BINDING_CONTEXT_PATH | jq -r 'keys[]'
+}
+
+function ctx_binding {
+  local i
+  i="$1"; shift
+  cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .binding'
+}
+
+function ctx_type {
+  i="$1"; shift
+  cat $BINDING_CONTEXT_PATH | jq -r --arg i "$i" '.[$i | tonumber] | .type'
+}
+
+function hook {
+  for i in $(ctx_index); do
+    echo "====[ $i ]======================================================"
+    case "$(ctx_binding "$i")" in
+      onStartup)
+        echo "Starting..."
+        ;;
+      kubernetes)
+        case "$(ctx_type "$i")" in
+          Synchronization)
+            cat $BINDING_CONTEXT_PATH | jq -cr '.[] | "objects: \(.objects//[] | .[].object.metadata | "\(.namespace) \(.name)")"'
+            echo $KUBERNETES_PATCH_PATH
+            ;;
+          *)
+            echo "Unknown type: $(ctx_type "$i")"
+            ;;
+        esac
+        ;;
+      *)
+        echo "Unknown binding: $(ctx_binding "$i")"
+        ;;
+    esac
+    echo "====[ $i ]======================================================"
+  done
+}
+
 function main {
-  echo "=========================================================="
-  cat $BINDING_CONTEXT_PATH | jq -cr '.[] | "objects: \(.objects//[] | .[].object.metadata | "\(.namespace) \(.name)")"'
-  cat $BINDING_CONTEXT_PATH | jq -cr '.[] | "keys: \(keys)"'
-  cat $BINDING_CONTEXT_PATH | jq -cr '.[] | "type: \(.type)"'
-  cat $BINDING_CONTEXT_PATH | jq -cr '.[] | "binding: \(.binding)"'
-  echo "=========================================================="
+  case "${1:-}" in
+    --config) cat hook.json ;;
+    *) hook ;;
+  esac
 }
 
 main "$@"
