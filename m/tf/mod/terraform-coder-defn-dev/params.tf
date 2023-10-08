b07e74b401d859812d4c1b293b202f802d76046c
amanibhavam <iam@defn.sh>
2023-10-08 10:58:10 -0700

    aceptroutes

--- m/tf/mod/terraform-coder-defn-dev/params.tf
+++ m/tf/mod/terraform-coder-defn-dev/params.tf
@@ -35,7 +35,7 @@ echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-dfd.conf
 echo 'fs.inotify.max_user_instances = 10000' | sudo tee -a /etc/sysctl.d/99-dfd.conf
 echo 'fs.inotify.max_user_watches = 524288' | sudo tee -a /etc/sysctl.d/99-dfd.conf
 sudo sysctl -p /etc/sysctl.d/99-dfd.conf
-sudo tailscale up --accept-dns=true --advertise-routes 10.43.0.0/16 --operator ubuntu --ssh --authkey "${var.tsauthkey}"
+sudo tailscale up --accept-dns=true --accept-routes=true --advertise-routes 10.43.0.0/16 --operator ubuntu --ssh --authkey "${var.tsauthkey}"
 sudo install -d -m 0755 ~ubuntu/m/c/dfd/openid
 tailscale serve https /openid ~/m/c/dfd/openid
 tailscale serve status
