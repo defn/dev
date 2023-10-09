fe2289672c07f9c4453e51c486d22b7f8c5e9241
amanibhavam <iam@defn.sh>
2023-10-09 14:48:52 -0700

    keep looping

--- m/bin/user-data.sh
+++ m/bin/user-data.sh
@@ -15,24 +15,26 @@ source .bash_profile
 set -x
 
 export CODER_AGENT_TOKEN
-CODER_AGENT_TOKEN=
 
-while [[ -z "${CODER_AGENT_TOKEN}" ]]; do
-    (
-        TOKEN="$(curl -sSL -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")"
-        instance_id="$(curl -sSL -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)"
-        cd ~/m/pkg/coder
-        nix develop --command true
-        cd ~/m/pkg/awscli
-        nix develop --command bash -c "aws secretsmanager get-secret-value --secret-id "${DFD_WORKSPACE_NAME}-${instance_id}" | jq -r '.SecretString | fromjson | .coder_agent_token' > /tmp/.coder-token" || true
-    )
-    CODER_AGENT_TOKEN="$(cat /tmp/.coder-token)"
-    echo rm -f /tmp/.coder-token
-    if [[ -z "${CODER_AGENT_TOKEN}" ]]; then
-        sleep 5
-    fi
+while true; do
+  CODER_AGENT_TOKEN=
+
+  cd ~/m/pkg/coder
+
+  TOKEN="$(curl -sSL -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")"
+  instance_id="$(curl -sSL -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)"
+
+  cd ~/m/pkg/coder
+  nix develop --command true
+
+  cd ~/m/pkg/awscli
+  nix develop --command bash -c "aws secretsmanager get-secret-value --secret-id "${DFD_WORKSPACE_NAME}-${instance_id}" | jq -r '.SecretString | fromjson | .coder_agent_token' > /tmp/.coder-token" || true
+  CODER_AGENT_TOKEN="$(cat /tmp/.coder-token)"
+  echo rm -f /tmp/.coder-token
+
+  cd ~/m/pkg/coder
+  nix develop --command coder agent || true
+
+  sleep 5
 done
-
-cd ~/m/pkg/coder
-exec nix develop --command coder agent
 
