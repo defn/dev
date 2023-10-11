baeaefe7579cd3f2c928e49ae55699ff181d0335
defnn <yk-22610028@defn.sh>
2023-10-11 16:12:11 +0000

    update pkg deps

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -13,7 +13,7 @@
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.0.0-3?dir=m/pkg/kubevirt;
     linkerd.url = github:defn/dev/pkg-linkerd-2.14.1-1?dir=m/pkg/linkerd;
     cilium.url = github:defn/dev/pkg-cilium-0.15.10-1?dir=m/pkg/cilium;
-    hubble.url = github:defn/dev/pkg-hubble-0.12.0-3?dir=m/pkg/hubble;
+    hubble.url = github:defn/dev/pkg-hubble-0.12.1-1?dir=m/pkg/hubble;
     tfo.url = github:defn/dev/pkg-tfo-2.1.1-1?dir=m/pkg/tfo;
   };
 
