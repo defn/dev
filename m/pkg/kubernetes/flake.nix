e148a5caff8d566df439f0ff43bc25132eef9f41
defnn <yk-22610028@defn.sh>
2023-10-20 16:33:00 +0000

    update pkg deps

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -9,12 +9,12 @@
     argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.0-1?dir=m/pkg/argoworkflows;
     argocd.url = github:defn/dev/pkg-argocd-2.8.4-1?dir=m/pkg/argocd;
     kn.url = github:defn/dev/pkg-kn-1.11.1-1?dir=m/pkg/kn;
-    vcluster.url = github:defn/dev/pkg-vcluster-0.16.3-1?dir=m/pkg/vcluster;
+    vcluster.url = github:defn/dev/pkg-vcluster-0.16.4-1?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.0.0-3?dir=m/pkg/kubevirt;
     linkerd.url = github:defn/dev/pkg-linkerd-2.14.1-1?dir=m/pkg/linkerd;
     cilium.url = github:defn/dev/pkg-cilium-0.15.11-1?dir=m/pkg/cilium;
     hubble.url = github:defn/dev/pkg-hubble-0.12.2-1?dir=m/pkg/hubble;
-    tfo.url = github:defn/dev/pkg-tfo-2.1.1-1?dir=m/pkg/tfo;
+    tfo.url = github:defn/dev/pkg-tfo-2.2.0-1?dir=m/pkg/tfo;
   };
 
   outputs = inputs: inputs.kubectl.inputs.pkg.main rec {
