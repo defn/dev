8886c216b853a3b5acc04da6dc995b4532b40473
amanibhavam <iam@defn.sh>
2023-10-30 22:54:56 -0700

    update pkg deps

--- m/pkg/vpn/flake.nix
+++ m/pkg/vpn/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    tailscale.url = github:defn/dev/pkg-tailscale-1.50.1-1?dir=m/pkg/tailscale;
+    tailscale.url = github:defn/dev/pkg-tailscale-1.52.0-1?dir=m/pkg/tailscale;
     cloudflared.url = github:defn/dev/pkg-cloudflared-2023.8.2-1?dir=m/pkg/cloudflared;
   };
 
