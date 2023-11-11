6ba5ab9712ea86f0c5ffd24c81fa7c973de77ead
amanibhavam <iam@defn.sh>
2023-11-11 15:04:08 -0800

    add dyff

---
+++ m/pkg/dyff/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs.pkg.url = github:defn/dev/pkg-pkg-0.0.12?dir=m/pkg/pkg;
+  outputs = inputs: inputs.pkg.downloadMain rec {
+    src = ./.;
+
+    url_template = input: "https://github.com/homeport/dyff/releases/download/v${input.vendor}/dyff_${input.vendor}_${input.os}_${input.arch}.tar.gz";
+
+    installPhase = pkg: ''
+      install -m 0755 -d $out $out/bin
+      install -m 0755 dyff $out/bin/dyff
+    '';
+
+    downloads = {
+
+
+      "x86_64-linux" = {
+        os = "linus";
+        arch = "amd64";
+        sha256 = "sha256-aaaaaG/E7jYcHRqF29RsJpNyt2xUE+Kpv9D3/lLGb3g="; # x86_64-linux
+      };
+      "aarch64-linux" = {
+        os = "linux";
+        arch = "arm64";
+        sha256 = "sha256-0TSLUNpFtV5ieH3F8auB9npD/PUAfJv614JOVdWZNBw="; # aarch64-linux
+      };
+      "x86_64-darwin" = {
+        os = "darwin";
+        arch = "amd64";
+        sha256 = "sha256-w80Ex84A2hUBnOl0myshqHBZ0Rfbo3GHZZo4zYag/SU="; # x86_64-darwin
+      };
+      "aarch64-darwin" = {
+        os = "darwin";
+        arch = "arm64";
+        sha256 = "sha256-9OXURuZBlcjJZnCKQEiw0dnciCnct8Z8ogd0nWeGSrY="; # aarch64-darwin
+      };
+    };
+  };
+}
