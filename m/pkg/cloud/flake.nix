49b50079aa25dddec290fac5c71239aeb9be5c2b
amanibhavam <iam@defn.sh>
2023-11-17 04:01:34 +0000

    update pkg deps

--- m/pkg/cloud/flake.nix
+++ m/pkg/cloud/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    terraform.url = github:defn/dev/pkg-terraform-1.6.3-1?dir=m/pkg/terraform;
+    terraform.url = github:defn/dev/pkg-terraform-1.6.4-1?dir=m/pkg/terraform;
     terraformdocs.url = github:defn/dev/pkg-terraformdocs-0.16.0-2?dir=m/pkg/terraformdocs;
     packer.url = github:defn/dev/pkg-packer-1.9.4-1?dir=m/pkg/packer;
     step.url = github:defn/dev/pkg-step-0.25.0-1?dir=m/pkg/step;
