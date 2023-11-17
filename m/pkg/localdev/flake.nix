fc739e073e0affba05224914755fa8510afc0a09
amanibhavam <iam@defn.sh>
2023-11-17 04:02:55 +0000

    update pkg deps

--- m/pkg/localdev/flake.nix
+++ m/pkg/localdev/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     tilt.url = github:defn/dev/pkg-tilt-0.33.6-1?dir=m/pkg/tilt;
-    gh.url = github:defn/dev/pkg-gh-2.38.0-1?dir=m/pkg/gh;
+    gh.url = github:defn/dev/pkg-gh-2.39.1-1?dir=m/pkg/gh;
     earthly.url = github:defn/dev/pkg-earthly-0.7.21-1?dir=m/pkg/earthly;
     oras.url = github:defn/dev/pkg-oras-1.1.0-1?dir=m/pkg/oras;
     buildkite.url = github:defn/dev/pkg-buildkite-3.59.0-1?dir=m/pkg/buildkite;
