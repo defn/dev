519b44142b3a16d7d848f4d9161b9d39a033ffb2
amanibhavam <iam@defn.sh>
2023-11-18 07:03:01 +0000

    node 18.18.2

--- m/pkg/nodedev/flake.nix
+++ m/pkg/nodedev/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.12?dir=m/pkg/pkg;
-    # https://github.com/NixOS/nixpkgs/commits/release-23.05
-    latest.url = github:NixOS/nixpkgs?rev=3fe694c4156b84dac12627685c7ae592a71e2206;
+    # https://github.com/NixOS/nixpkgs/commits/main
+    latest.url = github:NixOS/nixpkgs?rev=005617587ee2b7c003388b4539b9120ebcc90e44;
   };
 
   outputs = inputs: inputs.pkg.main rec {
