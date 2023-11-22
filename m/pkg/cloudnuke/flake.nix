abf4abc3c911be398da2aeb61805b3f0ab182ca9
amanibhavam <iam@defn.sh>
2023-11-22 23:15:06 +0000

    cloudnuke/ 0.32.0 -> 0.33.0

--- m/pkg/cloudnuke/flake.nix
+++ m/pkg/cloudnuke/flake.nix
@@ -19,22 +19,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-6lWGt7ClVX9k73gSpFQa5UbfaJ0nSS5/ND8RwiJqMSs="; # x86_64-linux
+        sha256 = "sha256-6LyECxQyEVhF9f4OOAbR2DMJR7KXDPGFz8WG9Lyt9ns="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-ky7urB6uZIt2nxBRY3Sh+zc2gQxw2BYRd9bB9R4LDd8="; # aarch64-linux
+        sha256 = "sha256-P6lYtWbP3l1ufpor71Sv/bmrsnP6RHb0IRuZssE0U2g="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-BC2sa/JBVZWZ2lBsLJwW0SLzsNmAcPZTsJmZ06Kn35g="; # x86_64-darwin
+        sha256 = "sha256-gdtnyvrr3rHE4nRjjTBaeu4ttA5L2mec0mYnXSEpFO8="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-4bcfF+wnvNOs+rCiXMQNl0h6GWs55W5ZQYarpOedvXI="; # aarch64-darwin
+        sha256 = "sha256-mOF9ujFK0LrWXT5VWEdCFE12gEa/H5vYPVg1k1d5pj0="; # aarch64-darwin
       };
     };
   };
