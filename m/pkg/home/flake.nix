b3895262138b5d4f9d9c346ea7ee2d8492e1f4a5
amanibhavam <iam@defn.sh>
2023-11-26 19:11:16 +0000

    prune registry

--- m/pkg/home/flake.nix
+++ m/pkg/home/flake.nix
@@ -79,6 +79,7 @@
       '';
 
       prune = ''
+          docker exec -ti registry /bin/registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true || true
           docker system prune -f --volumes
         	docker images | grep '<none>' | awk '{print $3}' | runmany 'docker rmi $1'
           docker system prune -f --volumes
