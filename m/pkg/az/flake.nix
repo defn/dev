6aa6cf1a1a5cb7754e229205808a4ddb42c22c3a
amanibhavam <iam@defn.sh>
2023-11-28 00:41:11 +0000

    update pkg deps

--- m/pkg/az/flake.nix
+++ m/pkg/az/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     cue.url = github:defn/dev/pkg-cue-0.6.0-2?dir=m/pkg/cue;
-    gum.url = github:defn/dev/pkg-gum-0.11.0-2?dir=m/pkg/gum;
+    gum.url = github:defn/dev/pkg-gum-0.12.0-1?dir=m/pkg/gum;
     glow.url = github:defn/dev/pkg-glow-1.5.1-5?dir=m/pkg/glow;
     dyff.url = github:defn/dev/pkg-dyff-1.6.0-2?dir=m/pkg/dyff;
   };
