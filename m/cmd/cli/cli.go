15dd4e6fd32ceed86b8e68654b28cdb8dddea140
amanibhavam <iam@defn.sh>
2023-11-28 04:57:24 +0000

    cutouts use each other

--- m/cmd/cli/cli.go
+++ m/cmd/cli/cli.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/defn/dev/m/command/api"
-	_ "github.com/defn/dev/m/command/dev"
 	_ "github.com/defn/dev/m/command/infra"
 	root "github.com/defn/dev/m/command/root"
 	_ "github.com/defn/dev/m/command/tui"
