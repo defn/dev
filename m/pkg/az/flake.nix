62c75d182d8ec51deb43ae44998b4ea6d3a88385
amanibhavam <iam@defn.sh>
2023-11-29 03:23:26 +0000

    add vhs

--- m/pkg/az/flake.nix
+++ m/pkg/az/flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     cue.url = github:defn/dev/pkg-cue-0.6.0-2?dir=m/pkg/cue;
     gum.url = github:defn/dev/pkg-gum-0.12.0-1?dir=m/pkg/gum;
+    vhs.url = github:defn/dev/pkg-vhs-0.12.0-1?dir=m/pkg/vhs;
     glow.url = github:defn/dev/pkg-glow-1.5.1-5?dir=m/pkg/glow;
     dyff.url = github:defn/dev/pkg-dyff-1.6.0-2?dir=m/pkg/dyff;
   };
@@ -13,9 +14,10 @@
       inherit src;
 
       propagatedBuildInputs = with ctx.pkgs; [
-        inputs.gum.defaultPackage.${ctx.system}
-        inputs.glow.defaultPackage.${ctx.system}
         inputs.cue.defaultPackage.${ctx.system}
+        inputs.gum.defaultPackage.${ctx.system}
+        inputs.vhs.defaultPackage.${ctx.system}
+        inputs.glow.defaultPackage.${ctx.system}
         inputs.dyff.defaultPackage.${ctx.system}
         jq
         yq
