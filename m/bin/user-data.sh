474d4d5067c21192a148a0729626e188fedc038b
amanibhavam <iam@defn.sh>
2023-12-02 22:49:14 -0800

    dont test dns

--- m/bin/user-data.sh
+++ m/bin/user-data.sh
@@ -7,13 +7,6 @@ shift
 export DFD_WORKSPACE_NAME="$1"
 shift
 
-while true; do
-	if test -n "$(dig +short "cache.nixos.org" || true)"; then
-		break
-	fi
-	sleep 5
-done
-
 cd
 
 ssh -o StrictHostKeyChecking=no git@github.com true || true
