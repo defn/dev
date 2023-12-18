e05b75bdb69afc1de4a2eca4d19f4e4b4f7e4c48
amanibhavam <iam@defn.sh>
2023-12-18 22:23:20 +0000

    update pkg deps

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -12,7 +12,7 @@
     vcluster.url = github:defn/dev/pkg-vcluster-0.18.1-1?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.0-1?dir=m/pkg/kubevirt;
     linkerd.url = github:defn/dev/pkg-linkerd-2.14.6-1?dir=m/pkg/linkerd;
-    cilium.url = github:defn/dev/pkg-cilium-0.15.18-1?dir=m/pkg/cilium;
+    cilium.url = github:defn/dev/pkg-cilium-0.15.19-1?dir=m/pkg/cilium;
     hubble.url = github:defn/dev/pkg-hubble-0.12.3-1?dir=m/pkg/hubble;
     tfo.url = github:defn/dev/pkg-tfo-2.2.0-1?dir=m/pkg/tfo;
   };
