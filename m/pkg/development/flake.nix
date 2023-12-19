01015910bc7464a56234de677549d2231c69a039
amanibhavam <iam@defn.sh>
2023-12-19 00:39:30 +0000

    update pkg no flake-utils

--- m/pkg/development/flake.nix
+++ m/pkg/development/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    buildifier.url = github:defn/dev/pkg-buildifier-6.4.0-1?dir=m/pkg/buildifier;
-    bazelisk.url = github:defn/dev/pkg-bazelisk-1.19.0-1?dir=m/pkg/bazelisk;
-    ibazel.url = github:defn/dev/pkg-ibazel-0.24.0-1?dir=m/pkg/ibazel;
+    buildifier.url = github:defn/dev/pkg-buildifier-6.4.0-2?dir=m/pkg/buildifier;
+    bazelisk.url = github:defn/dev/pkg-bazelisk-1.19.0-2?dir=m/pkg/bazelisk;
+    ibazel.url = github:defn/dev/pkg-ibazel-0.24.0-2?dir=m/pkg/ibazel;
   };
 
   outputs = inputs: inputs.buildifier.inputs.pkg.main rec {
