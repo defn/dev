5955e24dad830898930679eba86711c37177d907
amanibhavam <iam@defn.sh>
2023-12-19 00:42:07 +0000

    update pkg no flake-utils

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -1,20 +1,20 @@
 {
   inputs = {
-    kubectl.url = github:defn/dev/pkg-kubectl-1.26.7-5?dir=m/pkg/kubectl;
-    k3sup.url = github:defn/dev/pkg-k3sup-0.13.3-1?dir=m/pkg/k3sup;
-    k9s.url = github:defn/dev/pkg-k9s-0.29.1-1?dir=m/pkg/k9s;
-    helm.url = github:defn/dev/pkg-helm-3.13.3-1?dir=m/pkg/helm;
-    kustomize.url = github:defn/dev/pkg-kustomize-5.3.0-1?dir=m/pkg/kustomize;
-    stern.url = github:defn/dev/pkg-stern-1.27.0-1?dir=m/pkg/stern;
-    argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.2-1?dir=m/pkg/argoworkflows;
-    argocd.url = github:defn/dev/pkg-argocd-2.9.3-1?dir=m/pkg/argocd;
-    kn.url = github:defn/dev/pkg-kn-1.12.0-1?dir=m/pkg/kn;
-    vcluster.url = github:defn/dev/pkg-vcluster-0.18.1-1?dir=m/pkg/vcluster;
-    kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.0-1?dir=m/pkg/kubevirt;
-    linkerd.url = github:defn/dev/pkg-linkerd-2.14.6-1?dir=m/pkg/linkerd;
-    cilium.url = github:defn/dev/pkg-cilium-0.15.19-1?dir=m/pkg/cilium;
-    hubble.url = github:defn/dev/pkg-hubble-0.12.3-1?dir=m/pkg/hubble;
-    tfo.url = github:defn/dev/pkg-tfo-2.2.0-1?dir=m/pkg/tfo;
+    kubectl.url = github:defn/dev/pkg-kubectl-1.26.7-6?dir=m/pkg/kubectl;
+    k3sup.url = github:defn/dev/pkg-k3sup-0.13.3-2?dir=m/pkg/k3sup;
+    k9s.url = github:defn/dev/pkg-k9s-0.29.1-2?dir=m/pkg/k9s;
+    helm.url = github:defn/dev/pkg-helm-3.13.3-2?dir=m/pkg/helm;
+    kustomize.url = github:defn/dev/pkg-kustomize-5.3.0-2?dir=m/pkg/kustomize;
+    stern.url = github:defn/dev/pkg-stern-1.27.0-2?dir=m/pkg/stern;
+    argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.2-2?dir=m/pkg/argoworkflows;
+    argocd.url = github:defn/dev/pkg-argocd-2.9.3-2?dir=m/pkg/argocd;
+    kn.url = github:defn/dev/pkg-kn-1.12.0-2?dir=m/pkg/kn;
+    vcluster.url = github:defn/dev/pkg-vcluster-0.18.1-2?dir=m/pkg/vcluster;
+    kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.0-2?dir=m/pkg/kubevirt;
+    linkerd.url = github:defn/dev/pkg-linkerd-2.14.6-2?dir=m/pkg/linkerd;
+    cilium.url = github:defn/dev/pkg-cilium-0.15.19-2?dir=m/pkg/cilium;
+    hubble.url = github:defn/dev/pkg-hubble-0.12.3-2?dir=m/pkg/hubble;
+    tfo.url = github:defn/dev/pkg-tfo-2.2.0-2?dir=m/pkg/tfo;
   };
 
   outputs = inputs: inputs.kubectl.inputs.pkg.main rec {
