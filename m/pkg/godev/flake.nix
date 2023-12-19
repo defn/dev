86a4921878810037c001761309a78e311a7ab269
amanibhavam <iam@defn.sh>
2023-12-19 00:40:01 +0000

    update pkg no flake-utils

--- m/pkg/godev/flake.nix
+++ m/pkg/godev/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    buf.url = github:defn/dev/pkg-buf-1.28.1-1?dir=m/pkg/buf;
-    cosign.url = github:defn/dev/pkg-cosign-2.2.2-1?dir=m/pkg/cosign;
-    goreleaser.url = github:defn/dev/pkg-goreleaser-1.22.1-1?dir=m/pkg/goreleaser;
+    buf.url = github:defn/dev/pkg-buf-1.28.1-2?dir=m/pkg/buf;
+    cosign.url = github:defn/dev/pkg-cosign-2.2.2-2?dir=m/pkg/cosign;
+    goreleaser.url = github:defn/dev/pkg-goreleaser-1.22.1-2?dir=m/pkg/goreleaser;
     # https://github.com/NixOS/nixpkgs/commits/main
     latest.url = github:NixOS/nixpkgs?rev=005617587ee2b7c003388b4539b9120ebcc90e44;
   };
