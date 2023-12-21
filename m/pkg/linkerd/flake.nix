2984d7f474cb3300f85eec4e068fe39f1ee07671
amanibhavam <iam@defn.sh>
2023-12-21 19:40:45 +0000

    upgrade linkerd cli

--- m/pkg/linkerd/flake.nix
+++ m/pkg/linkerd/flake.nix
@@ -19,22 +19,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "-amd64";
-        sha256 = "sha256-e210NX5k91jx5bpRK7JRixdcdMydBtRs0KTW9Y+R2+k="; # x86_64-linux
+        sha256 = "sha256-vDTMbPuTa+gQHgIusnGH6mFYdrVuDpxr0sFl4TXVqtM="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "-arm64";
-        sha256 = "sha256-GG1+SclaAaLEBhKIVj1qQbX6Pl9e/7A7wuZRiBV3cWY="; # aarch64-linux
+        sha256 = "sha256-zl1dkDejaa9/gP8+2GVXWMC3WhQenjye1kk3WS24YFI="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "";
-        sha256 = "sha256-hWQfPq8hHgIjvWeWih7OiQc/84zBT1ojI9R4RHlvdhE="; # x86_64-darwin
+        sha256 = "sha256-DWOQV2zLbntHl0R7R23MzzA/cj6KPEFUC4rqLuqZ/mY="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "-arm64";
-        sha256 = "sha256-EwG5uQtXkSgpvSsZKkt7cqBvb0oKOovDQbT/YGqtEcM="; # aarch64-darwin
+        sha256 = "sha256-3aEeM2sHTvhRI49KLiWCgrtPpO5QH8tDoSquk+SeHko="; # aarch64-darwin
       };
     };
   };
