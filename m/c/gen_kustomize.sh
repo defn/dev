e296d8ac266b6818f30a38e900a81bf2dd122a34
amanibhavam <iam@defn.sh>
2023-12-21 06:28:09 +0000

    use release.py in bazel build

--- m/c/gen_kustomize.sh
+++ m/c/gen_kustomize.sh
@@ -3,18 +3,18 @@
 set -euo pipefail
 
 function main {
+	local release_py
 	local app
 	local out
 
+	release_py="$1"
+	shift
 	app="$1"
 	shift
 	out="$1"
 	shift
 
-	jq -r '.gen | to_entries[] | .key as $dir | .value | to_entries[] | "\($dir)/\(.key) \(.value | @base64)"' <"${app}" | while read -r fname content; do
-		mkdir -p "${out}/${fname%/*}"
-		echo "${content}" | base64 -d >"${out}/${fname}"
-	done
+	jq --arg out "${out}" -r '.gen | to_entries[] | .key as $dir | .value | to_entries[] | "\($out)/\($dir)/\(.key) \(.value | @base64) 0"' <"${app}" | $release_py
 }
 
 main "$@"
