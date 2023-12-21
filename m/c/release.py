63014fa8d0d96223934c8ab9b00990ce5ba17f0a
amanibhavam <iam@defn.sh>
2023-12-21 06:28:09 +0000

    use release.py in bazel build

--- m/c/release.py
+++ m/c/release.py
@@ -6,8 +6,8 @@ import sys
 import re
 
 def process_line(line):
-    # Split the line into filename and base64 encoded string
-    filename, encoded_data = line.split(' ', 1)
+    # Split the line into escape flag, filename and base64 encoded string
+    filename, encoded_data, escape = line.split(' ')
 
     # Create a directory with the filename if it doesn't exist
     os.makedirs(os.path.dirname(filename), exist_ok=True)
@@ -16,7 +16,10 @@ def process_line(line):
     decoded_data = base64.b64decode(encoded_data)
 
     # Transform the text using the sed substitution
-    transformed_data = re.sub(r'{{', '{{ "{{" }}', decoded_data.decode('utf-8'))
+    if escape == "1":
+        transformed_data = re.sub(r'{{', '{{ "{{" }}', decoded_data.decode('utf-8'))
+    else:
+        transformed_data = decoded_data.decode('utf-8')
 
     # Write the transformed data to the corresponding file
     with open(filename, 'w') as file:
