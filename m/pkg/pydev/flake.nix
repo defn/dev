032b276907e98e8ac8d6dffbf36eebe58355c548
amanibhavam <iam@defn.sh>
2023-12-21 17:25:20 +0000

    pydev flake

---
+++ m/pkg/pydev/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    pkg.url = github:defn/dev/pkg-pkg-0.0.13?dir=m/pkg/pkg;
+    # https://github.com/NixOS/nixpkgs/commits/main
+    latest.url = github:NixOS/nixpkgs?rev=005617587ee2b7c003388b4539b9120ebcc90e44;
+  };
+
+  outputs = inputs: inputs.pkg.main rec {
+    src = ./.;
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs =
+        with (import inputs.latest { system = ctx.system; }); [
+          (python311.withPackages (ps: with ps; [ pipx ]))
+        ];
+    };
+  };
+}
