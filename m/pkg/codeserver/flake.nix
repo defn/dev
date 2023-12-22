d1a08b0ca3433bbd245aed6e6e76cf6d9f39958e
amanibhavam <iam@defn.sh>
2023-12-22 05:48:25 +0000

    codeserver/ 4.19.1 -> 4.20.0

--- m/pkg/codeserver/flake.nix
+++ m/pkg/codeserver/flake.nix
@@ -30,22 +30,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-eSouvCo6hXdXmSSuoUncEE/rILE7mMSjSRT5pjQSMng="; # x86_64-linux
+        sha256 = "sha256-6b8wXG1LOmhWJE8dlAk2apVm3lz2NEwHamgahkpAhdQ="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-CECY0N5dFmzKHpWH3/7fOSIraxaTOtYliiVhN/XvFPA="; # aarch64-linux
+        sha256 = "sha256-DDyD7VkVFaxIQLh/r5AY0KYzK8zegn6EkCF7KoENDmI="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "macos";
         arch = "amd64";
-        sha256 = "sha256-odJOk+6gPxteuX6e4NZN+O8bqBMqWDbnvBs/uZ5jyxQ="; # x86_64-darwin
+        sha256 = "sha256-YmkaLM2NMgOWtKVTjppYZmfBsNZcT1UunoRUxfhEJ08="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "macos";
         arch = "amd64"; # code-server not avaialble for darwin arm64
-        sha256 = "sha256-odJOk+6gPxteuX6e4NZN+O8bqBMqWDbnvBs/uZ5jyxQ="; # aarch64-darwin
+        sha256 = "sha256-YmkaLM2NMgOWtKVTjppYZmfBsNZcT1UunoRUxfhEJ08="; # aarch64-darwin
       };
     };
   };
