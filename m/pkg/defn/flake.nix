e6f45607db8f1dc4b96c06e359fce3cf1cde9774
amanibhavam <iam@defn.sh>
2023-12-24 19:27:09 +0000

    update

--- m/pkg/defn/flake.nix
+++ m/pkg/defn/flake.nix
@@ -1,14 +1,14 @@
 {
   inputs = {
     nix.url = github:defn/dev/pkg-nix-0.0.54?dir=m/pkg/nix;
-    secrets.url = github:defn/dev/pkg-secrets-0.0.58?dir=m/pkg/secrets;
+    secrets.url = github:defn/dev/pkg-secrets-0.0.60?dir=m/pkg/secrets;
     utils.url = github:defn/dev/pkg-utils-0.0.54?dir=m/pkg/utils;
-    development.url = github:defn/dev/pkg-development-0.0.79?dir=m/pkg/development;
-    cloud.url = github:defn/dev/pkg-cloud-0.0.213?dir=m/pkg/cloud;
-    kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.201?dir=m/pkg/kubernetes;
-    remotedev.url = github:defn/dev/pkg-remotedev-0.0.30?dir=m/pkg/remotedev;
-    localdev.url = github:defn/dev/pkg-localdev-0.0.211?dir=m/pkg/localdev;
-    godev.url = github:defn/dev/pkg-godev-0.0.123?dir=m/pkg/godev;
+    development.url = github:defn/dev/pkg-development-0.0.80?dir=m/pkg/development;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.214?dir=m/pkg/cloud;
+    kubernetes.url = github:defn/dev/pkg-kubernetes-0.0.202?dir=m/pkg/kubernetes;
+    remotedev.url = github:defn/dev/pkg-remotedev-0.0.31?dir=m/pkg/remotedev;
+    localdev.url = github:defn/dev/pkg-localdev-0.0.212?dir=m/pkg/localdev;
+    godev.url = github:defn/dev/pkg-godev-0.0.124?dir=m/pkg/godev;
     jsdev.url = github:defn/dev/pkg-jsdev-0.0.62?dir=m/pkg/jsdev;
     pydev.url = github:defn/dev/pkg-pydev-0.0.125?dir=m/pkg/pydev;
     shell.url = github:defn/dev/pkg-shell-0.0.63?dir=m/pkg/shell;
