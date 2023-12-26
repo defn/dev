08040d6b8c1f12d1a43bfa147aa84783dee0244e
amanibhavam <iam@defn.sh>
2023-12-25 20:28:52 -0800

    dont mess with ec2 module

--- m/tf/mod/terraform-coder-defn-dev/params.tf
+++ m/tf/mod/terraform-coder-defn-dev/params.tf
@@ -2,60 +2,64 @@ variable "tsauthkey" {}
 
 locals {
   aws_ec2_count = data.coder_parameter.provider.value == "aws-ec2" ? 1 : 0
-}
 
-data "coder_parameter" "nix_volume_size" {
-  name         = "nix_volume_size"
-  display_name = "nix volume size"
-  description  = "The size of the nix volume to create for the workspace in GB"
-  type         = "number"
-  default      = "100"
-  icon         = "https://raw.githubusercontent.com/matifali/logos/main/database.svg"
-  mutable      = true
-  validation {
-    min = 100
-    max = 200
-  }
-}
+  username = "ubuntu"
 
-data "coder_parameter" "workdir" {
-  name         = "workdir"
-  display_name = "Working directory"
-  description  = "Working directory"
-  type         = "string"
-  default      = "/home/${local.username}/m"
-  icon         = "https://raw.githubusercontent.com/matifali/logos/main/database.svg"
-  mutable      = true
-}
+  coder_name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
 
-data "coder_parameter" "source_rev" {
-  name         = "source_rev"
-  display_name = "Source revision"
-  description  = "Source revision"
-  type         = "string"
-  default      = "main"
-  icon         = "https://raw.githubusercontent.com/matifali/logos/main/database.svg"
-  mutable      = true
-}
+  user_data = <<EOT
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
 
-data "coder_parameter" "prefix" {
-  name         = "prefix"
-  display_name = "Workspace name prefix"
-  description  = "Workspace name prefix"
-  type         = "string"
-  default      = "coder"
-  icon         = "https://raw.githubusercontent.com/matifali/logos/main/database.svg"
-  mutable      = false
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+hostname: ${local.coder_name}
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+
+set -x
+
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-dfd.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-dfd.conf
+echo 'fs.inotify.max_user_instances = 10000' | sudo tee -a /etc/sysctl.d/99-dfd.conf
+echo 'fs.inotify.max_user_watches = 524288' | sudo tee -a /etc/sysctl.d/99-dfd.conf
+sudo sysctl -p /etc/sysctl.d/99-dfd.conf
+
+while true; do
+  if test -n "$(dig +short "cache.nixos.org" || true)"; then
+    break
+  fi
+  sleep 5
+done
+
+sudo tailscale up --accept-dns=true --accept-routes=true --operator ubuntu --ssh --authkey "${var.tsauthkey}"
+
+nohup sudo -H -E -u ${local.username} bash -c 'cd && (git pull || true) && cd m && exec bin/user-data.sh ${data.coder_workspace.me.access_url} ${local.coder_name}' >/tmp/cloud-init.log 2>&1 &
+disown
+--//--
+EOT
 }
 
 data "coder_parameter" "provider" {
   name         = "provider"
   display_name = "Provider"
   description  = "The service provider to deploy the workspace in"
+  default      = "aws-ec2"
   icon         = "/emojis/1f30e.png"
   mutable      = true
-
-  default = "aws-ec2"
 
   option {
     name  = "Amazon Web Services VM"
@@ -89,3 +93,16 @@ data "coder_parameter" "instance_type" {
   }
 }
 
+data "coder_parameter" "nix_volume_size" {
+  name         = "nix_volume_size"
+  display_name = "nix volume size"
+  description  = "The size of the nix volume to create for the workspace in GB"
+  type         = "number"
+  default      = "100"
+  icon         = "https://raw.githubusercontent.com/matifali/logos/main/database.svg"
+  mutable      = true
+  validation {
+    min = 100
+    max = 200
+  }
+}
