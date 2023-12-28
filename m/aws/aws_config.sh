7a807812d6ec5b5108be2270365eed677c1e3b1c
amanibhavam <iam@defn.sh>
2023-12-28 07:36:03 +0000

    aws_config to lib.sh

--- m/aws/aws_config.sh
+++ m/aws/aws_config.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-set -efu
+function main {
+	local config="${in[config]}"
+	local template_org="${in[template_org]}"
+	local template_account="${in[template_account]}"
 
-while read -r org url region; do
-	perl -pe "s{ORG}{${org}}g; s{REGION}{${region}}g" <"$2"
-	while read -r org2 member account_id sso_role; do
-		if [[ ${org2} == "${org}" ]]; then
-			echo "aws config: ${org} ${member} ${account_id}" 1>&2
-			perl -pe "s{ORG}{${org}}g; s{MEMBER}{${member}}g; s{REGION}{${region}}g; s{URL}{${url}}g; s{ACCOUNT_ID}{${account_id}}g; s{SSO_ROLE}{${sso_role}}g;" <"$3"
-			echo
-			echo
-		fi
-	done < <(jq -r '.org | . as $org | keys[] | . as $o | $org[$o].account | keys[] | . as $member | "\($o) \($org[$o].account[$member].account) \($org[$o].account[$member].id) \($org[$o].account[$member].sso_role)"' <"$1" || true)
-done < <(jq -r '.org | . as $org | keys[] | "\(.) \($org[.].url) \($org[.].region)"' <"$1" || true) >"$4.tmp"
+	while read -r org url region; do
+		perl -pe "s{ORG}{${org}}g; s{REGION}{${region}}g" <"${template_org}"
+		while read -r org2 member account_id sso_role; do
+			if [[ ${org2} == "${org}" ]]; then
+				echo "aws config: ${org} ${member} ${account_id}" 1>&2
+				perl -pe "s{ORG}{${org}}g; s{MEMBER}{${member}}g; s{REGION}{${region}}g; s{URL}{${url}}g; s{ACCOUNT_ID}{${account_id}}g; s{SSO_ROLE}{${sso_role}}g;" <"${template_account}"
+				echo
+				echo
+			fi
+		done < <(jq -r '.org | . as $org | keys[] | . as $o | $org[$o].account | keys[] | . as $member | "\($o) \($org[$o].account[$member].account) \($org[$o].account[$member].id) \($org[$o].account[$member].sso_role)"' <"${config}" || true)
+	done < <(jq -r '.org | . as $org | keys[] | "\(.) \($org[.].url) \($org[.].region)"' <"${config}" || true) >"${out}"
+}
 
-mv -f "$4.tmp" "$4"
+source b/lib/lib.sh
