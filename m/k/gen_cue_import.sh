68983848062a46d809dd8045876a515fe7703739
amanibhavam <iam@defn.sh>
2024-01-05 03:15:30 +0000

    move c/k/y/APP.cue to bazel

---
+++ m/k/gen_cue_import.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function main {
+	local app="${in[app]}"
+
+	(
+		cue import "${app}" -p y --with-context \
+			-l '"res"' -l 'strings.ToLower(data.kind)' \
+			-l "\"$(basename ${app} .yaml)\"" \
+			-l 'strings.ToLower(*data.metadata.namespace | "cluster")' \
+			-l data.metadata.name --outfile - \
+			| perl -pe 's{^\s*\w+:\s+null\s*}{}' \
+	) > "${out}"
+}
+
+source b/lib/lib.sh
