8ba7587288a29adfde5829917eb789c2450a5e68
amanibhavam <iam@defn.sh>
2024-01-05 03:15:30 +0000

    move c/k/y/APP.cue to bazel

--- m/k/gen_kustomize_build.sh
+++ m/k/gen_kustomize_build.sh
@@ -3,12 +3,8 @@
 function main {
 	local app="${in[app]}"
 
-	touch "${out}"
+	kustomize build --load-restrictor LoadRestrictionsNone --enable-helm "${app}" >"${out}"
 
-	if test -e "k/${app}/kustomization.yaml"; then
-		rm -rf "k/${app}/chart"
-		kustomize build --load-restrictor LoadRestrictionsNone --enable-helm "k/${app}" >"${out}"
-	fi
 }
 
 source b/lib/lib.sh
