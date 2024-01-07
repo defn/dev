e83a09b660ca4e25f4cf98fc387e14f742417325
amanibhavam <iam@defn.sh>
2024-01-07 22:30:54 +0000

    fix cue dep

--- m/k/gen_deploy_yaml.sh
+++ m/k/gen_deploy_yaml.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 function main {
-	(
-		set +f
-		cue export --out json -e cached_yaml k/*.cue "$@" |
-			jq -r 'to_entries[] | .value'
-	) >"${out}"
+	local cue="${shome}/${in[cue]}"
+
+	set +f
+	"${cue}" export --out json -e cached_yaml k/*.cue "$@" \
+		| jq -r 'to_entries[] | .value' \
+	>"${out}"
 }
 
 source b/lib/lib.sh
