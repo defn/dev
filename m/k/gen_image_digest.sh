c8ab04e5b0ca9c5f80bfc7673bd6eee2d94825a7
amanibhavam <iam@defn.sh>
2024-01-08 00:55:51 +0000

    fix builds

---
+++ m/k/gen_image_digest.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function main {
+	local app="${in[app]}"
+	local cue="${in[cue]}"
+
+	(
+		set +f
+		echo package r
+		"${cue}" export --out json -e images "${app}" k/*.cue | jq -r '.[]' \
+			| sort -u \
+			| runmany 8 'echo cache: \"$1\": \"$(echo $1 | perl -pe '"'"'s{[:@].*$}{}'"'"')@$(skopeo inspect docker://${1%%@sha256*} | jq -r .Digest)\"' \
+			| sort \
+			| while read -r l; do
+				echo
+				echo "$l"
+			done
+	) >"${out}"
+}
+
+source b/lib/lib.sh
