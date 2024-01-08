771215b5cfba69c4bee4093905f2fc49f3936ab4
amanibhavam <iam@defn.sh>
2024-01-08 07:09:59 +0000

    getting ready to use helm chart versions in env

--- m/k/gen_deploy_yaml.sh
+++ m/k/gen_deploy_yaml.sh
@@ -3,8 +3,13 @@
 function main {
 	local cue="${shome}/${in[cue]}"
 
+	local cue_import="$1"; shift
+	local image_digest="$1"; shift
+
+	local out="$1"; shift
+
 	set +f
-	"${cue}" export --out json -e cached_yaml k/*.cue "$@" \
+	"${cue}" export --out json -e cached_yaml k/*.cue "${cue_import}" "${image_digest}" \
 		| jq -r 'to_entries[] | .value' \
 	>"${out}"
 }
