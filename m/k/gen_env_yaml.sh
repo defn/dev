fc4f139ca9fbc1fcbbe7bd343c681dab70e0971f
amanibhavam <iam@defn.sh>
2024-01-08 09:12:30 +0000

    supply versions

--- m/k/gen_env_yaml.sh
+++ m/k/gen_env_yaml.sh
@@ -9,12 +9,20 @@ function main {
 	local out="$1"; shift
 
 	cat "${cue_import}" | grep -v 'targetRevision: "app-version-not-found"' > cue_import.cue
+	touch cue_versions.cue
 
-	cluster="coder-amanibhavam-district"
-	name="kyverno"
-	version="0.0.2"
+	for h in "$@"; do
+		mkdir chart
+		tar xfz "${h}" -C chart
 
-	echo "res: application: \"${cluster}-cluster-env\": argocd: \"${cluster}-cluster-${name}\": spec: source: targetRevision: \"${version}\"" > cue_versions.cue
+		local cluster=$(set +f; grep ^name: chart/*/Chart.yaml | awk '{print $2}' | cut -d- -f1-3)
+		local name=$(set +f; grep ^name: chart/*/Chart.yaml | awk '{print $2}' | cut -d- -f5-)
+		local version=$(set +f; grep ^version: chart/*/Chart.yaml | awk '{print $2}')
+
+		echo "res: application: \"${cluster}-cluster-env\": argocd: \"${cluster}-cluster-${name}\": spec: source: targetRevision: \"${version}\"" >> cue_versions.cue
+
+		rm -rf chart
+	done
 
 	set +f
 	"${cue}" export --out json -e cached_yaml k/*.cue "cue_import.cue" "cue_versions.cue" "${image_digest}" \
