3f88c3874ea3a132323ed6e65e9e0fbe0694de17
amanibhavam <iam@defn.sh>
2024-01-10 18:22:23 +0000

    opentofu/ 1.6.0-rc1 -> 1.6.0 [skip ci]

--- m/pkg/opentofu/flake.nix
+++ m/pkg/opentofu/flake.nix
@@ -21,22 +21,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-yBD6ZNcUffQnJoE2DvW8mjZuV0v9q9a4ixjiw2w7Gu4="; # x86_64-linux
+        sha256 = "sha256-uWw9EjW8T9U7GZF1gYo1ZC5Qy8a4K4Qi3KtZJA0G2IU="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-wExSxa/UoEF/z5x60Nf/BXSJg1Sa6DDuRRunfeC9d2w="; # aarch64-linux
+        sha256 = "sha256-wgWMHi8iItgd4K0pHqzj40MwzHt0MV6qy2CltkAlJ7E="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-kc8tVGxH+MXG+C+MD4DyOBKiWKu8qnzBPJmlIa8S/cs="; # x86_64-darwin
+        sha256 = "sha256-DZT9fSvJ0Pcn3YOdm8z0x/Kl0LDHYUcv3RgJQplHBBs="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-/nCyTnCG0wnOWxa2PsMtPpMuGBtxMCjSQA+nozO3G6E="; # aarch64-darwin
+        sha256 = "sha256-xmj1bVB5kIv0KItF9z0iOjvheuRTNvQcFPVHKwhQKQY="; # aarch64-darwin
       };
     };
   };
