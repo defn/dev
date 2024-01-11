5427915a53895193d1a7262148ab7dc91cd54efa
amanibhavam <iam@defn.sh>
2024-01-11 00:57:24 +0000

    update pkg deps [skip ci]

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     kubectl.url = github:defn/dev/pkg-kubectl-1.26.7-8?dir=m/pkg/kubectl;
     k3sup.url = github:defn/dev/pkg-k3sup-0.13.5-1?dir=m/pkg/k3sup;
-    k9s.url = github:defn/dev/pkg-k9s-0.31.3-1?dir=m/pkg/k9s;
+    k9s.url = github:defn/dev/pkg-k9s-0.31.4-1?dir=m/pkg/k9s;
     helm.url = github:defn/dev/pkg-helm-3.13.3-4?dir=m/pkg/helm;
     kustomize.url = github:defn/dev/pkg-kustomize-5.3.0-4?dir=m/pkg/kustomize;
     stern.url = github:defn/dev/pkg-stern-1.28.0-1?dir=m/pkg/stern;
@@ -12,7 +12,7 @@
     vcluster.url = github:defn/dev/pkg-vcluster-0.18.1-4?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.1-1?dir=m/pkg/kubevirt;
     linkerd.url = github:defn/dev/pkg-linkerd-2.14.8-1?dir=m/pkg/linkerd;
-    cilium.url = github:defn/dev/pkg-cilium-0.15.19-4?dir=m/pkg/cilium;
+    cilium.url = github:defn/dev/pkg-cilium-0.15.20-1?dir=m/pkg/cilium;
     hubble.url = github:defn/dev/pkg-hubble-0.12.3-4?dir=m/pkg/hubble;
     tfo.url = github:defn/dev/pkg-tfo-2.2.0-4?dir=m/pkg/tfo;
     mirrord.url = github:defn/dev/pkg-mirrord-3.82.0-5?dir=m/pkg/mirrord;
