e3815bee711051739f584b81ff8fabb688be722c
amanibhavam <iam@defn.sh>
2024-01-11 08:57:34 +0000

    check if image already cached

--- m/k/gen_deploy_yaml.sh
+++ m/k/gen_deploy_yaml.sh
@@ -13,7 +13,14 @@ function main {
 
 	"${cue}" export --out json -e image_digests "${cue_import}" "${image_digest}" k/*.cue \
 		| jq -r '.[]' \
-		| runmany 4 2 'proxy=$2; skopeo copy docker://${proxy} docker://cache.defn.run:5000/${2} --multi-arch all --dest-tls-verify=false --insecure-policy'
+		| while read -r image_original image_digested; do
+			if [[ "$(skopeo inspect --tls-verify=false "docker://cache.defn.run:5000/${image_digested}" | jq -r .Digest)" != "${image_digested##*@}" ]]; then
+				echo "caching ${image_digested} to docker://cache.defn.run:5000/${image_digested%%@*}"
+				skopeo copy docker://${image_digested} docker://cache.defn.run:5000/${image_digested%%@*} --multi-arch all --dest-tls-verify=false --insecure-policy
+			else
+				echo "already cached ${image_digested} to docker://cache.defn.run:5000/${image_digested%%@*}"
+			fi
+		done
 
 	"${cue}" export --out json -e cached_yaml "${cue_import}" "${image_digest}" k/*.cue \
 		| jq -r 'to_entries[] | .value' \
