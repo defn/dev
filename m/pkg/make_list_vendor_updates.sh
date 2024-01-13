194466891e0403c5aaf6f82176878a13a8e7e244
amanibhavam <iam@defn.sh>
2024-01-13 01:15:56 +0000

    ignore lnkerd edge

--- m/pkg/make_list_vendor_updates.sh
+++ m/pkg/make_list_vendor_updates.sh
@@ -18,7 +18,7 @@ function main {
 		if [[ ${current} != "${latest}" ]]; then
 			echo "${slug} ${current} ${latest} ${real_tag}"
 		fi
-	done | (grep -E -v 'earthly/ 0.8.0-rc1 0.7|argocd/ 2.10.0-rc1 2.9|vhs/ 0.6.0 0.7.0|coder-compat/|linkerd/ 2.14.[0-9]+ 2.1[23]|argocd/ 2.8.3 2.[567]|linkerd/ 2.14.[0-9] 23|terraform/ 1.5.7 1.[34]|helm/ 3.12.3 3.13.0|argoworkflows/ 3.5.0 3.4|kn/ 1.12.0 1.11|cloudflared/ 2023.8.2 2023.10.0|vcluster/ 0.17.1 0.17.0 ' || true)
+	done | (grep -E -v 'earthly/ 0.8.0-rc1 0.7|argocd/ 2.10.0-rc1 2.9|vhs/ 0.6.0 0.7.0|coder-compat/|linkerd/ 2.14.[0-9]+ 2.1[23]|linkerd/ 2.14.[0-9]+ 24|argocd/ 2.8.3 2.[567]|linkerd/ 2.14.[0-9] 23|terraform/ 1.5.7 1.[34]|helm/ 3.12.3 3.13.0|argoworkflows/ 3.5.0 3.4|kn/ 1.12.0 1.11|cloudflared/ 2023.8.2 2023.10.0|vcluster/ 0.17.1 0.17.0 ' || true)
 }
 
 main "$@"
