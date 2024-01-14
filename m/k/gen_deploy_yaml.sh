36bbd081a9843552c8773d6b53e0226013a7efbd
amanibhavam <iam@defn.sh>
2024-01-14 21:18:57 +0000

    replace registry

--- m/k/gen_deploy_yaml.sh
+++ m/k/gen_deploy_yaml.sh
@@ -4,6 +4,7 @@
 
 function main {
 	local cue="${shome}/${in[cue]}"
+	local registry="$1"; shift
 
 	local cue_import="$1"; shift
 	local image_digest="$1"; shift
@@ -14,11 +15,11 @@ function main {
 	"${cue}" export --out json -e image_digests "${cue_import}" "${image_digest}" k/*.cue \
 		| jq -r '.[]' \
 		| while read -r image_original image_digested; do
-			if [[ "$(skopeo inspect --tls-verify=false "docker://cache.defn.run:5000/${image_digested}" | jq -r .Digest)" != "${image_digested##*@}" ]]; then
-				echo "caching ${image_digested} to docker://cache.defn.run:5000/${image_digested%%@*}"
-				skopeo copy docker://${image_digested} docker://cache.defn.run:5000/${image_digested%%@*} --multi-arch all --dest-tls-verify=false --insecure-policy
+			if [[ "$(skopeo inspect --tls-verify=false "docker://${registry}/${image_digested}" | jq -r .Digest)" != "${image_digested##*@}" ]]; then
+				echo "caching ${image_digested} to docker://${registry}/${image_digested%%@*}"
+				skopeo copy docker://${image_digested} docker://${registry}/${image_digested%%@*} --multi-arch all --dest-tls-verify=false --insecure-policy
 			else
-				echo "already cached ${image_digested} to docker://cache.defn.run:5000/${image_digested%%@*}"
+				echo "already cached ${image_digested} to docker://${registry}/${image_digested%%@*}"
 			fi
 		done
 
