a7b2ef9f2ed788c2270b5bcb62eddc960bdd53a4
amanibhavam <iam@defn.sh>
2024-01-16 05:34:55 +0000

    flake for ubuuntu iso manip

---
+++ m/pkg/iso/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    pkg.url = github:defn/dev/pkg-pkg-0.0.15?dir=m/pkg/pkg;
+  };
+
+  outputs = inputs: inputs.pkg.main rec {
+    src = builtins.path { path = ./.; name = "pkg-iso"; };
+
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
+      propagatedBuildInputs = with ctx.pkgs; [
+        xorriso
+      ];
+    };
+  };
+}
