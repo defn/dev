90e791854ecc7324a427cad3746a57ee3ddabe14
amanibhavam <iam@defn.sh>
2024-01-16 19:40:52 +0000

    update pkg deps [skip ci]

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -6,14 +6,14 @@
     helm.url = github:defn/dev/pkg-helm-3.13.3-4?dir=m/pkg/helm;
     kustomize.url = github:defn/dev/pkg-kustomize-5.3.0-4?dir=m/pkg/kustomize;
     stern.url = github:defn/dev/pkg-stern-1.28.0-1?dir=m/pkg/stern;
-    argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.3-1?dir=m/pkg/argoworkflows;
+    argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.4-1?dir=m/pkg/argoworkflows;
     argocd.url = github:defn/dev/pkg-argocd-2.10.0-rc1-5?dir=m/pkg/argocd;
     kn.url = github:defn/dev/pkg-kn-1.12.0-4?dir=m/pkg/kn;
     vcluster.url = github:defn/dev/pkg-vcluster-0.18.1-4?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.1-1?dir=m/pkg/kubevirt;
     linkerd.url = github:defn/dev/pkg-linkerd-2.14.8-1?dir=m/pkg/linkerd;
     cilium.url = github:defn/dev/pkg-cilium-0.15.20-1?dir=m/pkg/cilium;
-    hubble.url = github:defn/dev/pkg-hubble-0.12.3-4?dir=m/pkg/hubble;
+    hubble.url = github:defn/dev/pkg-hubble-0.13.0-1?dir=m/pkg/hubble;
     tfo.url = github:defn/dev/pkg-tfo-2.2.0-4?dir=m/pkg/tfo;
     mirrord.url = github:defn/dev/pkg-mirrord-3.83.0-1?dir=m/pkg/mirrord;
   };
