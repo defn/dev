b9c3f120561790650f497dd360f2ecbf8c1ca856
amanibhavam <iam@defn.sh>
2024-01-18 02:11:29 +0000

    update pkg deps [skip ci]

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -3,7 +3,7 @@
     kubectl.url = github:defn/dev/pkg-kubectl-1.26.7-8?dir=m/pkg/kubectl;
     k3sup.url = github:defn/dev/pkg-k3sup-0.13.5-1?dir=m/pkg/k3sup;
     k9s.url = github:defn/dev/pkg-k9s-0.31.5-1?dir=m/pkg/k9s;
-    helm.url = github:defn/dev/pkg-helm-3.13.3-4?dir=m/pkg/helm;
+    helm.url = github:defn/dev/pkg-helm-3.14.0-1?dir=m/pkg/helm;
     kustomize.url = github:defn/dev/pkg-kustomize-5.3.0-4?dir=m/pkg/kustomize;
     stern.url = github:defn/dev/pkg-stern-1.28.0-1?dir=m/pkg/stern;
     argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.4-1?dir=m/pkg/argoworkflows;
