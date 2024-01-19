4d686e2ea5e37d24c8a8cdab935feb08ef39e141
amanibhavam <iam@defn.sh>
2024-01-19 23:44:55 +0000

    update pkg deps [skip ci]

--- m/pkg/vpn/flake.nix
+++ m/pkg/vpn/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     tailscale.url = github:defn/dev/pkg-tailscale-1.58.0-1?dir=m/pkg/tailscale;
-    cloudflared.url = github:defn/dev/pkg-cloudflared-2024.1.3-1?dir=m/pkg/cloudflared;
+    cloudflared.url = github:defn/dev/pkg-cloudflared-2024.1.4-1?dir=m/pkg/cloudflared;
   };
 
   outputs = inputs: inputs.tailscale.inputs.pkg.main rec {
