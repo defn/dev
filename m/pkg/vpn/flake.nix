231bc5b059364e2af5aadc63d405b33dfff2f490
amanibhavam <iam@defn.sh>
2024-01-20 21:42:28 +0000

    add wireproxy [skip ci]

--- m/pkg/vpn/flake.nix
+++ m/pkg/vpn/flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     tailscale.url = github:defn/dev/pkg-tailscale-1.58.0-1?dir=m/pkg/tailscale;
     cloudflared.url = github:defn/dev/pkg-cloudflared-2024.1.4-1?dir=m/pkg/cloudflared;
+    wireproxy.url = github:defn/dev/pkg-wireproxy-2024.1.4-1?dir=m/pkg/wireproxy;
   };
 
   outputs = inputs: inputs.tailscale.inputs.pkg.main rec {
@@ -11,6 +12,7 @@
       propagatedBuildInputs = with ctx.pkgs; [
         inputs.tailscale.defaultPackage.${ctx.system}
         inputs.cloudflared.defaultPackage.${ctx.system}
+        inputs.wireproxy.defaultPackage.${ctx.system}
         easyrsa
         openvpn
         wireguard-tools
