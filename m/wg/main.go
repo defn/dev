3510d0112e543347660cb732868d9d1e8e1d045b
amanibhavam <iam@defn.sh>
2024-01-20 22:02:13 +0000

    no comments

--- m/wg/main.go
+++ m/wg/main.go
@@ -11,14 +11,12 @@ import (
 )
 
 func main() {
-	// Create a new Dialer based on a WireGuard configuration file
 	d, err := wiredialer.NewDialerFromFile(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// Create a new HTTP client that uses the Dialer
 	client := &http.Client{
 		Transport: &http.Transport{
 			DialContext: d.DialContext,
@@ -26,7 +24,6 @@ func main() {
 		},
 	}
 
-	// Make a request
 	resp, err := client.Get(os.Args[2])
 	if err != nil {
 		fmt.Println(err)
@@ -34,7 +31,5 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	// Print the response body
 	io.Copy(os.Stdout, resp.Body)
-
 }
