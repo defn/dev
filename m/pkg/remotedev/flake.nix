6f0488d3b1f8100c08813d4595f49e30fdd3df0b
amanibhavam <iam@defn.sh>
2024-01-22 15:18:55 +0000

    update pkg deps [skip ci]

--- m/pkg/remotedev/flake.nix
+++ m/pkg/remotedev/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    coder.url = github:defn/dev/pkg-coder-2.7.0-1?dir=m/pkg/coder;
+    coder.url = github:defn/dev/pkg-coder-2.7.1-1?dir=m/pkg/coder;
     codeserver.url = github:defn/dev/pkg-codeserver-4.20.0-3?dir=m/pkg/codeserver;
   };
 
