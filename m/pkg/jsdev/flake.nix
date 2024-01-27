a9e591a5f329ac88655369af4337a49167092f42
amanibhavam <iam@defn.sh>
2024-01-27 18:24:42 +0000

    latest 24.05 [skip ci]

--- m/pkg/jsdev/flake.nix
+++ m/pkg/jsdev/flake.nix
@@ -3,7 +3,7 @@
     pkg.url = github:defn/dev/pkg-pkg-0.0.15?dir=m/pkg/pkg;
     # https://github.com/NixOS/nixpkgs/tree/24.05-pre/pkgs/development/web/nodejs
     # https://github.com/NixOS/nixpkgs/blob/24.05-pre/pkgs/top-level/aliases.nix
-    latest.url = github:NixOS/nixpkgs?rev=b013b3ee50cace81104bc29b8fc4496a3093b5cd;
+    latest.url = github:NixOS/nixpkgs?rev=87cc06983c14876bb56a6a84935d1a3968f35999;
   };
 
   outputs = inputs: inputs.pkg.main rec {
