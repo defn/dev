c179c754318aaf0ba12e4b794158cbb6a4c7b539
amanibhavam <iam@defn.sh>
2024-01-27 18:12:21 +0000

    latest 24.05 pre [skip ci]

--- m/pkg/jsdev/flake.nix
+++ m/pkg/jsdev/flake.nix
@@ -1,8 +1,9 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.15?dir=m/pkg/pkg;
-    # https://github.com/NixOS/nixpkgs/commits/main
-    latest.url = github:NixOS/nixpkgs?rev=005617587ee2b7c003388b4539b9120ebcc90e44;
+    # https://github.com/NixOS/nixpkgs/tree/24.05-pre/pkgs/development/web/nodejs
+    # https://github.com/NixOS/nixpkgs/blob/24.05-premaster/pkgs/top-level/aliases.nix
+    latest.url = github:NixOS/nixpkgs?rev=b013b3ee50cace81104bc29b8fc4496a3093b5cd;
   };
 
   outputs = inputs: inputs.pkg.main rec {
@@ -11,7 +12,7 @@
     defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs =
         with (import inputs.latest { system = ctx.system; }); [
-          nodejs-18_x
+          nodejs_18
         ];
     };
   };
