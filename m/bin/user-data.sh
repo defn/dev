952c59380849cb1fde906e9c2a5cc4fc93f66515
amanibhavam <iam@defn.sh>
2024-01-28 20:56:35 +0000

    formatting shell scripts

--- m/bin/user-data.sh
+++ m/bin/user-data.sh
@@ -31,7 +31,7 @@ export CODER_AGENT_TOKEN
 TOKEN="$(curl -sSL -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")"
 instance_id="$(curl -sSL -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)"
 
-aws secretsmanager get-secret-value --secret-id "${DFD_WORKSPACE_NAME}-${instance_id}" | jq -r '.SecretString | fromjson | .coder_agent_token' > /tmp/.coder-token
+aws secretsmanager get-secret-value --secret-id "${DFD_WORKSPACE_NAME}-${instance_id}" | jq -r '.SecretString | fromjson | .coder_agent_token' >/tmp/.coder-token
 CODER_AGENT_TOKEN="$(cat /tmp/.coder-token)"
 echo rm -f /tmp/.coder-token
 
