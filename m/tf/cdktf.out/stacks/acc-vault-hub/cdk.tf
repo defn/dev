e8a7f29dced6a7832802e7064bab2e8e159bd26e
amanibhavam <iam@defn.sh>
2024-01-28 21:29:11 +0000

    generate configs

---
+++ m/tf/cdktf.out/stacks/acc-vault-hub/cdk.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "5.34.0"
+      source  = "aws"
+    }
+  }
+  backend "s3" {
+    bucket         = "dfn-defn-terraform-state"
+    dynamodb_table = "dfn-defn-terraform-state-lock"
+    encrypt        = true
+    key            = "stacks/acc-vault-hub/terraform.tfstate"
+    profile        = "defn-org-sso"
+    region         = "us-east-1"
+  }
+
+
+}
+
+provider "aws" {
+  profile = "vault-hub-sso"
+  alias   = "vault-hub"
+}
+module "vault-hub" {
+  name      = "terraform"
+  namespace = "dfn"
+  stage     = "defn"
+  source    = "./assets/__cdktf_module_asset_26CE565C/3B662577168AEE3844EE70D2853CDA26/terraform-aws-defn-account"
+  providers = {
+    aws = aws.vault-hub
+  }
+}
