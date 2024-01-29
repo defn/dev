a6c3c29b624f6cc22a79329efaf7c641b50d6632
amanibhavam <iam@defn.sh>
2024-01-29 06:44:10 +0000

    new locations for state

--- m/tf/cdktf.out/stacks/org-spiral/cdk.tf
+++ m/tf/cdktf.out/stacks/org-spiral/cdk.tf
@@ -9,7 +9,7 @@ terraform {
     bucket         = "dfn-defn-terraform-state"
     dynamodb_table = "dfn-defn-terraform-state-lock"
     encrypt        = true
-    key            = "stacks/spiral/terraform.tfstate"
+    key            = "stacks/org-spiral/terraform.tfstate"
     profile        = "defn-org-sso"
     region         = "us-east-1"
   }
