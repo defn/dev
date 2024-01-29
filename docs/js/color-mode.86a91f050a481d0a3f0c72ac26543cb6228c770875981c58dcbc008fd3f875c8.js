4c9c1e188d2a1dde6324adf5a8796e0c2d99f69e
amanibhavam <iam@defn.sh>
2024-01-29 17:52:29 +0000

    format everything

--- docs/js/color-mode.86a91f050a481d0a3f0c72ac26543cb6228c770875981c58dcbc008fd3f875c8.js
+++ docs/js/color-mode.86a91f050a481d0a3f0c72ac26543cb6228c770875981c58dcbc008fd3f875c8.js
@@ -1,4 +1,34 @@
-(()=>{(()=>{"use strict";let t=localStorage.getItem("theme"),r=()=>t||(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"),a=function(e){e==="auto"&&window.matchMedia("(prefers-color-scheme: dark)").matches?document.documentElement.setAttribute("data-bs-theme","dark"):document.documentElement.setAttribute("data-bs-theme",e)};a(r()),window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",()=>{(t!=="light"||t!=="dark")&&a(r())}),window.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll("[data-bs-theme-value]").forEach(e=>{e.addEventListener("click",()=>{let d=e.getAttribute("data-bs-theme-value");localStorage.setItem("theme",d),a(d)})})})})();})();
+(() => {
+  (() => {
+    "use strict";
+    let t = localStorage.getItem("theme"),
+      r = () =>
+        t ||
+        (window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light"),
+      a = function (e) {
+        e === "auto" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? document.documentElement.setAttribute("data-bs-theme", "dark")
+          : document.documentElement.setAttribute("data-bs-theme", e);
+      };
+    a(r()),
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", () => {
+          (t !== "light" || t !== "dark") && a(r());
+        }),
+      window.addEventListener("DOMContentLoaded", () => {
+        document.querySelectorAll("[data-bs-theme-value]").forEach((e) => {
+          e.addEventListener("click", () => {
+            let d = e.getAttribute("data-bs-theme-value");
+            localStorage.setItem("theme", d), a(d);
+          });
+        });
+      });
+  })();
+})();
 /*!
  * Modified from
  * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
