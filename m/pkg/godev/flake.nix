15669fa98ffc69bfb0cddef1273b05d2b63808ad
amanibhavam <iam@defn.sh>
2024-01-29 17:46:58 +0000

    add yaegi [skip ci]

--- m/pkg/godev/flake.nix
+++ m/pkg/godev/flake.nix
@@ -3,6 +3,7 @@
     buf.url = github:defn/dev/pkg-buf-1.29.0-1?dir=m/pkg/buf;
     cosign.url = github:defn/dev/pkg-cosign-2.2.2-4?dir=m/pkg/cosign;
     goreleaser.url = github:defn/dev/pkg-goreleaser-1.23.0-1?dir=m/pkg/goreleaser;
+    yaegi.url = github:defn/dev/pkg-yaegi-0.15.1-2?dir=m/pkg/yaegi;
     # https://github.com/NixOS/nixpkgs/tree/24.05-pre/pkgs/development/compilers/go
     # https://github.com/NixOS/nixpkgs/blob/24.05-pre/pkgs/top-level/aliases.nix
     latest.url = github:NixOS/nixpkgs?rev=87cc06983c14876bb56a6a84935d1a3968f35999;
@@ -26,6 +27,7 @@
           inputs.buf.defaultPackage.${ctx.system}
           inputs.cosign.defaultPackage.${ctx.system}
           inputs.goreleaser.defaultPackage.${ctx.system}
+          inputs.yaegi.defaultPackage.${ctx.system}
         ];
     };
   };
