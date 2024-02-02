8a158352f3aa1011843463c48ad663c44079f41a
amanibhavam <iam@defn.sh>
2024-02-02 18:24:09 +0000

    update pkg deps [skip ci]

--- m/pkg/az/flake.nix
+++ m/pkg/az/flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
-    cue.url = github:defn/dev/pkg-cue-0.7.0-4?dir=m/pkg/cue;
-    gum.url = github:defn/dev/pkg-gum-0.13.0-4?dir=m/pkg/gum;
-    vhs.url = github:defn/dev/pkg-vhs-0.7.1-4?dir=m/pkg/vhs;
-    glow.url = github:defn/dev/pkg-glow-1.5.1-8?dir=m/pkg/glow;
-    dyff.url = github:defn/dev/pkg-dyff-1.6.0-5?dir=m/pkg/dyff;
+    cue.url = github:defn/dev/pkg-cue-0.7.0-6?dir=m/pkg/cue;
+    gum.url = github:defn/dev/pkg-gum-0.13.0-5?dir=m/pkg/gum;
+    vhs.url = github:defn/dev/pkg-vhs-0.7.1-5?dir=m/pkg/vhs;
+    glow.url = github:defn/dev/pkg-glow-1.5.1-9?dir=m/pkg/glow;
+    dyff.url = github:defn/dev/pkg-dyff-1.6.0-6?dir=m/pkg/dyff;
   };
 
   outputs = inputs: inputs.cue.inputs.pkg.main rec {
