63ef0f3378691715c9171e123b699c5da23d32e5
amanibhavam <iam@defn.sh>
2024-02-02 18:24:56 +0000

    update pkg deps [skip ci]

--- m/pkg/development/flake.nix
+++ m/pkg/development/flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    buildifier.url = github:defn/dev/pkg-buildifier-6.4.0-4?dir=m/pkg/buildifier;
-    bazelisk.url = github:defn/dev/pkg-bazelisk-1.19.0-4?dir=m/pkg/bazelisk;
-    ibazel.url = github:defn/dev/pkg-ibazel-0.24.0-4?dir=m/pkg/ibazel;
-    oci.url = github:defn/dev/pkg-oci-0.0.55?dir=m/pkg/oci;
+    buildifier.url = github:defn/dev/pkg-buildifier-6.4.0-5?dir=m/pkg/buildifier;
+    bazelisk.url = github:defn/dev/pkg-bazelisk-1.19.0-5?dir=m/pkg/bazelisk;
+    ibazel.url = github:defn/dev/pkg-ibazel-0.24.0-5?dir=m/pkg/ibazel;
+    oci.url = github:defn/dev/pkg-oci-0.0.56?dir=m/pkg/oci;
   };
 
   outputs = inputs: inputs.buildifier.inputs.pkg.main rec {
