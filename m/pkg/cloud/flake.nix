c23c4b609aa7b6c44db4ea33e8efc3d5ee289b51
amanibhavam <iam@defn.sh>
2024-02-02 18:24:39 +0000

    update pkg deps [skip ci]

--- m/pkg/cloud/flake.nix
+++ m/pkg/cloud/flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    terraform.url = github:defn/dev/pkg-terraform-1.7.2-1?dir=m/pkg/terraform;
-    terraformdocs.url = github:defn/dev/pkg-terraformdocs-0.17.0-3?dir=m/pkg/terraformdocs;
-    packer.url = github:defn/dev/pkg-packer-1.10.1-1?dir=m/pkg/packer;
-    step.url = github:defn/dev/pkg-step-0.25.2-1?dir=m/pkg/step;
-    awscli.url = github:defn/dev/pkg-awscli-2.15.16-1?dir=m/pkg/awscli;
-    chamber.url = github:defn/dev/pkg-chamber-2.13.6-4?dir=m/pkg/chamber;
+    terraform.url = github:defn/dev/pkg-terraform-1.7.2-2?dir=m/pkg/terraform;
+    terraformdocs.url = github:defn/dev/pkg-terraformdocs-0.17.0-4?dir=m/pkg/terraformdocs;
+    packer.url = github:defn/dev/pkg-packer-1.10.1-2?dir=m/pkg/packer;
+    step.url = github:defn/dev/pkg-step-0.25.2-2?dir=m/pkg/step;
+    awscli.url = github:defn/dev/pkg-awscli-2.15.16-2?dir=m/pkg/awscli;
+    chamber.url = github:defn/dev/pkg-chamber-2.13.6-5?dir=m/pkg/chamber;
   };
 
   outputs = inputs: inputs.terraform.inputs.pkg.main rec {
