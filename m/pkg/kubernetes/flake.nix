bc6318105757d78839ed3806bd0636a8b22a5866
amanibhavam <iam@defn.sh>
2024-02-02 18:26:38 +0000

    update pkg deps [skip ci]

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -1,21 +1,21 @@
 {
   inputs = {
-    kubectl.url = github:defn/dev/pkg-kubectl-1.26.7-8?dir=m/pkg/kubectl;
-    k3sup.url = github:defn/dev/pkg-k3sup-0.13.5-1?dir=m/pkg/k3sup;
-    k9s.url = github:defn/dev/pkg-k9s-0.31.7-1?dir=m/pkg/k9s;
-    helm.url = github:defn/dev/pkg-helm-3.14.0-1?dir=m/pkg/helm;
-    kustomize.url = github:defn/dev/pkg-kustomize-5.3.0-4?dir=m/pkg/kustomize;
-    stern.url = github:defn/dev/pkg-stern-1.28.0-1?dir=m/pkg/stern;
-    argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.4-1?dir=m/pkg/argoworkflows;
-    argocd.url = github:defn/dev/pkg-argocd-2.10.0-rc1-5?dir=m/pkg/argocd;
-    kn.url = github:defn/dev/pkg-kn-1.13.0-1?dir=m/pkg/kn;
-    vcluster.url = github:defn/dev/pkg-vcluster-0.18.1-4?dir=m/pkg/vcluster;
-    kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.1-1?dir=m/pkg/kubevirt;
+    kubectl.url = github:defn/dev/pkg-kubectl-1.26.7-9?dir=m/pkg/kubectl;
+    k3sup.url = github:defn/dev/pkg-k3sup-0.13.5-2?dir=m/pkg/k3sup;
+    k9s.url = github:defn/dev/pkg-k9s-0.31.7-2?dir=m/pkg/k9s;
+    helm.url = github:defn/dev/pkg-helm-3.14.0-2?dir=m/pkg/helm;
+    kustomize.url = github:defn/dev/pkg-kustomize-5.3.0-5?dir=m/pkg/kustomize;
+    stern.url = github:defn/dev/pkg-stern-1.28.0-2?dir=m/pkg/stern;
+    argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.4-2?dir=m/pkg/argoworkflows;
+    argocd.url = github:defn/dev/pkg-argocd-2.10.0-rc1-6?dir=m/pkg/argocd;
+    kn.url = github:defn/dev/pkg-kn-1.13.0-2?dir=m/pkg/kn;
+    vcluster.url = github:defn/dev/pkg-vcluster-0.18.1-5?dir=m/pkg/vcluster;
+    kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.1-2?dir=m/pkg/kubevirt;
     linkerd.url = github:defn/dev/pkg-linkerd-24.1.3-1?dir=m/pkg/linkerd;
-    cilium.url = github:defn/dev/pkg-cilium-0.15.22-1?dir=m/pkg/cilium;
-    hubble.url = github:defn/dev/pkg-hubble-0.13.0-1?dir=m/pkg/hubble;
-    tfo.url = github:defn/dev/pkg-tfo-2.2.0-4?dir=m/pkg/tfo;
-    mirrord.url = github:defn/dev/pkg-mirrord-3.86.0-1?dir=m/pkg/mirrord;
+    cilium.url = github:defn/dev/pkg-cilium-0.15.22-2?dir=m/pkg/cilium;
+    hubble.url = github:defn/dev/pkg-hubble-0.13.0-2?dir=m/pkg/hubble;
+    tfo.url = github:defn/dev/pkg-tfo-2.2.0-5?dir=m/pkg/tfo;
+    mirrord.url = github:defn/dev/pkg-mirrord-3.86.0-2?dir=m/pkg/mirrord;
   };
 
   outputs = inputs: inputs.kubectl.inputs.pkg.main rec {
