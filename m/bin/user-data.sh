7ba82bb3dbdc2c713fe6911afa0a3039b9d014f1
amanibhavam <iam@defn.sh>
2024-02-03 22:50:20 +0000

    try without the token

--- m/bin/user-data.sh
+++ m/bin/user-data.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-#export CODER_AGENT_AUTH="aws-instance-identity"
-export CODER_AGENT_AUTH="token"
+export CODER_AGENT_AUTH="aws-instance-identity"
+#export CODER_AGENT_AUTH="token"
+
 export CODER_AGENT_URL="$1"
 shift
 export DFD_WORKSPACE_NAME="$1"
@@ -25,13 +26,11 @@ set +x
 source .bash_profile
 set -x
 
-export CODER_AGENT_TOKEN
-
-TOKEN="$(curl -sSL -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")"
-instance_id="$(curl -sSL -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)"
-
-aws secretsmanager get-secret-value --secret-id "${DFD_WORKSPACE_NAME}-${instance_id}" | jq -r '.SecretString' >/tmp/.coder-token
-CODER_AGENT_TOKEN="$(cat /tmp/.coder-token)"
+#export CODER_AGENT_TOKEN
+#TOKEN="$(curl -sSL -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")"
+#instance_id="$(curl -sSL -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)"
+#aws secretsmanager get-secret-value --secret-id "${DFD_WORKSPACE_NAME}-${instance_id}" | jq -r '.SecretString' >/tmp/.coder-token
+#CODER_AGENT_TOKEN="$(cat /tmp/.coder-token)"
 
 coder agent >/tmp/user-data.log 2>&1 &
 disown
