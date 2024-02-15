69738b219fec4ba77bd687313eb0149304103c56
amanibhavam <iam@defn.sh>
2024-02-15 00:55:56 +0000

    update pkg deps [skip ci]

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -3,11 +3,11 @@
     kubectl.url = github:defn/dev/pkg-kubectl-1.26.7-9?dir=m/pkg/kubectl;
     k3sup.url = github:defn/dev/pkg-k3sup-0.13.5-2?dir=m/pkg/k3sup;
     k9s.url = github:defn/dev/pkg-k9s-0.31.8-1?dir=m/pkg/k9s;
-    helm.url = github:defn/dev/pkg-helm-3.14.0-2?dir=m/pkg/helm;
+    helm.url = github:defn/dev/pkg-helm-3.14.1-1?dir=m/pkg/helm;
     kustomize.url = github:defn/dev/pkg-kustomize-5.3.0-5?dir=m/pkg/kustomize;
     stern.url = github:defn/dev/pkg-stern-1.28.0-2?dir=m/pkg/stern;
     argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.4-2?dir=m/pkg/argoworkflows;
-    argocd.url = github:defn/dev/pkg-argocd-2.10.0-rc4-8?dir=m/pkg/argocd;
+    argocd.url = github:defn/dev/pkg-argocd-2.10.1-1?dir=m/pkg/argocd;
     kn.url = github:defn/dev/pkg-kn-1.13.0-2?dir=m/pkg/kn;
     vcluster.url = github:defn/dev/pkg-vcluster-0.19.0-1?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.1-2?dir=m/pkg/kubevirt;
