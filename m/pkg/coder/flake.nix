133c6b044b2d51c22cf202325aa8e03c20b30181
amanibhavam <iam@defn.sh>
2024-02-16 08:15:17 -0800

    coder/ 2.8.2 -> 2.8.3 [skip ci]

--- m/pkg/coder/flake.nix
+++ m/pkg/coder/flake.nix
@@ -30,22 +30,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-hsX/DcnldshzDcbSvbLdRIdQL9zU3XrPPpXBbBUwCGI="; # x86_64-linux
+        sha256 = "sha256-udI2xgqAeN8czYIrYBVZ9I/asSW4FsH9WXH+wLXTAoU="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-Rr6VCVc0mRDOBu7p96T4JwSJ7KOu3wg6vGiMaLHT7DM="; # aarch64-linux
+        sha256 = "sha256-+EnB6nopVNkadTdZEu6apm/ZuHBAabxTmzg/N0VoBq8="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-RH7sRIUIZ97raBvzSt2Po6xD1PXhGKx5lFYpjiFFpyo="; # x86_64-darwin
+        sha256 = "sha256-pGdc2koqnd0yqgkiHrigmQQFsTwzArkgoOj9n6kcsEg="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-jPVKgYhEQgO19jS8TOyf+8JuumFM8bajZZ0K48Ppogk="; # aarch64-darwin
+        sha256 = "sha256-UsS8E3nQNyDOYpmxbcPLjATNIyL9xn5DiRef4t5NFuY="; # aarch64-darwin
       };
     };
   };
