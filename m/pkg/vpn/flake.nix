07734e3adbf57087c47cb5329148827479dab02b
amanibhavam <iam@defn.sh>
2024-02-16 01:28:00 +0000

    update pkg deps [skip ci]

--- m/pkg/vpn/flake.nix
+++ m/pkg/vpn/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    tailscale.url = github:defn/dev/pkg-tailscale-1.58.2-2?dir=m/pkg/tailscale;
+    tailscale.url = github:defn/dev/pkg-tailscale-1.60.0-1?dir=m/pkg/tailscale;
     cloudflared.url = github:defn/dev/pkg-cloudflared-2024.2.0-1?dir=m/pkg/cloudflared;
     wireproxy.url = github:defn/dev/pkg-wireproxy-1.0.7-1?dir=m/pkg/wireproxy;
   };
