3ae7666ab1e81a44ef34d7ae023e6f205269d1ea
amanibhavam <iam@defn.sh>
2024-02-28 01:21:53 -0800

    beh

--- m/tf/mod/terraform-coder-defn-managed/managed.tf
+++ m/tf/mod/terraform-coder-defn-managed/managed.tf
@@ -1,34 +1,35 @@
-resource "aws_ssm_document" "agent" {
-  name          = "coder_agent_${base64encode(coder_agent.main.token)}"
-  document_type = "Command"
-
-  content = <<-DOC
-  {
-    "schemaVersion": "1.2",
-    "description": "Coder Agent",
-    "parameters": {
-    },
-    "runtimeConfig": {
-      "aws:runShellScript": {
-        "properties": [{
-            "id": "0.aws:runShellScript",
-            "runCommand": [
-              "bash",
-              "-c",
-              "echo ${base64encode(coder_agent.main.init_script)} | base64 -d | env CODER_AGENT_TOKEN=${coder_agent.main.token} bash -"
-            ]
-        }]
-      }
-    }
-  }
-  DOC
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_ssm_association" "agent" {
-  name             = "agent"
-  document_version = aws_ssm_document.agent.default_version
+data "aws_ssm_document" "script" {
+  name            = "AWS-RunRemoteScript"
+  document_format = "YAML"
+}
+
+resource "aws_s3_object" "scripts" {
+  key            = "coder_agent_${base64encode(coder_agent.main.token)}"
+  bucket         = "defn-net-scripts-2"
+  content_base64 = base64encode("echo ${base64encode(coder_agent.main.init_script)} | base64 -d | env CODER_AGENT_TOKEN=${coder_agent.main.token} bash -")
+  acl            = "private"
+}
+
+resource "aws_ssm_association" "script" {
+  name             = "AWS-RunRemoteScript"
+  association_name = "coder_agent_${base64encode(coder_agent.main.token)}"
+
   targets {
     key    = "InstanceIds"
     values = [data.coder_parameter.instance.value]
+  }
+
+  parameters = {
+    commandLine = "coder_agent_${base64encode(coder_agent.main.token)}"
+    sourceType  = "S3"
+    sourceInfo  = <<EOJ
+{
+    "path": "https://s3.amazonaws.com/defn-net-scripts-2/coder_agent_${base64encode(coder_agent.main.token)}"
+}
+EOJ
   }
 }
