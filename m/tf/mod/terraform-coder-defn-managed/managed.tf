03eda19b1c66d7fb6d9cd1dd3f9999a8cb8bab9d
amanibhavam <iam@defn.sh>
2024-02-29 02:26:11 +0000

    fix old code-server

--- m/tf/mod/terraform-coder-defn-managed/managed.tf
+++ m/tf/mod/terraform-coder-defn-managed/managed.tf
@@ -21,6 +21,6 @@ resource "aws_ssm_association" "script" {
   }
 
   parameters = {
-    commands = "( (echo sudo pkill -f -9 coder.agent '||' true; echo sudo pkill -f -9 coder-server '||' true; echo sudo pkill -9 -f ssm-document-worker '||' true; echo sudo pkill -9 -f awsrunShellScript '||' true; echo cd ~ubuntu; echo export STARSHIP_NO=1 CODER_AGENT_TOKEN=${coder_agent.main.token}; echo source .bash_profile; echo ${base64encode(coder_agent.main.init_script)} | base64 -d; echo) > /tmp/meh_${local.slug}.sh; exec setsid sudo -H -u ubuntu bash -x /tmp/meh_${local.slug}.sh >>/tmp/startup.log 2>&1 &) &"
+    commands = "( (echo sudo pkill -f -9 coder.agent '||' true; echo sudo pkill -f -9 code-server '||' true; echo sudo pkill -9 -f ssm-document-worker '||' true; echo sudo pkill -9 -f awsrunShellScript '||' true; echo cd ~ubuntu; echo export STARSHIP_NO=1 CODER_AGENT_TOKEN=${coder_agent.main.token}; echo source .bash_profile; echo ${base64encode(coder_agent.main.init_script)} | base64 -d; echo) > /tmp/meh_${local.slug}.sh; exec setsid sudo -H -u ubuntu bash -x /tmp/meh_${local.slug}.sh >>/tmp/startup.log 2>&1 &) &"
   }
 }
