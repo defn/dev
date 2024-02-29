deda946a9521d4cba4497ffb3f022199119debef
amanibhavam <iam@defn.sh>
2024-02-29 05:31:44 +0000

    update pkg deps [skip ci]

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -9,7 +9,7 @@
     argoworkflows.url = github:defn/dev/pkg-argoworkflows-3.5.4-2?dir=m/pkg/argoworkflows;
     argocd.url = github:defn/dev/pkg-argocd-2.10.1-1?dir=m/pkg/argocd;
     kn.url = github:defn/dev/pkg-kn-1.13.0-2?dir=m/pkg/kn;
-    vcluster.url = github:defn/dev/pkg-vcluster-0.19.2-1?dir=m/pkg/vcluster;
+    vcluster.url = github:defn/dev/pkg-vcluster-0.19.3-1?dir=m/pkg/vcluster;
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.1.1-2?dir=m/pkg/kubevirt;
     kuma.url = github:defn/dev/pkg-kuma-2.6.1-2?dir=m/pkg/kuma;
     cilium.url = github:defn/dev/pkg-cilium-0.15.23-1?dir=m/pkg/cilium;
