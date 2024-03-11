d0aa676e74142072856fbbd4dfc9d80255aa77bc
amanibhavam <iam@defn.sh>
2024-03-11 06:29:11 +0000

    provide the build implementation

---
+++ m/tf/tf.go
@@ -0,0 +1,35 @@
+package tf
+
+import (
+	_ "embed"
+
+	"github.com/spf13/cobra"
+
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+
+	infra "github.com/defn/dev/m/command/infra"
+	root "github.com/defn/dev/m/command/root"
+)
+
+func Init(schema string) {
+	root.RootCmd.AddCommand(&cobra.Command{
+		Use:   "build",
+		Short: "Generates Terraform configs from CUE",
+		Long:  `Generates Terraform configs from CUE.`,
+
+		Run: func(cmd *cobra.Command, args []string) {
+			site := infra.LoadUserAwsProps(schema)
+			app := cdktf.NewApp(&cdktf.AppConfig{})
+
+			for _, org := range site.Organization {
+				AwsOrganizationStack(app, &site, &org)
+
+				for _, acc := range org.Accounts {
+					AwsAccountStack(app, &site, &org, &acc)
+				}
+			}
+
+			app.Synth()
+		},
+	})
+}
