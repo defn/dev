2bb8591670bd4d26222877e5aed9bc4eccb658fd
amanibhavam <iam@defn.sh>
2024-03-11 04:25:02 +0000

    bring in cloud flake, for tf

--- m/demo/flake.nix
+++ m/demo/flake.nix
@@ -2,8 +2,7 @@
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
     cue.url = github:defn/dev/pkg-cue-0.7.1-1?dir=m/pkg/cue;
-    terraform.url = github:defn/dev/pkg-terraform-1.7.4-1?dir=m/pkg/terraform;
-    awscli.url = github:defn/dev/pkg-awscli-2.15.27-1?dir=m/pkg/awscli;
+    cloud.url = github:defn/dev/pkg-cloud-0.0.243?dir=m/pkg/cloud;
     latest.url = github:NixOS/nixpkgs?rev=87cc06983c14876bb56a6a84935d1a3968f35999;
   };
 
@@ -23,8 +22,7 @@
           nodejs_20
 
           inputs.cue.defaultPackage.${ctx.system}
-          inputs.terraform.defaultPackage.${ctx.system}
-          inputs.awscli.defaultPackage.${ctx.system}
+          inputs.cloud.defaultPackage.${ctx.system}
 
           bashInteractive
         ];
