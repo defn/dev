9c9b3cea6855045aa6261498aff8bebd0835caf2
amanibhavam <iam@defn.sh>
2024-03-12 18:44:45 +0000

    update pkg deps [skip ci]

--- m/pkg/kubernetes/flake.nix
+++ m/pkg/kubernetes/flake.nix
@@ -14,7 +14,7 @@
     kubevirt.url = github:defn/dev/pkg-kubevirt-1.2.0-1?dir=m/pkg/kubevirt;
     kuma.url = github:defn/dev/pkg-kuma-2.6.1-2?dir=m/pkg/kuma;
     cilium.url = github:defn/dev/pkg-cilium-0.16.0-1?dir=m/pkg/cilium;
-    hubble.url = github:defn/dev/pkg-hubble-0.13.0-2?dir=m/pkg/hubble;
+    hubble.url = github:defn/dev/pkg-hubble-0.13.2-1?dir=m/pkg/hubble;
     tfo.url = github:defn/dev/pkg-tfo-2.2.0-5?dir=m/pkg/tfo;
     mirrord.url = github:defn/dev/pkg-mirrord-3.91.0-1?dir=m/pkg/mirrord;
     crossplane.url = github:defn/dev/pkg-crossplane-1.15.0-1?dir=m/pkg/crossplane;
