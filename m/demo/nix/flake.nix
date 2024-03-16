8e9578b148e75ed09b0f64ebaf04a7a334f00a6c
amanibhavam <iam@defn.sh>
2024-03-16 21:58:01 +0000

    upgrade

--- m/demo/nix/flake.nix
+++ m/demo/nix/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
-    cue.url = github:defn/dev/pkg-cue-0.8.0-alpha.5-2?dir=m/pkg/cue;
+    cue.url = github:defn/dev/pkg-cue-0.8.0-1?dir=m/pkg/cue;
     cloud.url = github:defn/dev/pkg-cloud-0.0.247?dir=m/pkg/cloud;
     latest.url = github:NixOS/nixpkgs?rev=87cc06983c14876bb56a6a84935d1a3968f35999;
   };
