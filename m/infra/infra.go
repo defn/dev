95906da4b8878a35d3f9efeb00d44aac7966bd30
amanibhavam <iam@defn.sh>
2024-03-16 06:01:22 +0000

    starting with the coder templates

--- m/infra/infra.go
+++ m/infra/infra.go
@@ -36,6 +36,9 @@ func init() {
 			tf.GlobalStack(app, &site)
 
 			tf.CoderDefnEc2Stack(app, &site, "template")
+			tf.CoderDefnSshStack(app, &site, "template")
+			tf.CoderDefnMacOsStack(app, &site, "template")
+			tf.CoderDefnDevContainerStack(app, &site, "template")
 
 			app.Synth()
 		},
