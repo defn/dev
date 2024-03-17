8532fd13e333719313814906def936fafc9bd409
amanibhavam <iam@defn.sh>
2024-03-17 19:18:24 +0000

    no embedded [skip ci]

--- m/pkg/az/flake.nix
+++ m/pkg/az/flake.nix
@@ -1,24 +1,15 @@
 {
   inputs = {
-    cue.url = github:defn/dev/pkg-cue-0.8.0-1?dir=m/pkg/cue;
-    gum.url = github:defn/dev/pkg-gum-0.13.0-5?dir=m/pkg/gum;
-    vhs.url = github:defn/dev/pkg-vhs-0.7.1-5?dir=m/pkg/vhs;
-    glow.url = github:defn/dev/pkg-glow-1.5.1-9?dir=m/pkg/glow;
-    dyff.url = github:defn/dev/pkg-dyff-1.7.1-1?dir=m/pkg/dyff;
+    pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
   };
 
-  outputs = inputs: inputs.cue.inputs.pkg.main rec {
+  outputs = inputs: inputs.pkg.main rec {
     src = builtins.path { path = ./.; name = "pkg-az"; };
 
     defaultPackage = ctx: ctx.wrap.bashBuilder {
       inherit src;
 
       propagatedBuildInputs = with ctx.pkgs; [
-        inputs.cue.defaultPackage.${ctx.system}
-        inputs.gum.defaultPackage.${ctx.system}
-        inputs.vhs.defaultPackage.${ctx.system}
-        inputs.glow.defaultPackage.${ctx.system}
-        inputs.dyff.defaultPackage.${ctx.system}
         jq
         yq
         gron
