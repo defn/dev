bb63ec88ea33bcf7405670c4aec707344203d90e
amanibhavam <iam@defn.sh>
2024-03-17 19:00:45 +0000

    no embedded flakes [skip ci]

--- m/pkg/cloud/flake.nix
+++ m/pkg/cloud/flake.nix
@@ -1,31 +1,13 @@
 {
   inputs = {
-    terraform.url = github:defn/dev/pkg-terraform-1.7.5-1?dir=m/pkg/terraform;
-    terraformdocs.url = github:defn/dev/pkg-terraformdocs-0.17.0-4?dir=m/pkg/terraformdocs;
-    packer.url = github:defn/dev/pkg-packer-1.10.2-1?dir=m/pkg/packer;
-    step.url = github:defn/dev/pkg-step-0.25.2-2?dir=m/pkg/step;
-    awscli.url = github:defn/dev/pkg-awscli-2.15.30-1?dir=m/pkg/awscli;
-    chamber.url = github:defn/dev/pkg-chamber-2.14.1-1?dir=m/pkg/chamber;
+    pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
   };
 
-  outputs = inputs: inputs.terraform.inputs.pkg.main rec {
+  outputs = inputs: inputs.pkg.main rec {
     src = builtins.path { path = ./.; name = "pkg-cloud"; };
 
     defaultPackage = ctx: ctx.wrap.bashBuilder {
       inherit src;
-
-      propagatedBuildInputs =
-        let
-          flakeInputs = [
-            inputs.terraform.defaultPackage.${ctx.system}
-            inputs.terraformdocs.defaultPackage.${ctx.system}
-            inputs.packer.defaultPackage.${ctx.system}
-            inputs.step.defaultPackage.${ctx.system}
-            inputs.awscli.defaultPackage.${ctx.system}
-            inputs.chamber.defaultPackage.${ctx.system}
-          ];
-        in
-        flakeInputs;
 
       installPhase = ''
         mkdir -p $out/bin
