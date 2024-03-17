3126cb78aa0c93cfbe804d764df67ffef08e830d
amanibhavam <iam@defn.sh>
2024-03-17 19:13:03 +0000

    no eebdded flakes [skip ci]

--- m/pkg/vpn/flake.nix
+++ m/pkg/vpn/flake.nix
@@ -3,7 +3,7 @@
     pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
   };
 
-  outputs = inputs: inputs.tailscale.inputs.pkg.main rec {
+  outputs = inputs: inputs.pkg.main rec {
     src = builtins.path { path = ./.; name = "pkg-vpn"; };
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
