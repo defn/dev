ccc4d5d28c2150379586231b78d38723c019b5d4
amanibhavam <iam@defn.sh>
2024-03-30 19:15:34 +0000

    upgrade nix to 2.21.1 [skip ci]

--- m/pkg/nix/flake.nix
+++ m/pkg/nix/flake.nix
@@ -1,13 +1,16 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/tools/package-management/nix/default.nix
+    latest.url = github:NixOS/nixpkgs?rev=d2657c4a3dfc42c13e13243161b31e6bbdfac36e;
   };
 
   outputs = inputs: inputs.pkg.main rec {
     src = builtins.path { path = ./.; name = "pkg-nix"; };
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
-      propagatedBuildInputs = with ctx.pkgs; [
+      propagatedBuildInputs = 
+        with (import inputs.latest { system = ctx.system; }); [
         direnv
         nix
         nix-direnv
