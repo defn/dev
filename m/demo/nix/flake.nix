366c30fb5bc3695093c829e9c84e2a85a36b49fb
amanibhavam <iam@defn.sh>
2024-04-01 01:51:18 -0700

    generalize flake groups

--- m/demo/nix/flake.nix
+++ m/demo/nix/flake.nix
@@ -6,6 +6,8 @@
     godev.url = github:defn/dev/pkg-godev-0.0.136?dir=m/pkg/godev;
     jsdev.url = github:defn/dev/pkg-jsdev-0.0.68?dir=m/pkg/jsdev;
     pydev.url = github:defn/dev/pkg-pydev-0.0.128?dir=m/pkg/pydev;
+    secrets.url = github:defn/dev/pkg-secrets-0.0.81?dir=m/pkg/secrets;
+    shell.url = github:defn/dev/pkg-shell-0.0.68?dir=m/pkg/shell;
     latest.url = github:NixOS/nixpkgs?rev=87cc06983c14876bb56a6a84935d1a3968f35999;
   };
 
@@ -15,23 +17,13 @@
     defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs =
         with (import inputs.latest { system = ctx.system; }); [
-          direnv
-          git
-          git-lfs
-          gnumake
-          aws-vault
-          bat
-          jq
-
-          perl
-
           inputs.cue.defaultPackage.${ctx.system}
           inputs.cloud.defaultPackage.${ctx.system}
           inputs.godev.defaultPackage.${ctx.system}
           inputs.jsdev.defaultPackage.${ctx.system}
           inputs.pydev.defaultPackage.${ctx.system}
-
-          bashInteractive
+          inputs.secrets.defaultPackage.${ctx.system}
+          inputs.shell.defaultPackage.${ctx.system}
         ];
     };
   };
