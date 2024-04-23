f4995972ea74436fd785e29115835ffe7c106108
amanibhavam <iam@defn.sh>
2024-04-23 06:59:52 +0000

    dont manage here

--- m/tf/mod/terraform-coder-defn-managed/params.tf
+++
@@ -1,9 +0,0 @@
-data "coder_parameter" "instance" {
-  name         = "instance"
-  display_name = "Managed Instance ID"
-  description  = "Managed Instance ID"
-  type         = "string"
-  icon         = "/icon/memory.svg"
-  mutable      = false
-}
-
