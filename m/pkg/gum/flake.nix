a9e1e25abbb0ce3111a0f7b677c7367a9598b7b8
amanibhavam <iam@defn.sh>
2024-05-01 01:15:03 +0000

    upgrade gum [skip ci]

--- m/pkg/gum/flake.nix
+++ m/pkg/gum/flake.nix
@@ -7,29 +7,29 @@
 
     installPhase = pkg: ''
       install -m 0755 -d $out $out/bin
-      install -m 0755 gum $out/bin/gum
+      install -m 0755 */gum $out/bin/gum
     '';
 
     downloads = {
       "x86_64-linux" = {
         os = "Linux";
         arch = "x86_64";
-        sha256 = "sha256-1H60Rv3tsDd51099msh42Y/9isFs8ESVc9f3xWxnDn8="; # x86_64-linux
+        sha256 = "sha256-v5PDnXBvu0iIPZg7OnHNixYXWZpwIElTVztm7QwTNjA="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "Linux";
         arch = "arm64";
-        sha256 = "sha256-e8e14bDvovGDHGmBv3ImLaRzPogRb/ZxyB+qILepRyI="; # aarch64-linux
+        sha256 = "sha256-D0GQVefH84vDMMZ0ZtF5RB8T3GFH+ny1NJiUDgxG8i8="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "Darwin";
         arch = "x86_64";
-        sha256 = "sha256-fcnrYCcIPSe7EGdurQzdeWM2ijFeUFcI5DkNFZhHzlM="; # x86_64-darwin
+        sha256 = "sha256-s+GfpgirPjDoxkRTj/EBSuj3EOyDA7JCJ+sXYZzqKsw="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "Darwin";
         arch = "arm64";
-        sha256 = "sha256-I3TdfQZwfDNy5Sby5saS+Cblu4MJXD1K8LDhtfiut/o="; # aarch64-darwin
+        sha256 = "sha256-jCAeXRVxigz2YGVOgOPUxyYJ6XfqeLAVu7iPc0YEFGc="; # aarch64-darwin
       };
     };
   };
