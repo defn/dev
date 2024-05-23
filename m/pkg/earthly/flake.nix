6dd30aafffb12a90061d8ba2b5cc38f7cf12539f
amanibhavam <iam@defn.sh>
2024-05-23 18:14:15 +0000

    earthly/ 0.8.11 -> 0.8.12 [skip ci]

--- m/pkg/earthly/flake.nix
+++ m/pkg/earthly/flake.nix
@@ -18,22 +18,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-FRWETaF05388MdaGNDl8bGaBK0f5y+Iqc6Q97B3EjJg="; # x86_64-linux
+        sha256 = "sha256-dsiW/fFAe53lbH8bQ7779mQHPl0/mD4CVvfYDrZ+sCY="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-bBd2wc/zsn8nUZYyPeqD5aUhD7zjFEujhhCzNeAWOqc="; # aarch64-linux
+        sha256 = "sha256-AHJ/xJ+SplGJAyBJ9t4OWzDqKzINxEW1ZEB0d3qVI6I="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-YNBpAyQV130SZEM3B9SmYN2C2RkD5otRCA2G4web5iI="; # x86_64-darwin
+        sha256 = "sha256-i6OUxvNnbk0vQYaGhT8FX4LLvE+DTLZyFHtDVdkWvhE="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-aboDKTVSMd8avvLGhFxUjr80XBq67+sGpM+iHrMbTLc="; # aarch64-darwin
+        sha256 = "sha256-hAX+tXcmnPLOzKK7hBz1QdafQ3RNZy6FjEab/v4H1ek="; # aarch64-darwin
       };
     };
   };
