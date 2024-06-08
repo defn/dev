1074f66f1ced5345415d954ffb764b5d196c0491
amanibhavam <iam@defn.sh>
2024-06-08 06:35:28 +0000

    base on nixpkgs 24.05 [skip ci]

--- m/pkg/shell/flake.nix
+++ m/pkg/shell/flake.nix
@@ -1,13 +1,14 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
+    latest.url = github:NixOS/nixpkgs?rev=63dacb46bf939521bdc93981b4cbb7ecb58427a0;
   };
 
   outputs = inputs: inputs.pkg.main rec {
     src = builtins.path { path = ./.; name = "pkg-shell"; };
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
-      propagatedBuildInputs = with ctx.pkgs; [
+      propagatedBuildInputs = with (import inputs.latest { system = ctx.system; }); [
         procps
         vim
         openssh
