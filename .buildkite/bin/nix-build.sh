2a06dfa60ca7f871150d5ee9543aa0d1594d2dfb
amanibhavam <iam@defn.sh>
2024-06-14 06:47:28 +0000

    fix trunk

--- .buildkite/bin/nix-build.sh
+++ .buildkite/bin/nix-build.sh
@@ -10,4 +10,5 @@ else
 fi
 
 source .bash_profile
+# trunk-ignore(shellcheck/SC2016): ${1} is quoted so it can be evaluated in a runmany sub-shell
 runmany "$p" '(cd ${1} && nix build && attic push hello result);' m/pkg/*/
