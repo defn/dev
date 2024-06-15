66fccc583311d55b87107cb8c91a4b46415972df
amanibhavam <iam@defn.sh>
2024-06-15 07:20:12 +0000

    update pkg deps [skip ci]

--- m/pkg/pydev/flake.nix
+++ m/pkg/pydev/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
+    pkg.url = github:defn/dev/pkg-pkg-0.0.17?dir=m/pkg/pkg;
     # https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/interpreters/python
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/aliases.nix
     latest.url = github:NixOS/nixpkgs?rev=a47743431bb52c25eecbeda1bb2350902478befe;
