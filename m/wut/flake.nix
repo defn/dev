89ed7da369add04841bfa28f629f8d3bce5f9341
amanibhavam <iam@defn.sh>
2024-06-15 06:19:54 +0000

    working

--- m/wut/flake.nix
+++ m/wut/flake.nix
@@ -6,13 +6,34 @@
   outputs = inputs: inputs.pkg.main rec {
     src = builtins.path { path = ./.; name = "wut"; };
 
+    devShell = ctx: with ctx; pkgs.mkShell {
+      name = "wut-venv";
+      venvDir = "./.venv";
+
+      buildInputs = with pkgs.python3Packages; [
+        python
+        venvShellHook
+        numpy
+        (defaultPackage ctx) 
+      ];
+
+      postVenvCreation = ''
+        unset SOURCE_DATE_EPOCH
+        pip install -r requirements.txt
+      '';
+
+      postShellHook = ''
+        export MEH_LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.glibc pkgs.zlib pkgs.gcc.cc]}"
+      '';
+    };
+
     packages = ctx: {
       wut = ctx.pkgs.writeShellScriptBin "wut" ''
         exec env LD_LIBRARY_PATH=$MEH_LD_LIBRARY_PATH python wut.py
       '';
     };
 
-    defaultPackage = ctx: ctx.wrap.venvBuilder {
+    defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs = [
         (packages ctx).wut
       ];
