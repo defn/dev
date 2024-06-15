d6b013ae79fa6aae1438bcdc635e81968b88b6e3
amanibhavam <iam@defn.sh>
2024-06-15 07:05:41 +0000

    update pkg deps [skip ci]

--- m/pkg/attic/flake.nix
+++ m/pkg/attic/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    pkg.url = github:defn/dev/pkg-pkg-0.0.16?dir=m/pkg/pkg;
+    pkg.url = github:defn/dev/pkg-pkg-0.0.17?dir=m/pkg/pkg;
     attic.url = github:zhaofengli/attic?rev=717cc95983cdc357bc347d70be20ced21f935843;
   };
 
