2a6c5cbe819b2e3728881be8824662c6ded50f95
amanibhavam <iam@defn.sh>
2024-06-18 04:29:05 +0000

    upgrade packages, fix warning in ui

--- m/w/apps/story/.storybook/main.ts
+++ m/w/apps/story/.storybook/main.ts
@@ -1,17 +1,20 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from "@storybook/sveltekit";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
-    name: "@storybook/sveltekit",
+    name: getAbsolutePath("@storybook/sveltekit"),
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: {},
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
