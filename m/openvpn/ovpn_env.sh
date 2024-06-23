5899cf0a18d36163443d6124db007972a50d9e7c
amanibhavam <iam@defn.sh>
2024-06-23 21:32:56 +0000

    try

--- m/openvpn/ovpn_env.sh
+++ m/openvpn/ovpn_env.sh
@@ -1,14 +1,14 @@
 declare -x OVPN_AUTH=
 declare -x OVPN_CIPHER=
 declare -x OVPN_CLIENT_TO_CLIENT=
-declare -x OVPN_CN=penguin.linux.test
+declare -x OVPN_CN=192.168.1.26
 declare -x OVPN_COMP_LZO=0
 declare -x OVPN_DEFROUTE=1
 declare -x OVPN_DEVICE=tun
 declare -x OVPN_DEVICEN=0
 declare -x OVPN_DISABLE_PUSH_BLOCK_DNS=0
 declare -x OVPN_DNS=1
-declare -x OVPN_DNS_SERVERS=([0]="1.1.1.1" [1]="1.1.1.1")
+declare -x OVPN_DNS_SERVERS=([0]="1.1.1.1")
 declare -x OVPN_ENV=/home/ubuntu/m/openvpn/ovpn_env.sh
 declare -x OVPN_EXTRA_CLIENT_CONFIG=()
 declare -x OVPN_EXTRA_SERVER_CONFIG=()
@@ -21,5 +21,5 @@ declare -x OVPN_PROTO=udp
 declare -x OVPN_PUSH=([0]="route 100.79.169.78 255.255.255.255")
 declare -x OVPN_ROUTES=([0]="192.168.254.0/24")
 declare -x OVPN_SERVER=192.168.255.0/24
-declare -x OVPN_SERVER_URL=udp://penguin.linux.test
+declare -x OVPN_SERVER_URL=udp://192.168.1.26
 declare -x OVPN_TLS_CIPHER=
