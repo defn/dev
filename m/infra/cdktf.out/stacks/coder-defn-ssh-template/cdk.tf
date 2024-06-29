f3661322172a6392937267c3eb847de7f4278e85
amanibhavam <iam@defn.sh>
2024-06-29 17:10:06 +0000

    regen

--- m/infra/cdktf.out/stacks/coder-defn-ssh-template/cdk.tf
+++ m/infra/cdktf.out/stacks/coder-defn-ssh-template/cdk.tf
@@ -98,7 +98,7 @@ resource "null_resource" "deploy" {
   }
   count = data.coder_workspace.me.start_count
   provisioner "local-exec" {
-    command = "exec 2>&1; set -x; (echo cd; echo exec env CODER_AGENT_URL=${data.coder_workspace.me.access_url} CODER_AGENT_TOKEN=${coder_agent.main.token} CODER_NAME=${data.coder_workspace.me.name} CODER_HOMEDIR=${data.coder_parameter.homedir.value} CODER_INIT_SCRIPT_BASE64=${base64encode(coder_agent.main.init_script)} TS_AUTH_KEY=$(~/bin/ts-auth-key 2>/dev/null) ${data.coder_parameter.command.value}) | ${data.coder_parameter.remote.value} bash -x -"
+    command = "(echo cd; echo exec env CODER_AGENT_URL=${data.coder_workspace.me.access_url} CODER_AGENT_TOKEN=${coder_agent.main.token} CODER_NAME=${data.coder_workspace.me.name} CODER_HOMEDIR=${data.coder_parameter.homedir.value} CODER_INIT_SCRIPT_BASE64=${base64encode(coder_agent.main.init_script)} TS_AUTH_KEY=$(~/bin/ts-auth-key 2>/dev/null) ${data.coder_parameter.command.value}) | ${data.coder_parameter.remote.value} bash -x -"
     when    = create
   }
 }
