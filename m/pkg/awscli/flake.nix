b79de0a9c72b5b45b33bc63893c4a901509c78a8
amanibhavam <iam@defn.sh>
2024-08-13 00:23:56 +0000

    awscli/ 2.17.27 -> 2.17.28 [skip ci]

--- m/pkg/awscli/flake.nix
+++ m/pkg/awscli/flake.nix
@@ -43,20 +43,20 @@
         "x86_64-linux" = rec {
           os = "linux";
           arch = "x86_64";
-          sha256 = "sha256-CwO0ts4TWFN84RoCHEa82pD5Uysat9Jk4Qio+8rJSPI="; # x86_64-linux
+          sha256 = "sha256-3V+l4Y19M+gUOWhCYUOweEw+ftAO1kfQLVHpJ/P3POI="; # x86_64-linux
         };
         "aarch64-linux" = rec {
           os = "linux";
           arch = "aarch64";
-          sha256 = "sha256-5Me2hcrKM5Oql54SwMI4a8K3CikJO3/UvflH//2YHg8="; # aarch64-linux
+          sha256 = "sha256-aFhzKCdSu/8eLxHZT4W6YaryEORjoCebdAqFN0Z9B6o="; # aarch64-linux
         };
         "x86_64-darwin" = rec {
           os = "darwin";
-          sha256 = "sha256-VV/nxJ/dFQXRTGXUzYVCMNJlwk5J/aIYDOHKF3nGODk="; # x86_64-darwin
+          sha256 = "sha256-FWSVXd45E/GWOjvrJpLGv+ISiMsw8pbQiSU1gFBI0Zw="; # x86_64-darwin
         };
         "aarch64-darwin" = rec {
           os = "darwin";
-          sha256 = "sha256-VV/nxJ/dFQXRTGXUzYVCMNJlwk5J/aIYDOHKF3nGODk="; # aarch64-darwin
+          sha256 = "sha256-FWSVXd45E/GWOjvrJpLGv+ISiMsw8pbQiSU1gFBI0Zw="; # aarch64-darwin
         };
       };
     };
