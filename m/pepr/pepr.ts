88f233b22a74ce931292c93857ea60b8c9b82484
amanibhavam <iam@defn.sh>
2024-08-17 18:45:39 +0000

    load crd outside of the app

--- m/pepr/pepr.ts
+++ m/pepr/pepr.ts
@@ -1,5 +1,6 @@
 import { PeprModule } from "pepr";
+import { HelloPepr } from "./capabilities/hello";
 import { DefnPepr } from "./capabilities/defn";
 import cfg from "./package.json";
 
-new PeprModule(cfg, [DefnPepr]);
+new PeprModule(cfg, [HelloPepr, DefnPepr]);
