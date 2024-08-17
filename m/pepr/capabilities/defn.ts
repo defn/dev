fceda284140d6a36e8ae2f7fffc40a1bad65d273
amanibhavam <iam@defn.sh>
2024-08-17 18:23:29 +0000

    starat with only defn pepr

---
+++ m/pepr/capabilities/defn.ts
@@ -0,0 +1,33 @@
+import { Capability, K8s, Log, a, kind } from "pepr";
+
+export const DefnPepr = new Capability({
+  name: "defn",
+  description: "Sandbox Operator",
+  namespaces: ["defn"],
+});
+
+const { When, Store } = DefnPepr;
+
+When(a.Namespace)
+  .IsCreated()
+  .WithName("defn")
+  .Watch(async ns => {
+    Log.info("Namespace defn was created.");
+
+    try {
+      await K8s(kind.ConfigMap).Apply({
+        metadata: {
+          name: "defn",
+          namespace: "defn",
+        },
+        data: {
+          "ns-uid": ns.metadata?.uid,
+          counter: "1",
+        },
+      });
+    } catch (error) {
+      Log.error(error, "Failed to apply ConfigMap using server-side apply.");
+    }
+
+    Store.setItem("hello2", "world");
+  });
--- m/pepr/pepr.ts
+++ m/pepr/pepr.ts
@@ -1,6 +1,7 @@
-import { PeprModule } from "pepr";
-import { HelloPepr } from "./capabilities/hello";
+import { PeprModule, Log } from "pepr";
+// import { HelloPepr } from "./capabilities/hello";
+import { DefnPepr } from "./capabilities/defn";
 
 import cfg from "./package.json";
 
-new PeprModule(cfg, [HelloPepr]);
+new PeprModule(cfg, [DefnPepr]);
