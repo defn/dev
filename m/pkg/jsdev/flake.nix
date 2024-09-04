e66a6c359476e4f8a1bf401bab7017ec608debf5
amanibhavam <iam@defn.sh>
2024-09-04 01:41:30 +0000

    use node 20 [skip ci]

--- m/pkg/jsdev/flake.nix
+++ m/pkg/jsdev/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.17?dir=m/pkg/pkg;
     # https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/web/nodejs
-    latest.url = github:NixOS/nixpkgs?rev=d2cac0ee1bdb55879af0f98023e478d6fc7f5e41;
+    latest.url = github:NixOS/nixpkgs?rev=24536bf02cd9b199fdca550ba7e4f53b9a94119a;
   };
 
   outputs = inputs: inputs.pkg.main rec {
@@ -12,7 +12,7 @@
       propagatedBuildInputs =
         with (import inputs.latest { system = ctx.system; }); [
           # ghost doesn't support 20
-          nodejs_18
+          nodejs_20
         ];
     };
   };
