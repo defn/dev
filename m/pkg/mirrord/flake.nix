8e1033f1624b7d6109e1f9cdf2d2726f2f83cb65
amanibhavam <iam@defn.sh>
2024-09-04 02:01:15 +0000

    mirrord/ 3.115.1 -> 3.116.0 [skip ci]

--- m/pkg/mirrord/flake.nix
+++ m/pkg/mirrord/flake.nix
@@ -22,22 +22,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "x86_64";
-        sha256 = "sha256-9B4eJyBQc5OCAuxJ/1QI7LhGxAUHTvhJ918xdWwxTjc="; # x86_64-linux
+        sha256 = "sha256-jk+WYVoc6bPxQ83v4hGxfFLVqxQZ/SfBzIvp/CeIkR4="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "aarch64";
-        sha256 = "sha256-AJ1z/rv+xLKtYirpFPvLeQxLvjcnxPgWXWuj96ONlyQ="; # aarch64-linux
+        sha256 = "sha256-ywgwoiRFKcabRhFZTs0kjlKBAOen9Ey66vb/dIT0Ig0="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "mac";
         arch = "universal";
-        sha256 = "sha256-9q1S61x7jmRU14m/V3bMaSLyqIDeRyUUDf1bKE4pxIY="; # x86_64-darwin
+        sha256 = "sha256-ssP5jTGBgDWQ13/xsgECF/CgHZJQYy62noSKDIKtHUU="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "mac";
         arch = "universal";
-        sha256 = "sha256-9q1S61x7jmRU14m/V3bMaSLyqIDeRyUUDf1bKE4pxIY="; # aarch64-darwin
+        sha256 = "sha256-ssP5jTGBgDWQ13/xsgECF/CgHZJQYy62noSKDIKtHUU="; # aarch64-darwin
       };
     };
   };
