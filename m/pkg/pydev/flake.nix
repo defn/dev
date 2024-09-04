a9df4739e96289ec333439d1d7643c1957605d00
amanibhavam <iam@defn.sh>
2024-09-04 02:06:56 +0000

    update pkg deps [skip ci]

--- m/pkg/pydev/flake.nix
+++ m/pkg/pydev/flake.nix
@@ -3,7 +3,7 @@
     pkg.url = github:defn/dev/pkg-pkg-0.0.17?dir=m/pkg/pkg;
     # https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/interpreters/python
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/aliases.nix
-    latest.url = github:NixOS/nixpkgs?rev=a47743431bb52c25eecbeda1bb2350902478befe;
+    latest.url = github:NixOS/nixpkgs?rev=b857d97eec324bd792d84eea9e0fb61149795d91;
   };
 
   outputs = inputs: inputs.pkg.main rec {
