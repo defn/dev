10a3da988ec713697390dbf845a95ff59cf98462
amanibhavam <iam@defn.sh>
2024-09-04 03:07:41 +0000

    py wrapper [skip ci]

--- m/pkg/pydev/flake.nix
+++ m/pkg/pydev/flake.nix
@@ -9,10 +9,17 @@
   outputs = inputs: inputs.pkg.main rec {
     src = builtins.path { path = ./.; name = "pkg-pydev"; };
 
+    packages = ctx: {
+      wut = with ctx.pkgs; writeShellScriptBin "py" ''
+        exec env LD_LIBRARY_PATH="${lib.makeLibraryPath [glibc zlib gcc.cc]}" "$@"
+      '';
+    };
+
     defaultPackage = ctx: ctx.wrap.nullBuilder {
       propagatedBuildInputs =
         with (import inputs.latest { system = ctx.system; }); [
           (python3.withPackages (ps: with ps; [ pip pipx ]))
+          (packages ctx).wut
         ];
     };
   };
