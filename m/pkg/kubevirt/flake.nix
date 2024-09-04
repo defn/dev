8c9882d2b5d94f546ca11b64f629f1e99de29d0e
amanibhavam <iam@defn.sh>
2024-09-04 01:55:57 +0000

    kubevirt/ 1.3.0 -> 1.3.1 [skip ci]

--- m/pkg/kubevirt/flake.nix
+++ m/pkg/kubevirt/flake.nix
@@ -18,22 +18,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-xbwdDOoJVkXzrKT7hsjp3ie5SfewbgiHNHJUdZYQSrc="; # x86_64-linux
+        sha256 = "sha256-XPA/EVc31BnF3HHZ1TCPkaGhk1+oEZ9wlXIBFE/5pP4="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-xbwdDOoJVkXzrKT7hsjp3ie5SfewbgiHNHJUdZYQSrc="; # aarch64-linux
+        sha256 = "sha256-XPA/EVc31BnF3HHZ1TCPkaGhk1+oEZ9wlXIBFE/5pP4="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-bMlYTTCEbNRXHj3r0HuLE/0/zmypsMRrXWX+iFWt6CA="; # x86_64-darwin
+        sha256 = "sha256-hyghQUNhsFjBDLSCLThPbJqm23auIx7wvrLBnPL6s54="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-bMlYTTCEbNRXHj3r0HuLE/0/zmypsMRrXWX+iFWt6CA="; # aarch64-darwin
+        sha256 = "sha256-hyghQUNhsFjBDLSCLThPbJqm23auIx7wvrLBnPL6s54="; # aarch64-darwin
       };
     };
   };
