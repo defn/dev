48c6ab74079f08871cdca81b69d535b305a0e2f0
amanibhavam <iam@defn.sh>
2024-09-21 05:22:24 +0000

    no other flake inputs [skip ci]

--- m/pkg/godev/flake.nix
+++ m/pkg/godev/flake.nix
@@ -1,13 +1,11 @@
 {
   inputs = {
-    cosign.url = github:defn/dev/pkg-cosign-2.4.0-1?dir=m/pkg/cosign;
-    goreleaser.url = github:defn/dev/pkg-goreleaser-2.3.2-1?dir=m/pkg/goreleaser;
-    yaegi.url = github:defn/dev/pkg-yaegi-0.16.1-2?dir=m/pkg/yaegi;
+    pkg.url = github:defn/dev/pkg-pkg-0.0.17?dir=m/pkg/pkg;
     # https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/compilers/go
     latest.url = github:NixOS/nixpkgs?rev=6802206bcc36c1be1ad0a3ff3a503d6d9502d4ac;
   };
 
-  outputs = inputs: inputs.goreleaser.inputs.pkg.main rec {
+  outputs = inputs: inputs.pkg.main rec {
     src = builtins.path { path = ./.; name = "pkg-godev"; };
 
     defaultPackage = ctx: ctx.wrap.nullBuilder {
@@ -22,9 +20,6 @@
           delve
           gopls
           go_1_23
-          inputs.cosign.defaultPackage.${ctx.system}
-          inputs.goreleaser.defaultPackage.${ctx.system}
-          inputs.yaegi.defaultPackage.${ctx.system}
         ];
     };
   };
