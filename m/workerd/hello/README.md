50369cbb056d7498a048b7686021fbd18a82c645
amanibhavam <iam@defn.sh>
2024-09-25 03:04:26 +0000

    workerd hello example

---
+++ m/workerd/hello/README.md
@@ -0,0 +1,1 @@
+This came from https://github.com/cloudflare/workerd/tree/main/samples/hello-wasm 4bc2a9687ae82c4610cfbd21b6edbe92cd6227f4
--- m/workerd/hello/src/lib.rs
+++ m/workerd/hello/src/lib.rs
@@ -4,11 +4,9 @@ mod utils;
 
 fn log_request(req: &Request) {
     console_log!(
-        "{} - [{}], located at: {:?}, within: {}",
+        "{} - [{}]",
         Date::now().to_string(),
-        req.path(),
-        req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or_else(|| "unknown region".into())
+        req.path()
     );
 }
 
