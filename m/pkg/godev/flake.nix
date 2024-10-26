f892498f28e6d536b7cae53bfa2c949380e5c0f8
amanibhavam <iam@defn.sh>
2024-10-26 02:35:07 +0000

    go 1.23.2 [skip ci]

--- m/pkg/godev/flake.nix
+++ m/pkg/godev/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.17?dir=m/pkg/pkg;
     # https://github.com/NixOS/nixpkgs/tree/master/pkgs/development/compilers/go
-    latest.url = github:NixOS/nixpkgs?rev=6802206bcc36c1be1ad0a3ff3a503d6d9502d4ac;
+    latest.url = github:NixOS/nixpkgs?rev=5942e21e4e4add4d1e3a35415534c936ac75f001;
   };
 
   outputs = inputs: inputs.pkg.main rec {
