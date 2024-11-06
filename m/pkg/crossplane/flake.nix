734155fb18d0c6381b47c57ea13a9f5e0c760776
amanibhavam <iam@defn.sh>
2024-11-06 02:47:58 +0000

    crossplane/ 1.17.2 -> 1.18.0 [skip ci]

--- m/pkg/crossplane/flake.nix
+++ m/pkg/crossplane/flake.nix
@@ -19,22 +19,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-JWwepB7tXiLi2bOMaKByuV5GENGQO4M6KqsfsDOH0lg="; # x86_64-linux
+        sha256 = "sha256-CGxgQHjuzW5vHk3n2k2yq/cKTXXRiKPlrv2mrF0h4uY="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-oEFWaTY0Bcdq5dsi7+gmQXRRFbCc9LphEDmMt99GZcU="; # aarch64-linux
+        sha256 = "sha256-8tr0aga7klU2wp4E7j2iACYwaS32PJN10o9VhiCvTkA="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-R3lGYT/QH97wsd3qVScVl8i3jzKWmrxHucGyvnwxnL8="; # x86_64-darwin
+        sha256 = "sha256-o8ixcyjl983g1FubnwZet+L7iioS/8ab8Beq4+lYf4Q="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-R3lGYT/QH97wsd3qVScVl8i3jzKWmrxHucGyvnwxnL8="; # aarch64-darwin
+        sha256 = "sha256-o8ixcyjl983g1FubnwZet+L7iioS/8ab8Beq4+lYf4Q="; # aarch64-darwin
       };
     };
   };
