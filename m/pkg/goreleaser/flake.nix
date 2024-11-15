6d86bf43e2e4e0c59dd07fcd9ddf6134a197eee0
amanibhavam <iam@defn.sh>
2024-11-15 02:47:11 +0000

    goreleaser/ 2.4.4 -> 2.4.5 [skip ci]

--- m/pkg/goreleaser/flake.nix
+++ m/pkg/goreleaser/flake.nix
@@ -16,22 +16,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "x86_64";
-        sha256 = "sha256-vkxHoioCDu2jZAqIpuLgLLVl9GkYA0fYnhk2Wd2EwV4="; # x86_64-linux
+        sha256 = "sha256-SPC+agAeiPBurrxwEpNdbGMGE3oA3V4MA0xJubuEkgs="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-vQ09b54E4wLKFu7OGg/c3DxdmYYeKIKqBsrvsXkTxoc="; # aarch64-linux
+        sha256 = "sha256-pl4Pk86PUG9IOC70TY0u50UG7gCgKudNbhHa4UnZVgo="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "x86_64";
-        sha256 = "sha256-9aw1/sY75XxcRrw0gqpSfQY5iMtar76SlMQTO2sKjoc="; # x86_64-darwin
+        sha256 = "sha256-V6Gasj1n9jzBhwyrH8t9pUoBAwvYv5BtyZ/b4L5owp8="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "arm64";
-        sha256 = "sha256-jW3fMdE88zPGrijEjJnjLouzFkHFAu+mCpKIHuHTZ8U="; # aarch64-darwin
+        sha256 = "sha256-sQGmraKN8xIo9zfcJnsQJUdHDZF6CT+1AJDu7xCsv2M="; # aarch64-darwin
       };
     };
   };
