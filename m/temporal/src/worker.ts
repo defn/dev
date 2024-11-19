a88a2707311333c667b01cb7e84fc5aebedc8319
amanibhavam <iam@defn.sh>
2024-11-19 03:20:07 +0000

    temporal demo

---
+++ m/temporal/src/worker.ts
@@ -0,0 +1,17 @@
+// @@@SNIPSTART typescript-esm-worker
+import { Worker } from '@temporalio/worker';
+import { URL, fileURLToPath } from 'url';
+import path from 'path';
+import * as activities from './activities.js';
+
+// Support running both complied code and ts-node/esm loader
+const workflowsPathUrl = new URL(`./workflows${path.extname(import.meta.url)}`, import.meta.url);
+
+const worker = await Worker.create({
+  workflowsPath: fileURLToPath(workflowsPathUrl),
+  activities,
+  taskQueue: 'fetch-esm',
+});
+
+await worker.run();
+// @@@SNIPEND
