d61e4e95e806646207eeed7f7a447304ba4da9d3
amanibhavam <iam@defn.sh>
2024-11-21 03:43:53 +0000

    dont need much except for the workflow type

--- m/pepr/capabilities/defn.ts
+++ m/pepr/capabilities/defn.ts
@@ -5,12 +5,8 @@ import { components } from "../Script";
 
 import { Client } from "@temporalio/client";
 
-export async function greetHTTP(name: string): Promise<string> {
-  return `Hello, ${name}!`;
-}
-
 export async function example(name: string): Promise<string> {
-  return await greetHTTP(name);
+  return name;
 }
 
 class ScriptKind extends a.GenericKind {
@@ -32,13 +28,23 @@ export const DefnPepr = new Capability({
 const { When } = DefnPepr;
 
 When(ScriptKind)
+  .IsDeleted()
+  .Watch(async scr => {
+    await K8s(kind.ConfigMap).Delete({
+      metadata: {
+        name: scr.metadata.name,
+        namespace: scr.metadata.namespace,
+      },
+    });
+  });
+
+When(ScriptKind)
   .IsCreatedOrUpdated()
   .Watch(async scr => {
     Log.info("Script workdir: " + scr.spec.workdir);
     Log.info("Script script: " + scr.spec.script);
 
-    let output = "";
-    exec(scr.spec.script, (error, stdout, stderr) => {
+    exec(scr.spec.script, async (error, stdout, stderr) => {
       if (error) {
         Log.error(`Error executing script: ${error}`);
         return;
@@ -51,32 +57,28 @@ When(ScriptKind)
 
       Log.info(`Script output: ${stdout}`);
 
-      output = stdout;
-    });
-
-    const client = new Client();
-    const result = await client.workflow.execute(example, {
-      taskQueue: "fetch-esm",
-      workflowId: `batch-${process.pid}`,
-      args: [`Temporal: ${scr.spec.script}`],
-    });
-
-    try {
-      await K8s(ScriptKind).Apply({
-        metadata: {
-          name: scr.metadata.name,
-          namespace: scr.metadata.namespace,
-          annotations: {
-            results: `From Temporal: ${result}, From Output: ${output}`,
-          },
-        },
+      const client = new Client();
+      const result = await client.workflow.execute(example, {
+        taskQueue: "fetch-esm",
+        workflowId: `batch-${process.pid}-${Math.random()}`,
+        args: [scr.spec.script],
       });
-    } catch (error) {
-      Log.error(
-        error,
-        `Failed to apply ConfigMap using server-side apply: ${error}`,
-      );
-    }
+
+      await K8s(kind.ConfigMap)
+        .Apply({
+          metadata: {
+            name: scr.metadata.name,
+            namespace: scr.metadata.namespace,
+          },
+          data: {
+            result: `From Temporal: ${result}, From Exec: ${stdout}`,
+          },
+        })
+        .then(() => Log.info("WebApp CRD registered"))
+        .catch(err => {
+          Log.error(err);
+        });
+    });
   });
 
 When(a.Namespace)
