f277b60588c2802c932dbf3176d22947820c744d
amanibhavam <iam@defn.sh>
2024-11-26 02:36:04 +0000

    Revert "remove joyide for now"
    This reverts commit c40d65eea61f9fae27b481e8213bb75d348d71e2.

---
+++ .joyride/scripts/tutorial.cljs
@@ -0,0 +1,5 @@
+(ns tutorial)
+
+(def tutorial_filename "m/toc/tutorial.cue")
+
+(def tutorial_webpage "m/toc/tutorial.html")
---
+++ m/coder/class/.joyride/scripts/tutorial.cljs
@@ -0,0 +1,5 @@
+(ns tutorial)
+
+(def tutorial_filename "Earthfile")
+
+(def tutorial_webpage "tutorial.html")
