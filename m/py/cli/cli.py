72d2be6a9f0860db73a57271e87bd02b67ad5023
amanibhavam <iam@defn.sh>
2024-12-08 04:19:10 +0000

    print out sys.path clearer

--- m/py/cli/cli.py
+++ m/py/cli/cli.py
@@ -9,6 +9,9 @@ def main():
         print("Usage: python cli.py <name>")
         sys.exit(1)
 
+    for f in sys.path:
+        print(f)
+
     name = sys.argv[1]
     print(hello(name))
     print(goodbye(name))
