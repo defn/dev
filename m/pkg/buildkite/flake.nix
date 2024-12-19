41038947000004a9d554b22656cb78572b0ad3dc
amanibhavam <iam@defn.sh>
2024-12-19 05:52:54 +0000

    buildkite/ 3.87.1 -> 3.88.0 [skip ci]

--- m/pkg/buildkite/flake.nix
+++ m/pkg/buildkite/flake.nix
@@ -16,22 +16,22 @@
       "x86_64-linux" = {
         os = "linux";
         arch = "amd64";
-        sha256 = "sha256-2/hY7epi1LorNDlM/GnYl7lduFS8TFcxOmZuhurdv4U="; # x86_64-linux
+        sha256 = "sha256-i1QYWxjTIvkFCb7ogWa/pcBgqIKliRrAu8AbsHn4Aks="; # x86_64-linux
       };
       "aarch64-linux" = {
         os = "linux";
         arch = "arm64";
-        sha256 = "sha256-nyA/8GWm4uoP+pV5hliWT/bV9wx8tu3TT4z1DhrNybo="; # aarch64-linux
+        sha256 = "sha256-HfYUduW6qY1B5ib8Amdnzb+i0qxaQtf5IgCsd5cduFw="; # aarch64-linux
       };
       "x86_64-darwin" = {
         os = "darwin";
         arch = "amd64";
-        sha256 = "sha256-4nuXrnQ84OsNoGpeBd2gKB3br+9qgnL58F183CusGLg="; # x86_64-darwin
+        sha256 = "sha256-c0Nm/iM7wZxJkoBk47uoh5tLDbT1+T65QP1uiblK18Q="; # x86_64-darwin
       };
       "aarch64-darwin" = {
         os = "darwin";
         arch = "amd64"; # no arm64 macos
-        sha256 = "sha256-4nuXrnQ84OsNoGpeBd2gKB3br+9qgnL58F183CusGLg="; # aarch64-darwin
+        sha256 = "sha256-c0Nm/iM7wZxJkoBk47uoh5tLDbT1+T65QP1uiblK18Q="; # aarch64-darwin
       };
     };
   };
