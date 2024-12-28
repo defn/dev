a94a5ff64016e70df2dc65b7cd5a9eccc15ed4f3
amanibhavam <iam@defn.sh>
2024-12-28 22:06:32 +0000

    create rest of astro websites

---
+++ m/w/defn.dev/astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    assets: "a",
+  },
+  integrations: [
+    starlight({
+      title: "Docs with Tailwind",
+      social: {
+        github: "https://github.com/defn/defn.dev",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [{ label: "Example Guide", slug: "guides/example" }],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
+});
