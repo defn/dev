9f34de08028580db77004f22a10a1d574527e123
amanibhavam <iam@defn.sh>
2024-12-30 00:13:44 +0000

    working hello endpoint

---
+++ m/w/apps/fn1/src/pages/hello.ts
@@ -0,0 +1,8 @@
+export async function GET() {
+  return new Response(JSON.stringify({ hello: "world" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
