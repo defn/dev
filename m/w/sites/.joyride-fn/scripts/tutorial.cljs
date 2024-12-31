70013ced5c8d03583a90aed71a110824c890e00c
amanibhavam <iam@defn.sh>
2024-12-31 21:25:48 +0000

    format wtih turnk

---
+++ m/w/sites/.joyride-fn/scripts/tutorial.cljs
@@ -0,0 +1,5 @@
+(ns tutorial)
+
+(def tutorial_filename "src/pages/hello.ts")
+
+(def tutorial_webpage ".joyride/scripts/tutorial.html")
--- m/w/sites/defn.run/fn/fn1/src/pages/hello.ts
+++ m/w/sites/defn.run/fn/fn1/src/pages/hello.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  return new Response(JSON.stringify({ hello: "world" }), {
+  return new Response(JSON.stringify({ hello: "worldx" }), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
