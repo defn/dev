28863ba1f2ef723582bf949e926130d77de1babb
amanibhavam <iam@defn.sh>
2025-01-01 04:36:44 +0000

    fixed the types

--- m/w/sites/defn.run/fn/fn2/src/pages/index.ts
+++ m/w/sites/defn.run/fn/fn2/src/pages/index.ts
@@ -1,13 +1,14 @@
+import type { APIContext } from "astro";
 import { MyDurableObject } from "./mydo.ts";
 export { MyDurableObject } from "./mydo.ts";
 
-export async function GET(context: any) {
+export async function GET(context: APIContext) {
+  const url = new URL(context.request.url);
+  const params = new URLSearchParams(url.search);
+
   const env = context.locals.runtime.env;
   const id = env.fndo.idFromName("foo");
   const stub: MyDurableObject = env.fndo.get(id);
-
-  const url = new URL(context.request.url);
-  const params = new URLSearchParams(url.search);
 
   var response = await stub.sayHello(params.get("thing"));
 
