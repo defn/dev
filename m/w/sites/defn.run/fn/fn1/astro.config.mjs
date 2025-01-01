0930e94ae1977a761fd320625feafa46ca1cdc25
amanibhavam <iam@defn.sh>
2025-01-01 12:21:08 +0000

    tailwind

--- m/w/sites/defn.run/fn/fn1/astro.config.mjs
+++ m/w/sites/defn.run/fn/fn1/astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
@@ -13,5 +15,5 @@ export default defineConfig({
       enabled: true,
     },
   }),
-  integrations: [svelte()],
+  integrations: [svelte(), tailwind()],
 });
