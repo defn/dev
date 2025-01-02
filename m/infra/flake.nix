2a58b61f78894b510096291552f7b008aed29785
amanibhavam <iam@defn.sh>
2025-01-02 02:12:53 +0000

    update flakes

--- m/infra/flake.nix
+++ m/infra/flake.nix
@@ -1,10 +1,10 @@
 {
   inputs = {
     pkg.url = github:defn/dev/pkg-pkg-0.0.17?dir=m/pkg/pkg;
-    terraform.url = github:defn/dev/pkg-terraform-1.10.1-1?dir=m/pkg/terraform;
-    helm.url = github:defn/dev/pkg-helm-3.16.3-1?dir=m/pkg/helm;
+    terraform.url = github:defn/dev/pkg-terraform-1.10.3-1?dir=m/pkg/terraform;
+    helm.url = github:defn/dev/pkg-helm-3.16.4-1?dir=m/pkg/helm;
     kustomize.url = github:defn/dev/pkg-kustomize-5.5.0-1?dir=m/pkg/kustomize;
-    shell.url = github:defn/dev/pkg-shell-0.0.70?dir=m/pkg/shell;
+    shell.url = github:defn/dev/pkg-shell-0.0.71?dir=m/pkg/shell;
     latest.url = github:NixOS/nixpkgs?rev=4b873163c34e9c4ac7ebaf5a74a1af4f483e027c;
   };
 
