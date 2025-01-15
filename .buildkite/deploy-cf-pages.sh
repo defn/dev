c29db8a73739166e78989d68de7977d350faf879
amanibhavam <iam@defn.sh>
2025-01-15 02:55:58 +0000

    nix and mise should already be installed

--- .buildkite/deploy-cf-pages.sh
+++ .buildkite/deploy-cf-pages.sh
@@ -10,7 +10,7 @@ docker run --rm \
     -v $(pwd)/.git:/home/ubuntu/.git \
     -v bazel-cache-1:/home/ubuntu/work/bazel \
     --entrypoint bash 169.254.32.1:5000/defn/dev -c \
-    "sudo chown ubuntu:ubuntu work/bazel; git reset --hard && source .bash_profile && make nix && cd m && mise trust -a && mise install && b build"
+    "sudo chown ubuntu:ubuntu work/bazel; git reset --hard && source .bash_profile && cd m && b build"
 
 docker run --rm \
     -v $(pwd)/.git:/home/ubuntu/.git \
