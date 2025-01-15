aa8b18277628eb391b164e608cbf6b9e18f9a973
amanibhavam <iam@defn.sh>
2025-01-15 02:39:10 +0000

    build bazel and wensite separately

--- .buildkite/deploy-cf-pages.sh
+++ .buildkite/deploy-cf-pages.sh
@@ -8,4 +8,10 @@ docker run --rm \
     -v $(pwd):/home/ubuntu \
     -v bazel-cache-1:/home/ubuntu/work/bazel \
     --entrypoint bash 169.254.32.1:5000/defn/dev -c \
-    "sudo chown ubuntu:ubuntu work/bazel; source .bash_profile && cd ${website} && m install && m package"
+    "sudo chown ubuntu:ubuntu work/bazel; source .bash_profile && cd m && b build"
+
+docker run --rm \
+    -v $(pwd):/home/ubuntu \
+    -v bazel-cache-1:/home/ubuntu/work/bazel \
+    --entrypoint bash 169.254.32.1:5000/defn/dev -c \
+    "source .bash_profile && cd ${website} && m install && m package"
