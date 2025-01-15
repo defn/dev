98a57d5670f299557dcbf41f9c87580d7578cf0a
amanibhavam <iam@defn.sh>
2025-01-15 02:54:22 +0000

    git reset to preserve nix links

--- .buildkite/deploy-cf-pages.sh
+++ .buildkite/deploy-cf-pages.sh
@@ -7,13 +7,13 @@ website="$1"; shift
 mkdir -p work/bazel
 
 docker run --rm \
-    -v $(pwd):/home/ubuntu \
+    -v $(pwd)/.git:/home/ubuntu/.git \
     -v bazel-cache-1:/home/ubuntu/work/bazel \
     --entrypoint bash 169.254.32.1:5000/defn/dev -c \
-    "sudo chown ubuntu:ubuntu work/bazel; source .bash_profile && make nix && cd m && mise trust -a && mise install && b build"
+    "sudo chown ubuntu:ubuntu work/bazel; git reset --hard && source .bash_profile && make nix && cd m && mise trust -a && mise install && b build"
 
 docker run --rm \
-    -v $(pwd):/home/ubuntu \
+    -v $(pwd)/.git:/home/ubuntu/.git \
     -v bazel-cache-1:/home/ubuntu/work/bazel \
     --entrypoint bash 169.254.32.1:5000/defn/dev -c \
-    "source .bash_profile && cd ${website} && m install && m package"
+    "git reset --hard && source .bash_profile && cd ${website} && m install && m package"
