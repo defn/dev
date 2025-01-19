1dd1263dc8457fc26eb7ffdd89d0aaeba98fff1a
amanibhavam <iam@defn.sh>
2025-01-19 04:19:45 +0000

    fix: iframe the external, proxied url

--- .config/joyride/src/tutlib.cljs
+++ .config/joyride/src/tutlib.cljs
@@ -2,9 +2,10 @@
   (:require ["path" :as path]
             ["vscode" :as vscode]
             [promesa.core :as p]
+            [clojure.string :as s]
             [tutorial :as tutorial]))
 
-(defn- open-tutorial [tutorial_filename, tutorial_webpage]
+(defn- open-tutorial [tutorial_filename tutorial_webpage tutorial_url]
   ;; open tutorial main file
   (p/let [doc (vscode/workspace.openTextDocument (path/join vscode/workspace.rootPath tutorial_filename))
           meh (vscode/window.showTextDocument doc #js {:preview false, :preserveFocus false, :viewColumn: vscode/ViewColumn.One})]
@@ -13,6 +14,10 @@
     (p/let [panel (vscode/window.createWebviewPanel "Tutorial" "Tutorial" vscode/ViewColumn.Two #js {:enableScripts true})
             uri (vscode/Uri.file (path/join vscode/workspace.rootPath tutorial_webpage))
             data (vscode/workspace.fs.readFile uri)
-            html (.decode (js/TextDecoder. "utf-8") data)]
-      (set! (.. panel -webview -html) (str html)))))
+            html (.decode (js/TextDecoder. "utf-8") data)
+            url (vscode/env.asExternalUri (vscode/Uri.parse tutorial_url))]
+
+      (set!  (.. panel -webview -html) (str (s/replace html "{{url}}" url))))))
+
+
 
--- m/w/sites/.joyride-site/scripts/tutorial.cljs
+++ m/w/sites/.joyride-site/scripts/tutorial.cljs
@@ -1,9 +1,11 @@
 (ns tutorial
-  (:require [tutlib :as tutlib]))
+  (:require [tutlib :as tutlib]
+            [clojure.string :as s]))
 
 (def tutorial_filename "src/content/docs/index.mdx")
 
 (def tutorial_webpage "tutorial.html")
 
+(def tutorial_url "http://localhost:4341")
 
-(tutlib/open-tutorial tutorial_filename tutorial_webpage)
+(tutlib/open-tutorial tutorial_filename tutorial_webpage tutorial_url)
