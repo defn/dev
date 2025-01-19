4a1584df15599b04183ff1efd24d86c8db192221
amanibhavam <iam@defn.sh>
2025-01-19 04:55:01 +0000

    ci: bazel build once during home instead of website

--- .buildkite/bin/deploy-cf-pages.sh
+++ .buildkite/bin/deploy-cf-pages.sh
@@ -5,14 +5,6 @@ set -efu -o pipefail
 website="$1"
 shift
 
-mkdir -p work/bazel
-
-docker run --rm \
-	-v $(pwd)/.git:/home/ubuntu/.git \
-	-v bazel-cache-1:/home/ubuntu/work/bazel \
-	--entrypoint bash 169.254.32.1:5000/defn/dev -c \
-	"sudo chown ubuntu:ubuntu work/bazel; git reset --hard && source .bash_profile && b build"
-
 docker run --rm \
 	-v $(pwd)/.git:/home/ubuntu/.git \
 	-v bazel-cache-1:/home/ubuntu/work/bazel \
--- .buildkite/bin/home-build.sh
+++ .buildkite/bin/home-build.sh
@@ -6,3 +6,11 @@ source .bash_profile
 cd m/i
 source ~/work/.buildkite/.env
 make latest
+
+mkdir -p work/bazel
+
+docker run --rm \
+	-v $(pwd)/.git:/home/ubuntu/.git \
+	-v bazel-cache-1:/home/ubuntu/work/bazel \
+	--entrypoint bash 169.254.32.1:5000/defn/dev -c \
+	"sudo chown ubuntu:ubuntu work/bazel; git reset --hard && source .bash_profile && cd m && b build"
