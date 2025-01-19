71a3a77ce325e1eb09c0182abe225b26e4816ff5
amanibhavam <iam@defn.sh>
2025-01-19 04:50:56 +0000

    fix: minimal bazel build for website changes

--- .buildkite/bin/deploy-cf-pages.sh
+++ .buildkite/bin/deploy-cf-pages.sh
@@ -11,7 +11,7 @@ docker run --rm \
 	-v $(pwd)/.git:/home/ubuntu/.git \
 	-v bazel-cache-1:/home/ubuntu/work/bazel \
 	--entrypoint bash 169.254.32.1:5000/defn/dev -c \
-	"sudo chown ubuntu:ubuntu work/bazel; git reset --hard && source .bash_profile && cd m && b build"
+	"sudo chown ubuntu:ubuntu work/bazel; git reset --hard && source .bash_profile && b build"
 
 docker run --rm \
 	-v $(pwd)/.git:/home/ubuntu/.git \
