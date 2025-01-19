bb0ab410313d5c095b972f7f3d226968484ae976
amanibhavam <iam@defn.sh>
2025-01-19 04:32:52 +0000

    feat: read dev server port from app_port

--- .config/joyride/src/tutlib.cljs
+++ .config/joyride/src/tutlib.cljs
@@ -5,17 +5,19 @@
             [clojure.string :as s]
             [tutorial :as tutorial]))
 
-(defn- open-tutorial [tutorial_filename tutorial_webpage tutorial_url]
+(defn- open-tutorial [tutorial_filename tutorial_webpage]
   ;; open tutorial main file
   (p/let [doc (vscode/workspace.openTextDocument (path/join vscode/workspace.rootPath tutorial_filename))
           meh (vscode/window.showTextDocument doc #js {:preview false, :preserveFocus false, :viewColumn: vscode/ViewColumn.One})]
 
     ;; open tutorial.html
     (p/let [panel (vscode/window.createWebviewPanel "Tutorial" "Tutorial" vscode/ViewColumn.Two #js {:enableScripts true})
-            uri (vscode/Uri.file (path/join vscode/workspace.rootPath tutorial_webpage))
-            data (vscode/workspace.fs.readFile uri)
-            html (.decode (js/TextDecoder. "utf-8") data)
-            url (vscode/env.asExternalUri (vscode/Uri.parse tutorial_url))]
+            webpage (vscode/Uri.file (path/join vscode/workspace.rootPath tutorial_webpage))
+            webdata (vscode/workspace.fs.readFile webpage)
+            portpage (vscode/Uri.file (path/join vscode/workspace.rootPath ".app_port"))
+            portdata (vscode/workspace.fs.readFile portpage)
+            html (.decode (js/TextDecoder. "utf-8") webdata)
+            url (vscode/env.asExternalUri (vscode/Uri.parse (str "http://localhost:" portdata)))]
 
       (set!  (.. panel -webview -html) (str (s/replace html "{{url}}" url))))))
 
--- m/w/sites/.joyride-site/scripts/tutorial.cljs
+++ m/w/sites/.joyride-site/scripts/tutorial.cljs
@@ -1,11 +1,4 @@
 (ns tutorial
-  (:require [tutlib :as tutlib]
-            [clojure.string :as s]))
+  (:require [tutlib :as tutlib]))
 
-(def tutorial_filename "src/content/docs/index.mdx")
-
-(def tutorial_webpage "tutorial.html")
-
-(def tutorial_url "http://localhost:4341")
-
-(tutlib/open-tutorial tutorial_filename tutorial_webpage tutorial_url)
+(tutlib/open-tutorial "src/content/docs/index.mdx" "tutorial.html")
