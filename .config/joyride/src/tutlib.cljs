e46a7b9ca3ce5c0e5bcd3f71db6d2114bbae3baf
amanibhavam <iam@defn.sh>
2025-01-26 04:51:16 +0000

    check if .app_up is non-empty

--- .config/joyride/src/tutlib.cljs
+++ .config/joyride/src/tutlib.cljs
@@ -6,20 +6,27 @@
             [tutorial :as tutorial]))
 
 (defn- open-tutorial [tutorial_filename tutorial_webpage]
-  ;; open tutorial main file
-  (p/let [doc (vscode/workspace.openTextDocument (path/join vscode/workspace.rootPath tutorial_filename))
-          meh (vscode/window.showTextDocument doc #js {:preview false, :preserveFocus false, :viewColumn: vscode/ViewColumn.One})]
-
-    ;; open tutorial.html
-    (p/let [panel (vscode/window.createWebviewPanel "Tutorial" "Tutorial" vscode/ViewColumn.Two #js {:enableScripts true})
+    (p/let [
             webpage (vscode/Uri.file (path/join vscode/workspace.rootPath tutorial_webpage))
             webdata (vscode/workspace.fs.readFile webpage)
+            webhtml (.decode (js/TextDecoder. "utf-8") webdata)
             portpage (vscode/Uri.file (path/join vscode/workspace.rootPath ".app_port"))
             portdata (vscode/workspace.fs.readFile portpage)
-            html (.decode (js/TextDecoder. "utf-8") webdata)
-            url (vscode/env.asExternalUri (vscode/Uri.parse (str "http://localhost:" portdata)))]
-
-      (set!  (.. panel -webview -html) (str (s/replace html "{{url}}" url))))))
-
+            url (vscode/env.asExternalUri (vscode/Uri.parse (str "http://localhost:" portdata)))
+            sfname (vscode/Uri.file (path/join vscode/workspace.rootPath ".app_up"))
+            sdata (vscode/workspace.fs.readFile sfname)]
+      (if (> (.-length sdata) 0)
+        (do
+          (p/let [
+                  doc (vscode/workspace.openTextDocument (path/join vscode/workspace.rootPath tutorial_filename))
+                  meh (vscode/window.showTextDocument doc #js {:preview false, :preserveFocus false, :viewColumn: vscode/ViewColumn.One})
+                  panel (vscode/window.createWebviewPanel "Tutorial" "Tutorial" vscode/ViewColumn.Two #js {:enableScripts true})]
+            (set!  (.. panel -webview -html) (str (s/replace webhtml "{{url}}" url)))))
+        (do
+          (js/console.log (str "sleeping..." sdata "."))
+          (js/setTimeout 
+            (fn [] (do
+              (open-tutorial tutorial_filename tutorial_webpage)))
+            1000)))))
 
 
