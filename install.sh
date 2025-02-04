9dc7f3edd546d2ef20a49b4aeaf1a76615d652d3
amanibhavam <iam@defn.sh>
2025-02-04 16:04:39 +0000

    fix: install needs dirmgnr, git

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -efu -o pipefail
 
 function main {
     sudo apt update
-    sudo apt install -y make direnv curl xz-utils
+    sudo apt install -y make direnv curl xz-utils dirmngr gpg
     source .bash_profile
     make install
 }
