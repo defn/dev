d9436ffc1ee045951ff200f6f4771e04a01271d5
amanibhavam <iam@defn.sh>
2025-02-04 16:03:03 +0000

    fix: correct xz-utils package name

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -efu -o pipefail
 
 function main {
     sudo apt update
-    sudo apt install -y make direnv curl xz
+    sudo apt install -y make direnv curl xz-utils
     source .bash_profile
     make install
 }
