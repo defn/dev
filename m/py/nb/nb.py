1db8f873e03e60bb99096bafec4b4e921673fc6a
amanibhavam <iam@defn.sh>
2025-02-13 04:40:38 +0000

    feat: marimo notebooks from script

--- m/py/nb/nb.py
+++ m/py/nb/nb.py
@@ -1,4 +1,10 @@
 import marimo
+from marimo._config.settings import GLOBAL_SETTINGS
+from marimo._server.file_router import AppFileRouter
+from marimo._server.model import SessionMode
+from marimo._server.start import start
+from marimo._server.tokens import AuthToken
+from marimo._utils.marimo_path import MarimoPath
 
 __generated_with = "0.10.19"
 app = marimo.App()
@@ -19,8 +25,28 @@ def _(mo):
 @app.cell
 def _():
     import marimo as mo
+
     return (mo,)
 
 
 if __name__ == "__main__":
-    print(app.run())
+    GLOBAL_SETTINGS.YES = True
+    GLOBAL_SETTINGS.QUIET = False
+    start(
+        file_router=AppFileRouter.from_filename(MarimoPath(__file__)),
+        development_mode=GLOBAL_SETTINGS.DEVELOPMENT_MODE,
+        quiet=GLOBAL_SETTINGS.QUIET,
+        host="127.0.0.1",
+        port=None,
+        proxy=None,
+        headless=True,
+        mode=SessionMode.RUN,
+        include_code=True,
+        ttl_seconds=None,
+        watch=True,
+        base_url="",
+        allow_origins=None,
+        auth_token=AuthToken(""),
+        cli_args=None,
+        redirect_console_to_browser=False,
+    )
