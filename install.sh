73b3579e56858df6db06522f7e7355e4222b22da
amanibhavam <iam@defn.sh>
2025-02-15 21:21:00 +0000

    feat: base ubuntu packages in Docker and install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -efu -o pipefail
 
 function main {
     sudo apt update
-    sudo apt install -y make direnv curl xz-utils dirmngr gpg rsync build-essential
+    # sync with m/Dockerfile
+    sudo apt install -y  make direnv curl xz-utils dirmngr gpg rsync build-essential sudo locales git rsync tini iproute2 iptables
     source .bash_profile
     make install
     make sync
