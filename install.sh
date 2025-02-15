a0394524503ae7ce3a82f2906e6c17a47429d81c
amanibhavam <iam@defn.sh>
2025-02-15 22:07:26 +0000

    feat: m/i image in terms of m base

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -efu -o pipefail
 function main {
     sudo apt update
     # sync with m/Dockerfile
-    sudo apt install -y  make direnv curl xz-utils dirmngr gpg rsync build-essential sudo locales git rsync tini iproute2 iptables
+    sudo apt install -y make direnv curl xz-utils dirmngr gpg rsync build-essential sudo ca-certificates tzdata locales git tini iproute2 iptables bc pv socat
     source .bash_profile
     make install
     make sync
