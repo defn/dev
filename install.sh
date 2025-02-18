f1fa5f02518e0704bfea0ff9ccbc5db0bcb62122
amanibhavam <iam@defn.sh>
2025-02-18 02:21:52 +0000

    feat: s6

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 set -efu -o pipefail
 
 function main {
-    sudo apt update
-    # sync with m/Dockerfile
-    sudo apt install -y make direnv curl xz-utils dirmngr gpg rsync build-essential sudo ca-certificates tzdata locales git tini iproute2 iptables bc pv socat
-    source .bash_profile
-    make install
-    make sync
-    make install
+	sudo apt update
+	# sync with m/Dockerfile
+	sudo apt install -y make direnv curl xz-utils dirmngr gpg pcscd scdaemon gpg-agent rsync build-essential sudo ca-certificates tzdata locales git git-lfs tini iproute2 iptables bc pv socat docker.io s6
+	source .bash_profile
+	make install
+	make sync
+	make install
 }
 
 main "$@"
