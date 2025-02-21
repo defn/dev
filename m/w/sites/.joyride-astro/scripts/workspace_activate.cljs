74a2560d4d804a44574811177ea540e3717609a6
amanibhavam <iam@defn.sh>
2025-02-21 01:26:38 +0000

    feat: run s6-svscan on vscode workspace startup

--- m/w/sites/.joyride-astro/scripts/workspace_activate.cljs
+++ m/w/sites/.joyride-astro/scripts/workspace_activate.cljs
@@ -1,4 +1,19 @@
 (ns workspace-activate
-  (:require [tutlib :as tutlib]))
+  (:require ["child_process" :as child-process]
+            ["vscode" :as vscode]
+            ["util" :as node-util]
+            [promesa.core :as p]
+            [tutlib :as tutlib]))
+
+(def exec!+ (node-util/promisify child-process/exec))
+
+(defn run-s6 []
+  (-> (p/do!
+       (exec!+ "s6-svscan"
+               #js {:cwd (str vscode/workspace.rootPath "/svc")
+                    :shell true}))
+      (p/catch (fn [e]
+                 (js/console.error "Error running s6-svscan:" e)))))
 
 (tutlib.load-tutorial "src/pages/index.astro" "tutorial.html")
+(run-s6)
