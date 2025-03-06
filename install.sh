8ddfddf4afe8d03e9c55c4c809c61d36bbc2cd9e
amanibhavam <iam@defn.sh>
2025-03-05 18:35:38 -0800

    fix: sync before install to remove mise prompt

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ function main {
 	# sync with m/Dockerfile
 	sudo apt install -y make direnv curl xz-utils dirmngr gpg pcscd scdaemon gpg-agent rsync build-essential sudo ca-certificates tzdata locales git git-lfs tini iproute2 iptables bc pv socat docker.io s6
 	source .bash_profile
-	make install
 	make sync
+	make install
 	make install
 }
 
