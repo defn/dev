8989de21ecf10c930a341456eb359ca24f966ac9
amanibhavam <iam@defn.sh>
2025-03-14 01:01:32 +0000

    fix: sync various install scripts

--- install.sh
+++ install.sh
@@ -3,10 +3,18 @@
 set -efu -o pipefail
 
 function main {
+	export DEBIAN_FRONTEND=noninteractive
+
 	sudo apt update
-	# sync with m/Dockerfile
-	sudo apt install -y make direnv curl xz-utils dirmngr gpg pcscd scdaemon gpg-agent rsync build-essential sudo ca-certificates tzdata locales git git-lfs tini iproute2 iptables bc pv socat docker.io s6 cpu-checker
+
+	# sync with m/Dockerfile, m/i/script/001-install-base
+	sudo apt install -y \
+		make direnv curl xz-utils dirmngr gpg pcscd scdaemon gpg-agent rsync \
+		build-essential sudo ca-certificates tzdata locales git git-lfs tini \
+		iproute2 iptables bc pv socat docker.io s6 cpu-checker bind9-dnsutils
+
 	source .bash_profile
+
 	make sync
 	make install
 }
