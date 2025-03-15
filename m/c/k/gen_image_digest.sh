fa547a6edead4b0d765b0984e638436225b06ffa
amanibhavam <iam@defn.sh>
2025-03-15 23:42:48 +0000

    fix: remove references to pkg/oci flake

--- m/c/k/gen_image_digest.sh
+++ m/c/k/gen_image_digest.sh
@@ -16,7 +16,7 @@ function main {
 		echo package k
 		"${cue}" export --out json -e images "${app}" k/*.cue | jq -r '.[]' |
 			sort -u |
-			runmany 8 'echo cache: \"$1\": \"$(echo $1 | perl -pe '"'"'s{[:@].*$}{}'"'"')@$(${skopeo} inspect docker://${1%%@sha256*} | jq -r .Digest)\"' |
+			runmany 8 'echo cache: \"$1\": \"$(echo $1 | perl -pe '"'"'s{[:@].*$}{}'"'"')@$(skopeo inspect docker://${1%%@sha256*} | jq -r .Digest)\"' |
 			while read -r l; do
 				echo "$l"
 			done
