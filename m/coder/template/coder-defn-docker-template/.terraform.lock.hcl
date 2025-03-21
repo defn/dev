4e7d25dc84a224d7119e99044ea667a5b519e18d
defn Nghiem <62630472+amanibhavam@users.noreply.github.com>
2025-03-20 20:25:52 -0700

    feat: docker workspaces (#119)
    fixes #118 
    
    Coder workspace template using Docker containers.
    
    Configures a Docker containers with environment variables, then executes
    `j create-coder-agent` in the container to set up the s6 services to run
    Coder agent. The rest naturally follows like any other workspace.
    
    This approach is essentially the same approach taken in the `ssh`
    template, where `ssh` to a remote server and `docker exec` to a
    container is how the Coder server gets into the workspace to run the `j
    create-coder-agent` command to initialize the Coder agent, etc.

---
+++ m/coder/template/coder-defn-docker-template/.terraform.lock.hcl
@@ -0,0 +1,45 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/coder/coder" {
+  version     = "2.1.0"
+  constraints = "2.1.0"
+  hashes = [
+    "h1:dtIpPUMDXTlUUduuIopu6Xe2N+AcJswzCRGWGsx7YAY=",
+    "zh:5127faf3deca705b1d6de96223616e7b31ae6f7b0287468331a5164e8dc5f699",
+    "zh:5be0cf519c0ad923168d3f3ad487a538c07df9e815f1b273ac9dc6b7a7f762b1",
+    "zh:61b5100256ae8faa361d6b5de5dbac0f0c7b625d9b4fdd4d831722df096579c3",
+    "zh:6982a82582a6e294ba4f6dcf35fcf1c10137b3f9dca71158c28ac275e6eb7bd8",
+    "zh:7048d505a7caf051dff47da790e717d5422ca2166cbc594b2200111fbf2a8741",
+    "zh:7f8f3cad020fe056fead728c7017fd19acf2c8e39ef0161659377b4b76652449",
+    "zh:acb09c32e2c580f9cdf5f62997e65f071e907c032dc92dd44f72987265b93471",
+    "zh:afbd14d934abcdaf07151299f3c68e074891b371f348e6a5b20121a4b240e1f2",
+    "zh:bc6d67bd144e5b857a5997fd9227a77d778b15d20d2e19ad48771a28cbdcb106",
+    "zh:be1bb8eaeb8ac6a99aca0554b701b218baf7f497a6a377394266abb41f58c08c",
+    "zh:c454925a4388965662be51c35cd8d8cfa957bfdcafe0459a9a030be70d9b9a72",
+    "zh:cb35e0273acc2fce29e5ad2282fab4169c22a164e451615216d79a86e9f2abda",
+    "zh:e6082c9b646ae1ee2970d879d1877317018e0600cbbb50c8d92aca90ec5a7b45",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:f8c8185fb6f3b91e2cf96924a7041ddb3e3c4024aeb9cb127a2081949cd9c2ce",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version     = "3.2.3"
+  constraints = "3.2.3"
+  hashes = [
+    "h1:+AnORRgFbRO6qqcfaQyeX80W0eX3VmjadjnUFUJTiXo=",
+    "zh:22d062e5278d872fe7aed834f5577ba0a5afe34a3bdac2b81f828d8d3e6706d2",
+    "zh:23dead00493ad863729495dc212fd6c29b8293e707b055ce5ba21ee453ce552d",
+    "zh:28299accf21763ca1ca144d8f660688d7c2ad0b105b7202554ca60b02a3856d3",
+    "zh:55c9e8a9ac25a7652df8c51a8a9a422bd67d784061b1de2dc9fe6c3cb4e77f2f",
+    "zh:756586535d11698a216291c06b9ed8a5cc6a4ec43eee1ee09ecd5c6a9e297ac1",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:9d5eea62fdb587eeb96a8c4d782459f4e6b73baeece4d04b4a40e44faaee9301",
+    "zh:a6355f596a3fb8fc85c2fb054ab14e722991533f87f928e7169a486462c74670",
+    "zh:b5a65a789cff4ada58a5baffc76cb9767dc26ec6b45c00d2ec8b1b027f6db4ed",
+    "zh:db5ab669cf11d0e9f81dc380a6fdfcac437aea3d69109c7aef1a5426639d2d65",
+    "zh:de655d251c470197bcbb5ac45d289595295acb8f829f6c781d4a75c8c8b7c7dd",
+    "zh:f5c68199f2e6076bce92a12230434782bf768103a427e9bb9abee99b116af7b5",
+  ]
+}
---
+++ m/coder/template/coder-defn-docker-template/cdk.tf
@@ -0,0 +1,168 @@
+terraform {
+  required_providers {
+    coder = {
+      version = "2.1.0"
+      source  = "coder/coder"
+    }
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+  backend "local" {
+
+  }
+}
+
+variable "docker_socket" {
+  default = ""
+  type    = string
+}
+
+provider "coder" {
+}
+
+provider "docker" {
+  host = var.docker_socket != "" ? var.docker_socket : null
+}
+
+data "coder_provisioner" "me" {}
+data "coder_workspace" "me" {}
+data "coder_workspace_owner" "me" {}
+
+data "coder_parameter" "homedir" {
+  default      = "/home/ubuntu/m"
+  description  = "home directory"
+  display_name = "HOME dir"
+  icon         = "https://raw.githubusercontent.com/matifali/logos/main/cpu-3.svg"
+  mutable      = true
+  name         = "homedir"
+  type         = "string"
+}
+
+data "coder_parameter" "os" {
+  default      = "linux"
+  description  = "Operating system"
+  display_name = "Operation system"
+  icon         = "https://raw.githubusercontent.com/matifali/logos/main/cpu-3.svg"
+  mutable      = true
+  name         = "os"
+  type         = "string"
+}
+
+data "coder_parameter" "arch" {
+  default      = "amd64"
+  description  = "CPU arch"
+  display_name = "CPU arch"
+  icon         = "https://raw.githubusercontent.com/matifali/logos/main/cpu-3.svg"
+  mutable      = true
+  name         = "arch"
+  type         = "string"
+}
+
+resource "coder_agent" "main" {
+  arch = data.coder_parameter.arch.value
+  auth = "token"
+  env = {
+    GIT_AUTHOR_EMAIL    = "${data.coder_workspace_owner.me.email}"
+    GIT_AUTHOR_NAME     = "${data.coder_workspace_owner.me.name}"
+    GIT_COMMITTER_EMAIL = "${data.coder_workspace_owner.me.email}"
+    GIT_COMMITTER_NAME  = "${data.coder_workspace_owner.me.name}"
+  }
+  os             = data.coder_parameter.os.value
+  startup_script = "cd ~ && source .bash_profile && j destroy-coder-agent && cd ~/m && j coder::code-server $${CODER_NAME}"
+  display_apps {
+    ssh_helper      = false
+    vscode          = false
+    vscode_insiders = false
+  }
+}
+
+resource "docker_volume" "home_volume" {
+  name = "coder-${data.coder_workspace.me.id}-home"
+
+  lifecycle {
+    ignore_changes = all
+  }
+
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace_owner.me.name
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace_owner.me.id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  labels {
+    label = "coder.workspace_name_at_creation"
+    value = data.coder_workspace.me.name
+  }
+}
+
+resource "docker_container" "workspace" {
+  count = data.coder_workspace.me.start_count
+
+  name = "coder-${data.coder_workspace_owner.me.name}-${lower(data.coder_workspace.me.name)}"
+
+  image    = "quay.io/defn/dev:base"
+  hostname = data.coder_workspace.me.name
+
+  env = [
+    "CODER_AGENT_TOKEN=${coder_agent.main.token}",
+    "CODER_AGENT_URL=${data.coder_workspace.me.access_url}",
+    "CODER_AGENT_TOKEN=${coder_agent.main.token}",
+    "CODER_NAME=${data.coder_workspace.me.name}",
+    "CODER_HOMEDIR=${data.coder_parameter.homedir.value}",
+    "CODER_INIT_SCRIPT_BASE64=${base64encode(coder_agent.main.init_script)}"
+  ]
+
+  host {
+    host = "host.docker.internal"
+    ip   = "host-gateway"
+  }
+
+  volumes {
+    container_path = "/home/coder"
+    volume_name    = docker_volume.home_volume.name
+    read_only      = false
+  }
+
+  provisioner "local-exec" {
+    command = "docker exec coder-${data.coder_workspace_owner.me.name}-${lower(data.coder_workspace.me.name)} bin/with-env just create-coder-agent"
+  }
+
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace_owner.me.name
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace_owner.me.id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  labels {
+    label = "coder.workspace_name"
+    value = data.coder_workspace.me.name
+  }
+}
+
+resource "coder_app" "code-server" {
+  agent_id     = coder_agent.main.id
+  display_name = "code-server"
+  icon         = "/icon/code.svg"
+  share        = "owner"
+  slug         = "cs"
+  subdomain    = true
+  url          = "http://localhost:8080/?folder=${data.coder_parameter.homedir.value}"
+  healthcheck {
+    interval  = 5
+    threshold = 6
+    url       = "http://localhost:8080/healthz"
+  }
+}
