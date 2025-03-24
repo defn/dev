bfee41a5aa365e2e9e78f72262a9bccf78d36576
amanibhavam <iam@defn.sh>
2025-03-24 00:39:37 +0000

    fix: wait for release-tag to be created

--- .buildkite/bin/home-build.sh
+++ .buildkite/bin/home-build.sh
@@ -4,17 +4,6 @@ set -efu -o pipefail
 
 source .bash_profile
 
-pushd m
+cd m
 source ~/work/.buildkite/.env
 j base
-popd
-
-exit 0
-
-mkdir -p /home/ubuntu/work/bazel
-docker run --rm \
-	-v $(pwd)/.git:/home/ubuntu/.git \
-	-v /home/ubuntu/work/.buildkite:/.buildkite \
-	-v bazel-cache-1:/home/ubuntu/work/bazel \
-	--entrypoint bash ghcr.io/defn/dev:base -c \
-	"sudo chown ubuntu:ubuntu work/bazel; source /.buildkite/.env; git reset --hard && source .bash_profile && cd m && b build"
