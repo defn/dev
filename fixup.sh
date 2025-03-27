dfa21a232fd828a1c35c86ba2bb1e64f55ba8a30
defn Nghiem <62630472+amanibhavam@users.noreply.github.com>
2025-03-27 00:04:29 +0000

    feat: ./install.sh works on github codespaces

--- fixup.sh
+++ fixup.sh
@@ -5,20 +5,20 @@ set -efu -o pipefail
 function main {
 	perl -pe 's{^\s*GSSAPIAuthentication}{#GSSAPIAuthentication}' -i /etc/ssh/ssh_config
 
-	if test -x /usr/local/bin/nix/ping; then	
+	if test -x /usr/local/bin/nix/ping; then
 		setcap cap_net_raw+p $(readlink /usr/local/bin/nix/ping)
 	fi
 
 	for a in docker-credential-{pass,secretservice}; do rm -vf "$(which "$a")"; done
 
 	if ! test -e .docker/config.json; then
-		install -o ubuntu -g ubuntu -m 0600 .docker/config.json.example .docker/config.json
+		install -o "$(id -un $SUDO_USER)" -g "$(id -gn $SUDO_USER)" -m 0600 .docker/config.json.example .docker/config.json
 	fi
 
 	if test -e /var/run/docker.sock; then
-		chgrp ubuntu /var/run/docker.sock
+		chgrp "$(id -gn $SUDO_USER)" /var/run/docker.sock
 	fi
-	usermod -aG docker ubuntu
+	usermod -aG docker "$(id -un $SUDO_USER)"
 }
 
 main "$@"
