0750e6553ff219f5735d3b828992cfea01c95bb0
amanibhavam <iam@defn.sh>
2025-03-29 04:00:00 +0000

    feat: remove ubuntu docker.io, making way for docker-ce

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ function main {
 	sudo apt install -y \
 		make direnv curl xz-utils dirmngr gpg pcscd scdaemon gpg-agent rsync \
 		build-essential sudo ca-certificates tzdata locales git git-lfs tini \
-		iproute2 iptables bc pv socat docker.io s6 cpu-checker bind9-dnsutils \
+		iproute2 iptables bc pv socat s6 cpu-checker bind9-dnsutils \
 		pass ||
 		true
 
