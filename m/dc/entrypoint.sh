a220cbbc3a7e7c6d21f6ba838f562db3db36f21f
amanibhavam <iam@defn.sh>
2025-04-06 10:04:09 -0700

    fix: m/dc j up moved to s6

--- m/dc/entrypoint.sh
+++ m/dc/entrypoint.sh
@@ -2,4 +2,10 @@
 
 sudo chown ubuntu:ubuntu ~/.config/coderv2
 
+(
+	sleep 5
+	sudo tailscale up --ssh --hostname "$(hostname | cut -d . -f1)-dev"
+	tailscale ip
+) &
+
 exec /bin/s6-svscan /home/ubuntu/m/svc
