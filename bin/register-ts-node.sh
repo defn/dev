2ca183495f6f2f47ee1e52d22fb417879d773d62
amanibhavam <iam@defn.sh>
2025-04-07 20:04:40 -0700

    chore: fmt bin

--- bin/register-ts-node.sh
+++ bin/register-ts-node.sh
@@ -6,12 +6,12 @@ export TOKEN=$(curl -s -d "client_id=${TS_CLIENT_ID}" -d "client_secret=${TS_CLI
 export AUTH_KEY=$(curl -s -X POST -u "${TOKEN}:" "https://api.tailscale.com/api/v2/tailnet/-/keys" -H 'Content-Type: application/json' -d '{"capabilities":{"devices":{"create":{"reusable":false,"ephemeral":true,"preauthorized":false,"tags":["tag:aws"]}}},"expirySeconds":60,"description":"Type your description"}' | jq -r '.key')
 
 if [ ! -z "${NODE_NAME}" ]; then
-  IDS=$(curl -s -u "${TOKEN}:" https://api.tailscale.com/api/v2/tailnet/-/devices | jq -r --arg node "${NODE_NAME}" '.devices[] | select(.hostname == $node) | .nodeId')
-  for ID in ${IDS}; do
-    echo "deleting node $ID"
-    curl -X DELETE -u "${TOKEN}:" "https://api.tailscale.com/api/v2/device/${ID}"
-    echo
-  done
+	IDS=$(curl -s -u "${TOKEN}:" https://api.tailscale.com/api/v2/tailnet/-/devices | jq -r --arg node "${NODE_NAME}" '.devices[] | select(.hostname == $node) | .nodeId')
+	for ID in ${IDS}; do
+		echo "deleting node $ID"
+		curl -X DELETE -u "${TOKEN}:" "https://api.tailscale.com/api/v2/device/${ID}"
+		echo
+	done
 fi
 
 echo tailscale up --hostname=$NODE_NAME --auth-key=$AUTH_KEY
