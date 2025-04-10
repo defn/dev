e8bf6d3ddfcfefe4cf5c24e34ac5a4ec742e475d
amanibhavam <iam@defn.sh>
2025-04-10 03:11:18 +0000

    fix: ami builder uses make sync

--- m/i/install-packer.sh
+++ m/i/install-packer.sh
@@ -3,6 +3,8 @@
 set -efu -o pipefail
 
 function main {
+	export DEBIAN_FRONTEND=noninteractive
+
 	if [[ "$(whoami || true)" == "ubuntu" ]]; then
 		sudo "$0" "$@"
 
@@ -12,25 +14,15 @@ function main {
 		mv dev/.git .
 		rm -rf dev
 		git reset --hard
-		chown -R ubuntu:ubuntu .
 
-		./install.sh	
+		make sync
 
 		return $?
 	fi
 
-	export DEBIAN_FRONTEND=noninteractive
+	apt update && apt upgrade -y
 
-	apt update
-
-	# sync with install.sh, m/i/Dockerfile
 	apt install -y \
-		make curl xz-utils dirmngr gpg pcscd scdaemon gpg-agent rsync \
-		build-essential sudo ca-certificates tzdata locales git git-lfs tini \
-		iproute2 iptables bc pv socat s6 cpu-checker bind9-dnsutils \
-		pass skopeo
-
-	apt update && apt install -y \
 		wireguard-tools qemu-system libvirt-clients libvirt-daemon-system \
 		openvpn easy-rsa expect tpm2-tools zfsutils-linux ubuntu-drivers-common
 
@@ -50,51 +42,34 @@ Update-Manager::Never-Include-Phased-Updates;
 APT::Get::Never-Include-Phased-Updates: True;
 EOF
 
+	tee /etc/sysctl.d/99-dfd.conf <<EOF
+net.ipv4.ip_forward=1
+net.ipv6.conf.all.forwarding=1
+fs.inotify.max_user_instances=10000
+fs.inotify.max_user_watches=524288
+EOF
+
+	mkdir -p /etc/systemd/network
+
+	tee /etc/systemd/network/dummy1.netdev <<EOF
+[NetDev]
+Name=dummy1
+Kind=dummy
+EOF
+
+	/etc/systemd/network/dummy1.network <<EOF
+[Match]
+Name=dummy1
+
+[Network]
+Address=169.254.32.1/32
+EOF
+
 	curl -v -fsSL https://tailscale.com/install.sh | bash -x
 	rm -rf /var/lib/tailscale
 
-	(
-		echo net.ipv4.ip_forward=1
-		echo net.ipv6.conf.all.forwarding=1
-		echo fs.inotify.max_user_instances=10000
-		echo fs.inotify.max_user_watches=524288
-	) | tee /etc/sysctl.d/99-dfd.conf
-
-	mkdir -p /etc/systemd/network
-	pushd /etc/systemd/network
-
-	local a
-	a=1
-
-	(
-		echo "[NetDev]"
-		echo Name=dummy"$a"
-		echo Kind=dummy
-	) | sudo tee dummy"$a".netdev
-
-	(
-		echo "[Match]"
-		echo Name=dummy"$a"
-		echo
-		echo "[Network]"
-		echo Address=169.254.32."$a"/32
-	) | sudo tee dummy"$a".network
-
-	popd
-
-	install -m 0755 -d /etc/apt/keyrings &&
-		curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
-		chmod a+r /etc/apt/keyrings/docker.asc &&
-		echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu noble stable" | tee /etc/apt/sources.list.d/docker.list &&
-		apt-get update &&
-		apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-	chown -R ubuntu:ubuntu /home/ubuntu && chmod u+s /usr/bin/sudo
+	chmod u+s /usr/bin/sudo
 	echo 'ubuntu ALL=(ALL:ALL) NOPASSWD: ALL' >/etc/sudoers.d/ubuntu
-
-	usermod -aG docker ubuntu
-
-	install -d -m 0700 -o ubuntu -g ubuntu /home/ubuntu
 }
 
 main "$@"
