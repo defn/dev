f43ffa2dfe454dc12d234fe4f6225195c2c7f1d7
amanibhavam <iam@defn.sh>
2025-04-10 00:22:00 +0000

    fix: run os upgrade, install first

--- Makefile
+++ Makefile
@@ -253,12 +253,12 @@ sync:
 	$(MAKE) sync_inner
 
 sync_inner:
+	mise run local upgrade
+	mise run local ubuntu
 	mise self-update --yes || true
 	git ls-files | grep 'mise.toml$$' | runmany 'mise trust $$1'
 	mise install
 	(cd m && mise install)
-	mise run local upgrade
-	mise run local ubuntu
 	mise run local fixup
 	for a in cue yaegi; do sudo ln -nfs $$(mise exec -- which $$a) /usr/local/bin/; done
 	bin/persist-cache
--- install.sh
+++ install.sh
@@ -5,15 +5,6 @@ set -efu -o pipefail
 function main {
 	export DEBIAN_FRONTEND=noninteractive
 
-	sudo apt update
-
-	# sync with m/i/Dockerfile, m/i/install-packer.sh
-	sudo apt install -y \
-		make curl xz-utils dirmngr gpg pcscd scdaemon gpg-agent rsync \
-		build-essential sudo ca-certificates tzdata locales git git-lfs tini \
-		iproute2 iptables bc pv socat s6 cpu-checker bind9-dnsutils \
-		pass skopeo
-
 	if [[ ! -x ~/.local/bin/mise ]]; then curl -sSL https://mise.run | bash; fi
 
 	source .bash_profile
