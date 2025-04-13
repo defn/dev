045aaf38e35d25ffdb0803359ca215119576ec3d
amanibhavam <iam@defn.sh>
2025-04-13 16:36:53 -0700

    fix: m/dc tailscaled startup and docker permissions

--- m/dc/entrypoint.sh
+++ m/dc/entrypoint.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
 sudo chown -R ubuntu:ubuntu ~/.config/coderv2 ~/.local/share/code-server/extensions ~/.config ~/.local
+sudo chgrp docker /var/run/docker.sock
+
+ln -nfs ../svc.d/tailscaled ~/m/svc/
 
 (
-	sleep 5
-	sudo tailscale up --ssh --hostname "$(hostname | cut -d . -f1)-dev"
-	tailscale ip
+	while true; do
+		if tailscale ip; then
+			break
+		fi
+		if sudo tailscale up --ssh; then
+			break
+		fi
+		sleep 1
+	done
 ) &
 
 exec /bin/s6-svscan /home/ubuntu/m/svc
