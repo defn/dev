a970a510435d8715e629bd1bc1416201bd8d1491
amanibhavam <iam@defn.sh>
2025-04-17 08:09:54 -0700

    feat: example runs by default

--- m/command/root/root.go
+++ m/command/root/root.go
@@ -10,20 +10,15 @@ import (
 
 var cfgFile string
 
-// RootCmd represents the base command when called without any subcommands
-var RootCmd = &cobra.Command{
+var defaultRootCmd = &cobra.Command{
 	Use:   "cli",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = defaultRootCmd
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the RootCmd.
@@ -32,6 +27,11 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func ExecuteCommand(c *cobra.Command) {
+	RootCmd = c
+	Execute()
 }
 
 func init() {
