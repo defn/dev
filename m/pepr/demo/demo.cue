f1e30b204ca42ba14789f4c979dbab0cf57c65af
amanibhavam <iam@defn.sh>
2025-05-11 22:55:16 +0000

    fix: no temporal, pepr

--- m/pepr/demo/demo.cue
+++
@@ -1,8 +0,0 @@
-package api
-
-ex: demo: in: {
-    name: "demo"
-    namespace: "defn"
-    script: "id -a"
-    workdir: "/tmp"
-}
--- m/pepr/pepr.ts
+++
@@ -1,6 +0,0 @@
-import { PeprModule } from "pepr";
-import { UnicornPepr } from "./capabilities/unicorn";
-import { DefnPepr } from "./capabilities/defn";
-import cfg from "./package.json";
-
-new PeprModule(cfg, [UnicornPepr, DefnPepr]);
