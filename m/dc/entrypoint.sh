b4bcbdae3f82ad1f2418eafaa0977d28587e340c
amanibhavam <iam@defn.sh>
2025-10-08 18:58:02 -0700

    config for defn.xxx

--- m/dc/entrypoint.sh
+++ m/dc/entrypoint.sh
@@ -5,16 +5,4 @@ sudo chgrp docker /var/run/docker.sock
 
 ln -nfs ../svc.d/tailscaled ~/m/svc/
 
-(
-	while true; do
-		if tailscale ip; then
-			break
-		fi
-		if sudo tailscale up --ssh; then
-			break
-		fi
-		sleep 1
-	done
-) &
-
 exec /bin/s6-svscan /home/ubuntu/m/svc
