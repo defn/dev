f8173c7e71b169d2d7408a04ac98508ac1c19ef7
amanibhavam <iam@defn.sh>
2026-01-10 03:53:21 +0000

    remove old stuff

--- m/w/sites/nghiem.io/.astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Xu3dJ from "../../assets/closet.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/closet.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Xu3dJ]]);
-		
